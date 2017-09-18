f929a9e7e9d93b1caa5ff559ed9b1d42f1d85740
Marko Banusic <mbanusic@gmail.com>
2017-09-18 16:56:29 +0200

    Fix latest

--- widgets/tg_upravo_objavljeno.php
+++ widgets/tg_upravo_objavljeno.php
@@ -12,31 +12,47 @@ class TG_Procitajte_Danas extends WP_Widget
     }
 
     public function widget($args, $instance)
-    {  ?>
-        <div class="tg-widget rainbow-widget procitajte-danas">
-            <div class="tg-widget-head">
-                Upravo objavljeno
-            </div>
-            <div class="tg-widget-body">
-                <?php
-                global $post;
-                $args = array(
-                    'showposts' => 5,
-                    'post_type' => array('post', 'fotogalerije', 'video', 'price'),
-                );
-                $q = new WP_Query($args);
-                //$q = z_get_zone_query('masthead', array('posts_per_page' => 5));
-                while ($q->have_posts()) {
-                    $q->the_post();
-                    // Rainbow article small
-                    get_template_part('templates/articles/article-rainbow-small');
-                }
-                wp_reset_query();
-                ?>
-            </div>
-        </div>
+    {
+        $data = wp_cache_get('telegram_latest', 'widgets');
+        if (!$data) {
+	        ob_start();
 
-        <?php
+	        ?>
+            <div class="tg-widget rainbow-widget procitajte-danas">
+                <div class="tg-widget-head">
+                    Upravo objavljeno
+                </div>
+                <div class="tg-widget-body">
+			        <?php
+			        global $post;
+			        $args = array(
+				        'posts_per_page' => 5,
+				        'post_type'      => array(
+					        'post',
+					        'fotogalerije',
+					        'video',
+					        'price'
+				        ),
+				        'meta_key'       => 'latest_off',
+				        'meta_value'     => 0
+			        );
+			        $q    = new WP_Query( $args );
+			        //$q = z_get_zone_query('masthead', array('posts_per_page' => 5));
+			        while ( $q->have_posts() ) {
+				        $q->the_post();
+				        // Rainbow article small
+				        get_template_part( 'templates/articles/article-rainbow-small' );
+			        }
+			        wp_reset_query();
+			        ?>
+                </div>
+            </div>
+
+	        <?php
+            $data = ob_get_clean();
+            wp_cache_set('telegram_latest', $data, 'widgets', 600);
+        }
+        echo $data;
     }
 
 }
