fe5ec538a0f64d09dcbc3dc7e71f063b3e020f12
Marko Banusic <mbanusic@gmail.com>
2017-09-18 17:12:25 +0200

    Fix share

--- templates/layout/share-box.php
+++ templates/layout/share-box.php
@@ -1,6 +1,6 @@
 <div class="share-box">
     <div class="facebook">
-        <a href="https://www.facebook.com/dialog/share?app_id=103100136402693&display=popup&href=<?php echo urlencode( get_the_permalink() ); ?>&redirect_uri=<?php echo urlencode( get_the_permalink() ); ?>" target="_blank" class="fb fb-share" data-social-network="Facebook" data-social-action="share" data-social-target="<?php the_permalink(); ?>"><i class="fa fa-facebook"></i> Podijeli</a>
+        <a href="https://www.facebook.com/dialog/share?app_id=1383786971938581&display=popup&href=<?php echo urlencode( get_the_permalink() ); ?>&redirect_uri=<?php echo urlencode( get_the_permalink() ); ?>" target="_blank" class="fb fb-share" data-social-network="Facebook" data-social-action="share" data-social-target="<?php the_permalink(); ?>"><i class="fa fa-facebook"></i> Podijeli</a>
     </div>
     <div class="twitter">
         <a href="https://twitter.com/intent/tweet?counturl=<?php echo urlencode( get_the_permalink() ) ?>&text=<?php echo urlencode( get_the_title() ); ?>&url=<?php echo urlencode( get_the_permalink() ) ?>&via=TelegramHR" target="_blank" class="tw" data-social-network="Twitter" data-social-target="<?php the_permalink() ?>" data-social-action="tweet"><i class="fa fa-twitter"></i> Tweetaj</a>
