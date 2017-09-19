e0651c35f579596b39a51035cb9f8c37630af1be
Mihovil Radauš <mradaus@gmail.com>
2017-09-19 13:24:27 +0200

    - 404 fix - megabreak shadow overlay

--- 404.php
+++ 404.php
@@ -8,9 +8,9 @@
                 Čini se da je nešto još više otišlo u pogrešnom smjeru
             </h1>
             <p>
-                Ova stranica kreirana je s isključivim ciljem da na njoj nikada ne zateknete.
+                Ova stranica kreirana je s isključivim ciljem da se na njoj nikada ne zateknete.
                 Ukoliko ste vlastitom krivnjom ovdje ipak završili, srdačno Vas molimo da Telegram i dalje
-                smatrate ozbiljnim portalom, te se vratite na
+                smatrate ozbiljnim portalom te se vratite na
                 <a href="<?php echo home_url(); ?>">naslovnicu</a>.
             </p>
             <img id="tg-small" src="<?php echo get_template_directory_uri(); ?>/assets/img/telegram_logo.svg" height="25"/>
