14719c7e8c1094c5ca893efbafa2dc8138b98bae
Mihovil Radauš <mradaus@gmail.com>
2017-09-20 12:37:15 +0200

    Killed off Poster articles (be back in a month)

--- templates/layout/megabreak.php
+++ templates/layout/megabreak.php
@@ -7,19 +7,4 @@
     <?php
     telegram_load_megabreak('break', 1);
       ?>
-
-    <div class="megabreak-bottom">
-        <div class="container">
-            <div class="poster-articles">
-                <?php
-                telegram_load_megabreak('smallbreak', 1);
-                telegram_load_megabreak('smallbreak', 2);
-                telegram_load_megabreak('smallbreak', 3);
-                  ?>
-            </div>
-
-        </div>
-
-        <div class="outline-hider"></div>
-    </div>
 </div>
