ad42261cc1f5477d1fa7b4beb366c7bee6a80217
Marko Banusic <mbanusic@gmail.com>
2017-09-20 11:00:06 +0200

    Move promo to cli

--- cli.php
+++ cli.php
@@ -45,6 +45,74 @@ class Telegram_Command extends WP_CLI_Command {
 			$page++;
 		}
 	}
+
+	public function promo($args, $assoc_args) {
+		$position = $assoc_args['position'];
+		remove_action('pre_get_posts', 'telegram_pre_get_posts');
+		remove_action('pre_get_posts', 'telegram_main_query');
+		if ('desktop' == $position) {
+			$meta_query = array(
+				'relation' => 'OR',
+				array(
+					'key' => 'pozicija_1',
+					'value' => '1',
+				),
+				array(
+					'key' => 'pozicija_2',
+					'value' => '1',
+				),
+				array(
+					'key' => 'pozicija_4',
+					'value' => '1',
+				)
+			);
+		}
+		else {
+			$meta_query = array(
+				array(
+					'key' => 'pozicija_3',
+					'value' => '1'
+				)
+			);
+		}
+		$q = new WP_Query(array(
+			'post_status' => 'publish',
+			'cat' => 3926,
+			'posts_per_page' => 20,
+			'meta_query' => $meta_query,
+			'no_found_rows' => true
+		));
+		$pos = array();
+		while ($q->have_posts()) {
+			$q->the_post();
+			$obj = $this->get_object($q->post->ID, $position);
+			if ($obj['od'] <= current_time('Ymd')) {
+				if ( $obj['do'] <= date( 'Ymd', current_time('U') ) ) {
+					update_post_meta( $q->post->ID, 'pozicija_' . $obj['pos'], 0 );
+				} else {
+					$pos[] = $obj['ID'];
+				}
+			}
+		}
+		update_option( 'telegram_promo_'.$position, $pos );
+	}
+
+	private function get_object($post_id, $position) {
+		if ($position == 'desktop') {
+			$pos1 = 1;
+		}
+		else {
+			$pos1 = 3;
+		}
+
+		return $object = array(
+			'ID'  => intval( $post_id ),
+			'od'  => intval( get_post_meta( $post_id, 'datum_od' . $pos1, true ) ),
+			'do'  => intval( get_post_meta( $post_id, 'datum_do' . $pos1, true ) ),
+			'pos' => $pos1
+		);
+
+	}
 }
 
 WP_CLI::add_command( 'telegram', 'Telegram_Command' );
