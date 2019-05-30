3755dd0efad1469bcb94b6a05c54a41ab5311f08
Marko Banušić <mbanusic@gmail.com>
2019-05-30 15:43:18 +0200

    Fix redis race condition

--- functions-shared.php
+++ functions-shared.php
@@ -1263,3 +1263,22 @@ function telegram_rest_posts($args, $request) {
     $args['post_type'] = 'post,price';
     return $args;
 }
+
+/**
+ * Fix a race condition in alloptions caching
+ *
+ * See https://core.trac.wordpress.org/ticket/31245
+ */
+function _wpcom_vip_maybe_clear_alloptions_cache( $option ) {
+	if ( ! wp_installing() ) {
+		$alloptions = wp_load_alloptions(); //alloptions should be cached at this point
+
+		if ( isset( $alloptions[ $option ] ) ) { //only if option is among alloptions
+			wp_cache_delete( 'alloptions', 'options' );
+		}
+	}
+}
+
+add_action( 'added_option',   '_wpcom_vip_maybe_clear_alloptions_cache' );
+add_action( 'updated_option', '_wpcom_vip_maybe_clear_alloptions_cache' );
+add_action( 'deleted_option', '_wpcom_vip_maybe_clear_alloptions_cache' );
