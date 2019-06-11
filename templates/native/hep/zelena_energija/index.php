1717dd1338a548f1b12bdc363249d89e839ffbde
Marko Banušić <mbanusic@gmail.com>
2019-06-11 14:43:01 +0200

    Hep native

---
+++ templates/native/hep/zelena_energija/index.php
@@ -0,0 +1,98 @@
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/templates/native/hep/zelena_energija/style.css?ver=1.0" type="text/css" />
+<div class="native-page">
+    <div class="native-hero center">
+        <div class="native-hero-title full">
+            <h1 class="center-text">Otkrij dobru energiju</h1>
+            <h2 class="center-text">HEP zna vrijednost obnovljivih izvora</h2>
+        </div>
+        <div class="full center">
+            <video preload="auto" autoplay="" muted="" loop="">
+                <source src="<?php echo get_stylesheet_directory_uri() ?>/templates/native/hep/zelena_energija/img/motiongrafika_eko.webm" type="video/webm">
+                <source src="<?php echo get_stylesheet_directory_uri() ?>/templates/native/hep/zelena_energija/motiongrafika_eko.mp4" type="video/mp4">
+            </video>
+        </div>
+    </div>
+    <article>
+        <div class="container">
+            <h3>HEP ima sjajan plan kako povećati obnovljive izvore energije </h3>
+            <p class="subtitle">Hrvatska elektroprivreda do 2023. godine planira uložiti 750 milijuna kuna u sunčane elektrane Kaštelir, Cres, Vis, Vrlika Jug...</p>
+            <p>Obnovljivi izvori energije koji se dobivaju iz prirode svaki nam danom postaju sve važniji. Prije svega zbog svoje neškodljivosti prema okolišu. To je, naravno, prepoznao i HEP koji poduzima vrlo konkretne korake prema tome te do 2030. godine planira povećati udio obnovljivih izvora energije s 35 na više od 50 posto. Taj cilj HEP planira postići revitalizacijom, odnosno povećanjem snage i proizvodnje postojećih hidroelektrana, izgradnjom novih hidroelektrana te ulaganjima u vjetroelektrane, sunčane elektrane i ostale obnovljive izvore energije.</p>
+            <p>Konkretnije, HEP do 2023. godine ulaže 750 milijuna kuna u sunčane elektrane, a do 2030. godine planira imati sunčane elektrane ukupne snage 350 MW, što je jednako njegovoj polovici snage Nuklearne elektrane Krško. Sunčane elektrane Kaštelir, Cres, Vis i Vrlika Jug, ukupne snage 11,6 MW i vrijednosti 80 milijuna kuna, prve su četiri sunčane elektrane u <a href="https://www.youtube.com/watch?v=hX9-QBsVo2w" target="_blank">velikom investicijskom ciklusu Hrvatske elektroprivrede</a> do 2023. godine uložiti 750 milijuna kuna.</p>
+            <h4>VELIKI PLANOVI ZA BOLJU BUDUĆNOST</h4>
+            <p>Nakon što su početkom travnja potpisani ugovor o kupoprodaji <a href="https://www.youtube.com/watch?v=3GNGghgXuVQ&feature=youtu.be" target="_blank">Sunčane elektrane Kaštelir</a> te ugovor o izgradnji <a href="https://www.youtube.com/watch?v=yuOfq0emd_4&feature=youtu.be" target="_blank">Sunčane elektrane Cres,</a> uskoro započinje izgradnja Sunčane elektrane Vis, projekta kojega je u prosincu prošle godine HEP preuzeo od tvrtke Končar OIE. Također, u drugoj polovici 2019. HEP Proizvodnja će izgraditi Sunčanu elektranu Vrlika Jug.</p>
+            <img src="https://www.telegram.hr/wp-content/uploads/2019/05/SE-Ka%C5%A1telir.jpg" alt="Sunčana elektrana Kaštelir" />
+            <p>HEP je pokrenuo ciklus izgradnje sunčanih elektrana, kao dijela obnovljivog scenarija razvoja u skladu s polazištima buduće energetske strategije RH i smjernica EU energetsko-klimatske politike. Do 2030. godine HEP planira realizirati projekte sunčanih elektrana ukupne snage 350 MW. U idućem petogodišnjem razdoblju investicije HEP-a u sunčane elektrane tako će iznositi prosječno 150 milijuna kuna godišnje, odnosno 750 milijuna kuna do kraja 2023. godine, što u prosjeku omogućava izgradnju 20 MW novih solarnih kapaciteta godišnje. Navedeno uključuje i projekte integriranih sunčanih elektrana na zgradama u vlasništvu HEP-a.</p>
+            <p>Elektrane Vrlika Jug, Cres i Vis proizvodit će električnu energiju bez ugovora s HROTE-om o otkupu po poticajnoj cijeni. Sunčane elektrane na Visu i Cresu doprinijet će jačanju infrastrukture u funkciji razvoja turizma, jer će električnu energiju najviše proizvoditi onda kad je najpotrebnija, u razdobljima povećane potrošnje tijekom turističke sezone. Sva četiri projekta elektrana nastala su u suradnji HEP-a s partnerima – jedinicama lokalne i regionalne samouprave i tvrtkama.</p>
+            <p>Osim s tvrtkama, HEP u području sunčanih elektrana i drugih obnovljivih izvora surađuje i s jedinicama lokalne samouprave. Tako je u tijeku HEP-ov <a href="http://www.hep.hr/projekti/obnovljivi-izvori-energije/poziv-za-iskaz-interesa-oie/obavijest-o-zahtjevu-za-iskazivanjem-interesa/3402" target=_blank">javni poziv zainteresiranim partnerima za razvoj i prodaju projekata obnovljivih izvora energije,</a> koji je otvoren do 31. prosinca, i u sklopu kojeg je HEP spreman realizirati sve financijski isplative projekte koji će zadovoljiti energetske, ekonomske i okolišne kriterije.</p>
+            <h4>SUNČANE ELEKTRANE NA OTOCIMA</h4>
+            <p>U idućem razdoblju HEP planira snažnije pokrenuti program izgradnje fotonaponskih sunčanih elektrana, pri čemu je fokus lokacijama na većim otocima, gdje se mogu instalirati elektrane snage u pravilu do 10 MW. Sunčane elektrane koriste obnovljivi izvor energije, bez emisija stakleničkih plinova, što se uklapa u globalna nastojanja za ublažavanje klimatskih promjena. Sunčane elektrane na otocima važne su i stoga jer će električnu energiju najviše proizvoditi onda kad je najpotrebnija – u razdobljima povećane potrošnje tijekom turističke sezone. Sunčane elektrane su dobar izbor upravo na otocima i sličnim izoliranim područjima. Zbog dobre osunčanosti, lokacije na otocima su među najatraktivnijima. U skladu sa strateškim planovima Vlade RH, HEP će posebnu pozornost posvećuje razvoju elektroenergetske infrastrukture na otocima, što uključuje i razmatranje mogućnosti izgradnje sunčanih elektrana. Uglavnom je riječ o projektima koji se razvijaju u različitim oblicima partnerstva odnosno potencijalnog partnerstva s drugim poduzetnicima ili institucijama. Spomenuti projekti (SE Cres, SE Vis i SE Vrlika Jug) dobri su primjeri kako jedinice lokalne i regionalne samouprave mogu pomoći u bržem razvoju svojih područja.</p>
+        </div>
+        <div class="main-color">
+            <div class="container">
+                <h3>HEP-ove sunčane elektrane</h3>
+                <details open>
+                    <summary class="full"><h4>Sunčana elektrana Vis</h4></summary>
+                    <p>Sunčana elektrana Vis bit će smještena na brdu Grizova glavica, u blizini naselja Žena Glava, oko 3,6 kilometra jugozapadno od grada Visa te oko 4,8 kilometara istočno od Komiže. Instalirana snaga elektrane bit će 2,2 MW, a priključna 2 MW. Očekivana godišnja proizvodnja je 3,1 milijun kWh čime se mogu podmiriti potrebe oko 1.000 kućanstava. Radovi na izgradnji trajat će od travnja do prosinca 2019. Vrijednost projekta je oko 20 milijuna kuna.</p>
+                    <img src="https://www.telegram.hr/wp-content/uploads/2019/05/SE-Vis-vizualizacija.jpg" alt="Sunčana elektrana Vis" />
+                </details>
+                <details>
+                    <summary class="full"><h4>Sunčana elektrana Vrlika Jug</h4></summary>
+                    <p>Sunčana elektrana Vrlika Jug predstavlja realizaciju 1. faze planirane izgradnje na južnom dijelu Radne zone Kosore na području Grada Vrlike. Grad Vrlika je i razvijao projekt, koji je HEP Proizvodnja otkupila u prosincu 2018. godine. Vrijednost investicije iznosi 14,7 milijuna kuna. Priključna snaga elektrane bit će 2,1 MW, a očekivana godišnja proizvodnja oko 2,9 milijuna kWh. Čitava izgradnja trebala bi se realizirati tijekom druge polovice 2019. godine.</p>
+                </details>
+                <details>
+                    <summary class="full"><h4>Sunčana elektrana Cres</h4></summary>
+                    <p>Sunčana elektrana Cres, snage 6,5 MW, prvi je projekt koji je HEP preuzeo u sklopu ciklusa izgradnje i akvizicije sunčanih elektrana. Očekivana proizvodnja elektrane, koja će se nalaziti oko 2 kilometra sjeverno od naselja Orlec, je 8,5 milijuna kWh godišnje, što odgovara potrošnji oko 2.500 kućanstava. Ugovor o izgradnji vrijedan je 34,7 milijuna kuna. Radovi trebaju započeti u jesen 2019., a  završiti u travnju 2020. godine.</p>
+                    <img src="https://www.telegram.hr/wp-content/uploads/2019/05/SE-Cres-vizualizacija.jpg" alt="Sunčana elektrana Cres" />
+                </details>
+                <details>
+                    <summary class="full"><h4>Sunčana elektrana Kaštelir</h4></summary>
+                    <p>Sunčana elektrana Kaštelir ima priključnu snage 1 MW i očekivanu godišnju proizvodnju od oko 1,5 milijuna kWh, što odgovara potrošnji oko 500 kućanstava. U elektrani su ugrađeni paneli proizvođača opreme za sunčane elektrane Solvis iz Varaždina. Elektrana je u pogonu je od prosinca 2018. godine. Ima sklopljen ugovor s HROTE-om te se kao povlašteni proizvođač nalazi u sustavu poticaja.</p>
+                </details>
+            </div>
+        </div>
+    </article>
+    <article>
+        <div class="container">
+            <h3>HEP do 2030. godine planira ostvarivati devet milijardi kilovatsati godišnje iz obnovljivih izvora</h3>
+            <p class="subtitle">Ambiciozni cilj podrazumijeva povećanje udjela obnovljivih izvora u proizvodnji električne energije na više od 50 posto</p>
+            <p>
+            Prema predviđanjima svjetskih stručnjaka, potrošnja energije do 2035. godine trebala bi biti za trećinu veća od današnje. Energetska tranzicija iz dominantne upotrebe fosilnih goriva u dominantno korištenje obnovljivih izvora energije apsolutno je neizbježna i zbog povećanja potrošnje, ali i zbog klimatskih promjena. Sektor energetike i energetska politika razvijaju se kako bi se omogućio prijelaz na čistu energiju.</p>
+            <p>Diljem EU-a i država članica provode se planovi za postizanje dogovorenih energetskih i klimatskih ciljeva do 2030. te se razvija i poboljšava model tržišta električne energije i plina. Neprestano se razvijaju politike i mjere za potporu inovacijama i ulaganjima, a Europska komisija nedavno je, u skladu s obvezama preuzetima u okviru Pariškog klimatskog sporazuma, donijela dugoročnu strategiju za smanjenje emisija stakleničkih plinova do 2050.</p>
+            <h4>PARIŠKI SPORAZUM ZA SPAS OKOLIŠA</h4>
+            <p>Pariški klimatski sporazum poziva zemlje da ograniče porast temperature na ne više od 1,5 do dva stupnja Celzija kako bi se izbjegle ozbiljne posljedice po okoliš. Prema istraživanju The Boston Consultingovog Henderson Instituta, za postizanje takvog cilja potrebna je sveobuhvatna globalna primjena najnaprednijih tehnologija u proizvodnji, štednji i potrošnji energije, kao zajednički pothvat vlada i regulatora cijelog svijeta. No, osim što je politički iznimno izazovan, to je i iznimno skup pothvat.</p>
+            <img src="https://www.telegram.hr/wp-content/uploads/2019/05/GettyImages-1022892932-840x530.jpg" alt="Zelena energija" />
+            <p>On prema procjenama stručnjaka instituta zahtijeva ulaganja od 19 do 21 bilijuna dolara od danas do 2030., koja bi pokrila tek uvođenje tehnologija, mjere energetske učinkovitosti te zamjenu postojećih, konvencionalnih onečišćivača, a do 80% tih sredstava trebale bi uložiti države u razvoju. To je, međutim znatno manje od iznosa šteta uzrokovanih klimatskim promjenama, kakve se očekuju u budućnosti. Štete već danas pogađaju države diljem svijeta. Prema izvještaju Europske agencije za okoliš Hrvatska spada u skupinu od tri zemlje, zajedno sa Češkom i Mađarskom, s najvećim udjelom šteta od ekstremnih vremenskih i klimatskih događaja u BDP-u. Računa se da su ti gubici, u razdoblju od 1980. do 2015. godine, iznosili više od 2,8 milijardi eura.</p>
+            <h4>HEP-OV PLAN: DEVET MILIJARDI KILOVATSATI GODIŠNJE IZ OBNOVLJIVIH IZVORA</h4>
+            <p>HEP-ova nastojanja prate proklamirane planove dekarbonizacije Europske unije do 2050. godine, koji radi smanjenja emisija stakleničkih plinova za 80 do 95 posto u odnosu na 1990. godinu (a u pojedinim scenarijima i do 100 posto) nameću potrebu znatnog <a href="https://www.hep.hr/projekti/obnovljivi-izvori-energije/3406" target="_blank">povećanja udjela obnovljivih izvora energije.</a> Iako konkretni ciljevi za udjel obnovljivih izvora energije u potrošnji električne energije nisu još preneseni u domaću regulativu, realno je očekivati da će, unatoč već ostvarenom visokom udjelu obnovljivih izvora energije od 27,3 posto u bruto potrošnji energije (svih oblika energije, ne samo električne) u razdoblju od 2020. do 2030. godine, Hrvatska biti obvezna znatno povećati sadašnji udjel. Za ostvarenje tog nacionalnog cilja, HEP-ov udjel je nezamjenjiv.</p>
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/templates/native/hep/zelena_energija/img/hep_infografika.jpg" alt="Infografika HEP">
+            <p>U svojoj strategiji razvoja za razdoblje do 2030. godine, HEP grupa je za temeljni scenarij razvoja odredila postizanje dodatnih tri milijarde kilovatsati proizvodnje iz obnovljivih izvora energije. Naime, kako bi u proizvodnji električne energije HEP grupa povećala današnji udjel obnovljivih izvora s 35 na planiranih više od 50 posto u 2030. godini, bit će potrebno povećati proizvodnju iz obnovljivih izvora sa sadašnjih šest na čak devet milijardi kilovatsati u prosječnoj hidrološkoj godini. Taj cilj HEP planira ostvariti revitalizacijom, odnosno povećanjem snage i proizvodnje postojećih hidroelektrana, izgradnjom novih hidroelektrana te ulaganjima u vjetroelektrane, sunčane elektrane i ostale obnovljive izvore energije.</p>
+            <h4>HRVATSKA PRATI EU PLANOVE DEKARBONIZACIJE ENERGETIKE</h4>
+            <p>HEP je početkom veljače objavio <a href="http://www.hep.hr/obavijest/obavijest-o-zahtjevu-za-iskazivanjem-interesa/3402" target="_blank">poziv zainteresiranim partnerima za razvoj i prodaju projekata obnovljivih izvora energije na području Hrvatske.</a>     Poziv je otvoren do 31. prosinca 2019., a riječ je o nastavku HEP-ovih aktivnosti širenja korištenja obnovljivih izvora energije, kroz integraciju u vlastiti proizvodni portfelj onih projekata obnovljivih izvora energije koji su već završeni ili su u visokom stupnju razvoja. Poziv je upućen jedinicama lokalne samouprave (gradovima i općinama), zainteresiranima za izgradnju sunčanih elektrana te pravnim i fizičkim osobama, koje razvijaju projekte sunčanih elektrana ili koje razvijaju, ali i onima koje već imaju u pogonu vjetroelektrane ili hidroelektrane.</p>
+            <p>U konačnici, HEP-ov bi poziv potencijalnim partnerima, zajedno s ostalim HEP-ovim projektima u segmentu proizvodnje električne energije, trebao dovesti do ostvarenja ambicioznog plana zacrtanog kompanijskom razvojnom strategijom, koja predviđa da će udjel obnovljivih izvora u HEP-ovoj proizvodnji električne energije s oko 35 posto u 2018. godini porasti na više od 50 posto u 2030., a do 2050. godine na čak oko 70 posto, odnosno na dvostruko veću vrijednost nego danas.</p>
+            <p>Pritom treba naglasiti da HEP već danas više od 70 posto električne energije kojom raspolaže za potrebe svojih kupaca dobiva iz izvora koji ne emitiraju CO2, a to su proizvodnja u vlastitim hidroelektranama, otkup električne energije proizvedene iz elektrana na obnovljive izvore energije u sustavu poticaja i preuzimanje električne energije iz Nuklearne elektrane Krško koja je u 50-postotnom vlasništvu HEP-a. U 2018. godini udjel izvora koji ne emitiraju CO2 iznosio je čak 73 posto, po čemu je HEP u samom vrhu srodnih kompanija u Europi.</p>
+        </div>
+    </article>
+    <article>
+        <div class="container">
+            <h4>NATJEČAJ</h4>
+            <h3>ZelEn – novih milijun kuna za energetsku obnovu dječjih škola i vrtića</h3>
+            <p class="subtitle">Javni poziv s uvjetima sudjelovanja objavljen je na stranici zelen.hep.hr, a otvoren je do 24. lipnja</p>
+            <p>HEP Opskrba i Hrvatska elektroprivreda raspisali su novi javni poziv za dodjelu sredstava na ime naknade za proizvod ZelEn, ukupne vrijednosti od milijun kuna, što je kupovinom zelene električne energije u 2018. godini omogućilo više od 130 tvrtki, kupaca proizvoda ZelEn.</p>
+            <p>ZelEn je električna energija dobivena isključivo iz obnovljivih izvora energije, registriranih u sustavu jamstva podrijetla. Ovaj jedinstveni proizvod HEP Opskrbe namijenjen je kupcima koji su se u svom poslovanju odlučili za društveno odgovorno poslovanje, brigu o okolišu i korištenje električne energije iz obnovljivih izvora.</p>
+            <p>Sredstva ostvarena prodajom ZelEna prikupljaju se u fondu iz kojega se ostvaruju projekti iz područja obnovljivih izvora energije, energetske učinkovitosti i sustavnog gospodarenja energijom za potrebe ustanova javnoga sektora koje skrbe za društveno osjetljive kategorije stanovništva.</p>
+            <p>Sudionici natječaja mogu biti isključivo registrirane institucije ili ustanove javnog sektora sa sjedištem u Republici Hrvatskoj, osnovane za trajno obavljanje djelatnosti odgoja i obrazovanja, znanosti, kulture, informiranja, sporta, tjelesne kulture, tehničke kulture, skrbi o djeci, zdravstva, socijalne skrbi i skrbi o invalidima, i u kojima vlasnik/osnivač ne obavlja djelatnost radi stjecanja dobiti.</p>
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/templates/native/hep/zelena_energija/img/zelen_infografika.jpg" alt="Infografika HEP">
+            <p>Prijavitelji moraju posjedovati projektnu dokumentaciju za izvedbu energetske učinkovitosti i uvođenja obnovljivih izvora energije sa sveobuhvatnim troškovnicima i projektantskom procjenom investicije.</p>
+            <p>Tekst Javnog poziva s uvjetima sudjelovanja, Obrazac prijave i prateći obrasci objavljeni su na stranici <a href="http://zelen.hep.hr" target="_blank">zelen.hep.hr,</a> a natječaj je otvoren do 24. lipnja 2019. u 14 sati.</p>
+        </div>
+    </article>
+    <div class="native-signature wow fadeInUp">
+        <div class="container flex center">
+            <img class="tg-studio" src="https://super1.telegram.hr/wp-content/themes/super1-theme/templates/native/arena/dobre_odluke/img/tg_studio.png" alt="TG Studio logo">
+            <p>Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom HEP i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();
