18b6ce127c3a1b116a562dff887528f0a7bc946f
Marko Banušić <mbanusic@gmail.com>
2019-08-01 14:34:35 +0200

    Bayer native

---
+++ templates/native/bayer/ljetovanje/index.php
@@ -0,0 +1,94 @@
+<?php get_header(); 
+$template_url = get_theme_root_uri() . '/telegram2-desktop';
+?>
+<script src="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/functions.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Lilita+One&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/style.css?ver=1.0" type="text/css" />
+<div class="main-container barracuda">
+    <div class="native-hero flex full">
+        <div class="third flex-responsive center" style="justify-content: flex-end">
+            <div class="native-hero-art">
+                <img class="native-hero-art" src="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/img/doggo_max_chillax.svg" alt="Ljetovanje s psom" />
+            </div>
+        </div>
+        <div class="two-thirds flex-responsive native-title center">
+            <div>
+                <h1 class="wow slideInLeft full center-text">Na ljetovanje s ljubimcem</h1>
+                <h2 class="wow slideInLeft full center-text" data-wow-delay="0.5s">Zaštitite svoga psa i uživajte na moru</h2>
+            </div>
+        </div>
+    </div>
+    <div class="waves">
+        <img class="wave-1" src="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/img/wave1.png" alt="Val">
+        <img class="wave-2" src="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/img/wave2.png" alt="Val">
+    </div>
+    <a href="https://www.telegram.hr/partneri/spremate-se-s-ljubimcem-na-ljetovanje-znamo-kako-da-putovanje-bude-bezbrizno/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img src="https://www.telegram.hr/wp-content/uploads/2019/06/glavna-bayer-840x530.jpg" alt="Spremate se s ljubimcem na ljetovanje? Znamo kako da putovanje bude bezbrižno">
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Spremate se s ljubimcem na ljetovanje? Znamo kako da putovanje bude bezbrižno</h1>
+                <p>Putujete li s psom na Jadran, zaštitite ga kako bi izbjegli mogućnost zaraze opasnom lišmaniozom</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+
+    <a href="hhttps://www.telegram.hr/partneri/pitali-smo-veterinara-kako-u-ljetnim-mjesecima-ljubimca-zastititi-od-vrlo-opasne-i-ne-vas-rijetke-lismanioze/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Pitali smo veterinara kako u ljetnim mjesecima ljubimca zaštititi od vrlo opasne i ne baš rijetke lišmanioze</h1>
+                <p>Osim što se teško liječi i uočava, lišmanioza je prenosiva i na čovjeka, a u ljetnim mjesecima mogućnost zaraze je najveća</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img src="https://www.telegram.hr/wp-content/uploads/2019/07/GettyImages-1041987488-840x530.jpg" alt="Pitali smo veterinara kako u ljetnim mjesecima ljubimca zaštititi od vrlo opasne i ne baš rijetke lišmanioze">
+        </div>
+    </a>
+
+    <div class="container infografika">
+        <img src="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/img/infografika.png" alt="Infografika">
+    </div>
+
+    <a href="https://super1.telegram.hr/relax/voditeljice-barbara-kolar-i-ivana-miseric-idu-na-more-sa-svojim-psima-imaju-neodoljivu-pricu-o-odmoru-s-lolom-i-fridom/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img src="https://super1.telegram.hr/wp-content/uploads/sites/3/2019/07/ivana_miseric_62473527_136338017563011_3141272464019566853_n-880x520.jpg" alt="Voditeljice Barbara Kolar i Ivana Mišerić idu na more sa svojim psima. Imaju neodoljivu priču o odmoru s Lolom i Fridom">
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Voditeljice Barbara Kolar i Ivana Mišerić idu na more sa svojim psima. Imaju neodoljivu priču o odmoru s Lolom i Fridom</h1>
+                <p>"Ako ljetujem u Hrvatskoj, onda svoju Lolu obavezno vodim sa sobom."</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <a href="https://www.telegram.hr/partneri/poznati-parazitolog-objasnjava-zasto-je-ljubimce-bas-prije-ljetovanja-nuzno-zastititi-od-teske-bolesti-lismanioze/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Poznati parazitolog objašnjava zašto je ljubimce baš prije ljetovanja nužno zaštititi od teške bolesti lišmanioze</h1>
+                <p>Gotovo neizlječiva bolest kod pasa, lišmanioza, najrasprostranjenija je na području Dalmacije, a rizik se smanjuje upotrebom repelenata</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img src="https://www.telegram.hr/wp-content/uploads/2019/07/GettyImages-1127338371-840x530.jpg" alt="Poznati parazitolog objašnjava zašto je ljubimce baš prije ljetovanja nužno zaštititi od teške bolesti lišmanioze">
+        </div>
+    </a>
+    <div class="native-signature wow fadeInUp">
+        <div class="container flex center">
+            <img class="tg-studio" src="<?php echo esc_url( $template_url ) ?>/templates/native/bayer/ljetovanje/img/logos.png" alt="TG Studio and partners logos" />
+            <p>Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom Bayer i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
+<?php get_footer();
