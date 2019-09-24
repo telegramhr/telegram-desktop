1f06990ae9095809797a04b12859fe5fedc5227c
Marko Banušić <mbanusic@gmail.com>
2019-09-24 14:05:16 +0200

    remove HT

--- home.php
+++ home.php
@@ -2,9 +2,7 @@
 get_header(); ?>
 
 <div class="home-top-container">
-    <?php
-    the_widget('Telegram_HT_Desktop');
-    ?>
+    <?php the_widget('Telegram_Top_Vijesti'); ?>
 
     <div class="zones container">
         <?php
