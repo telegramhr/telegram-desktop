789e4e577743009a75095a7b46504a7547b17831
Marijan Mikolić <marijan@telegramgrupa.hr>
2019-11-07 11:55:53 +0100

    Visa advertorial

---
+++ templates/native/visa/premium/index.php
@@ -0,0 +1,179 @@
+﻿<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$directory = PARENT_PATH . "/templates/native/visa/premium/";
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300|Roboto:300,400,700&amp;subset=latin-ext" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="<?php echo $directory; ?>tmg_framework.css?ver=1.0" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $directory; ?>style.css?ver=1.0" type="text/css" />
+    <script src="<?php echo $directory; ?>js/slick/slick.js"></script>
+    <link rel="stylesheet" href="<?php echo $directory; ?>js/slick/slick.css"/>
+    <link rel="stylesheet" href="<?php echo $directory; ?>js/slick/slick-theme.css"/>
+</head>
+
+<body>
+<div class="main-container">
+    <header class="full center relative">
+        <div class="title margin25 padding50 center-text relative" data-tilt>
+            <h1>Premium Visa kartice. <br>Najbolje za vas</h1>
+            <h2>Najbolje za vas</h2>
+            <img class="powered-by desktop-only" src="<?php echo $directory; ?>img/powered-by.jpg" alt="Powered by Visa" widht="100" height="50">
+        </div>
+    </header>
+    <div class="full center"><img class="mobile-only" src="<?php echo $directory; ?>img/powered-by.jpg" alt="Powered by Visa" widht="100" height="50"></div>
+    <article>
+        <div class="container">
+            <h2 class="white-space main-color-text text-container center-text">Život bez kartica ne može se zamisliti. Evo zašto</h2>
+            <img src="<?php echo $directory; ?>img/1.jpg" alt="Plaćanje karticom" class="responsive no-horizontal-padding padding50"/>
+            <h3 class="white-space text-container">Malo tko od nas život može zamisliti bez kartica.  U doba kada sve rješavamo brzo, digitalno i nemamo vremena za odlaske u poslovnice banaka, kartice su pravi spas</h3> 
+            <p class="text-container">Jednostavne, uvijek dostupne, sigurne za korištenje, kartice sve više zamjenjuju gotovinu koja će, kako se procjenjuje, u narednih desetak godina biti svedena na minimum. Da je tako svjedoče brojke. Čak 95 posto Hrvata ima barem jednu karticu, a prosječan broj kartica po osobi je 2,7. Najčešće ih koriste građani iznad 40 godina, te češće muškarci, no znamo da ih itekako vole i žene. Online shopping, putovanja, kupovina na rate.... sve su to situacije koje ne možemo zamisliti bez kartica.</p>
+            <h3 class="white-space text-container">Kartice volimo od 20-ih godina prošlog stoljeća</h3>
+            <p class="text-container">Prve su kreditne kartice počeli izdavati hotelski lanci i velike tvrtke  u Americi 20-ih godina prošloga stoljeća kako bi kod potrošača razvili lojalnost i očito su itekako pogodili. Pravi procvat kreditne kartice doživljavaju nakon 1950. godine.  Bank of America je 1958. godine predstavila prvu bankovnu karticu nazvanu Bank Americard, a proizvod se ubrzo proširio i na Europu, gdje je 1965. godine u Belgiji osnovan Eurocard.</p>
+            <h3 class="white-space text-container">Visa MasterCard prva masovna kartica </h3>
+            <p class="text-container">Prva kreditna kartica koja je uspjela steći masovnu popularnost je Visa MasterCard, izdana kasnih šezdesetih. Kompjutorskom revolucijom i smanjenjem troškova izdavanja, kultura kupnje i plaćanja kreditnim karticama postala je raširena svugdje u svijetu, a Hrvati su je objeručke dočekali. </p>
+            <h3 class="white-space text-container">U Hrvatskoj je izdano 8,8 milijuna kartica</h3>
+            <p class="text-container">Prema podacima Hrvatske gospodarske komore u  Hrvatskoj je izdano 8,8 milijuna kreditnih i debitnih kartica, i ta je u brojka u neprestanom porastu.</p>
+            <h3 class="white-space text-container">U online kupnju s karticom</h3>
+            <p class="text-container">Posljednja istraživanja pokazuju da gotovo svi odrasli Hrvati koriste platne kartice. Debitne kartice koristi oko  89% građana, a kartice s odgodom plaćanja njih 55%. Sve tipove kartica iznadprosječno koriste oni s mjesečnim prihodima iznad 5.500 kuna, stariji od 40 godina te u većoj mjeri muškarci nego žene. Kreditne kartice najčešće koristimo za online kupnju. Razlozi za online kupnju su brojni, a kao glavni se ističe povoljnija cijena, ušteda vremena, mogućnost kupnje u bilo koje doba dana, veći izbor nego u klasičnim trgovinama, kupnja robe koju se može nabaviti samo online, mogućnost dostave na željenu adresu te mogućnost lakšeg uspoređivanja cijena nego u klasičnim dućanima. Na internetu kupuje čak 84% korisnika kartica, a 34% ih to čini više puta mjesečno.</p>
+            <p class="text-container"><img src="<?php echo $directory; ?>img/dalic.jpg" alt="Dalić" class="responsive no-horizontal-padding padding50"/></p>
+            <h3 class="white-space text-container">Kartice volimo i na putovanjima</h3>
+            <p class="text-container">Najaktivniji online shopperi su potrošači u dobnoj skupini od 30 do 39, s visokim stupnjem obrazovanja. Najčešće se kupuju odjeća, obuća i modni dodaci, potrošačka elektronika, predmeti za kućanstvo, kozmetika i parfemi  te ulaznice za kulturna događanja. Visoko na popisu su i usluge turističkih agencija, igračke i oprema za bebe, knjige i glazba te softver i zrakoplovne karte. Karticama volimo plaćati i kad smo na putovanjima, ponajprije zbog jednostavnosti i sigurnosti. Kartice se na inozemnim putovanjima u velikoj mjeri koriste za shopping, plaćanje smještaja te plaćanje u restoranima.</p>
+            <h3 class="white-space text-container">Premium Visa kartice za vrhunsko iskustvo</h3>
+            <p class="text-container">PBZ Card Premium Visa kartice jedinstven su i inovativan kartični proizvod, koji objedinjuje premium kartične pogodnosti i usluge, koje je razvio PBZ Card, lider na hrvatskom kartičnom tržištu, te niz prednosti i pogodnosti koje korisnicima omogućuje Visa, kao jedan od najsnažnijih kartičnih brendova u svijetu.  Premium Visa kartice nude premium usluge i pogodnosti, poput tehnološki najnaprednijeg beskontaktnog plaćanja, najvećih paketa osiguranja, potrošnje bez unaprijed zadanog limita, obročne otplate bez kamata i naknadna, kredita s otplatom putem kartica, pomoći na putu i drugog. Korisnici sudjeluju u Premium Rewardsu - najvećem programu nagrađivanja njihove vjernosti, a dostupne su im i Premium ponude - izbor pogodnosti kod odabranih prodajnih mjesta. Na usluzi im je Premium Card Service, Concierge i druge usluge posvećene njihovim zahtjevima. Kartice imaju i dodatne pogodnosti koje donosi Visa, poput najbolje prihvaćenosti u zemlji i svijetu, najviših sigurnosnih standarda te suvremenih rješenja plaćanja.</p>
+        </div>
+    </article>
+    <div class="gray-bg padding25 no-vertical-padding">
+        <div class="container">
+            <div class="white-space full center-text">
+                <h4>Infografika</h4>
+                <h2>Brojke o Visa kartici koje morate znati</h2>
+            </div>
+            <div class="white-space full flex">
+                <div class="third flex center-text padding50 infographic-parent">
+                    <div class="infographic-figure full bold main-color-text">3,3 milijarde</div>
+                    <div class="infographic-text full">korisnika širom svijeta</div>
+                </div>
+                <div class="third flex center-text padding50 infographic-parent">
+                    <div class="infographic-figure full bold main-color-text">200 </div>
+                    <div class="infographic-text full">je država u kojima posluje Visa kartica</div>
+                </div>
+                <div class="third flex center-text padding50 infographic-parent">
+                    <div class="infographic-figure full bold main-color-text">54 milijuna</div>
+                    <div class="infographic-text full">prodajnih mjesta u svijetu gdje je možete koristiti</div>
+                </div>
+            </div>
+            <img src="<?php echo $directory; ?>img/visa_infographic.svg" alt="Infografika" class="infographic-image"> 
+            <div class="white-space full flex">
+                <div class="third flex center-text padding50 infographic-parent">
+                    <div class="infographic-figure full bold main-color-text">2,7 milijuna</div>
+                    <div class="infographic-text full">bankomata </div>
+                </div>
+                <div class="third flex center-text padding50 infographic-parent">
+                    <div class="infographic-figure full bold main-color-text">24 rate</div>
+                    <div class="infographic-text full">bez kamata i naknada </div>
+                </div>
+                <div class="third flex center-text padding50 infographic-parent">
+                    <div class="infographic-figure full bold main-color-text">31.12.2019.</div>
+                    <div class="infographic-text full">datum je do kada nove korisnike Premium Visa kartice očekuje posebna pogodnost - bez plaćanja upisnine i članarine za prvu godinu korištenja</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main-color-bg">
+        <div class="container white-text">
+            <div class="white-space full center-text">
+                <h4>Q&A</h4>
+                <h2>5 pitanja o Premium Visa karticama koja morate znati</h2>
+            </div>
+            <div class="slick">
+                <div>
+                    <h3>1. Što nude Premium Visa kartice?</h3>
+                    <p>Premium Visa kartice objedinjuju kartične pogodnosti i usluge koje je razvio PBZ Card, lider na hrvatskom kartičnom tržištu, te prednosti i pogodnosti koje donosi Visa. U portfelju 12 kartičnih proizvoda za sve potrebe: od studenata, poduzetnika, do onih koji puno putuju i skupljaju nagradne milje te Premium Visa Gold - osnovna kartica ovog portfelja te kartice za one koji traže i trebaju vrhunsku uslugu i prestižne pogodnosti koje prate njihov životni stil. Za njih je kreirana Premium Visa Platinum i Premium Visa Infinite - najprestižnija kartica na našem tržištu, koja nudi novu dimenziju ekskluzivnih pogodnosti i funkcionalnosti.</p>
+                </div>
+                <div>
+                    <h3>2. Treba li PIN za aktivaciju Premium Visa kartice?</h3>
+                    <p>Za aktivaciju Premium Visa kartice kao i autorizaciju troškova na prodajnom mjestu te podizanje gotovog novca na bankomatu potreban je PIN. Stoga je važno da korisnik zapamti svoj PIN. Ako ga izgubi ili zaboravi, na njegov zahtjev izdat će mu se u bilo kojem trenutku novi PIN i to bez plaćanja naknade. Ako želi, PIN može promijeniti na bilo kojem bankomatu Privredne banke Zagreb.</p>
+                </div>
+                <div>
+                    <h3>3. Koliko je sigurno korištenje Premium Visa kartica?</h3>
+                    <p>Premium Visa kartica osigurana je Verified by Visa uslugom koja unutar 3-D Secure sustava pruža najveću razinu zaštite od zloupotrebe kartice na fizičkim i online prodajnim mjestima. Prilikom potvrde kupnje na online prodajnom mjestu koje je u 3-D Secure sustavu, korisnik potvrđuje svoj identitet pomoću jednokratne zaporke koja se generira putem mobilnog tokena (mTokena). Mobilni token treba aktivirati unutar My Way aplikacije.</p>
+                </div>
+                <div>
+                    <h3>4. Što je My Way usluga?</h3>
+                    <p>Ova usluga korisnicima omogućuje pregled i ispis računa iz prethodnih razdoblja, pregled nefakturiranih troškova i uplata, promjenu osobnih podataka te uvid u stanje Premium Rewards bodova. Tu korisnici Premium Visa kartica mogu aktivirati mToken, koji je neophodan za autorizaciju transakcija na online prodajnim mjestima koja su u 3-D Secure sustavu. Unutar My Way aplikacije korisnik može aktivirati i E-račun uslugu, kojom umjesto papirnatih računa/izvadaka poštom za odabranu karticu ili kartice dobiva račun/izvadak na prijavljenu adresu elektroničke pošte (e-mail).</p>
+                </div>
+                <div>
+                    <h3>5. Što je Premium Rewards program?</h3>
+                    <p>Korisnici Premium Visa kartica mogu sudjelovati u Premium Rewardsu - najjačem programu nagrađivanja njihove vjernosti. Premium Rewards program nagrađuje potrošnju i uredno plaćanja računa korisnika Premium Visa kartica. Nagradni bodovi se sakupljaju korištenjem kartica u svakoj prilici, a svaka potrošena i uredno plaćena kuna nagrađuje se 1 bodom. Prikupljeni bodovi mogu se zamijeniti za darove. Premium Rewards bodovi ne zastarijevaju.  Više saznajte na <a href="https://www.premiumrewards.hr/hr/naslovnica/" target="_blank">www.premiumrewards.hr </a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <article>
+        <div class="container">
+            <h2 class="white-space main-color-text text-container center-text">Život bez gotovine svjetski je trend, a mi smo otkrili i neke dodatne pogodnosti</h2>
+            <img src="<?php echo $directory; ?>img/2.jpg" alt="Kartica u ruci" class="responsive no-horizontal-padding padding50"/>
+            <h3 class="white-space text-container">Svjetska istraživanja pokazuju kako bi uskoro plaćanje gotovinom mogla biti stvar prošlosti</h3> 
+            <p class="text-container">Moderni načini plaćanja, online shopping, digitalni život... prema procjenama stručnjaka do 2022. gotovo bi u potpunosti mogli istisnuti gotovinu. U nekim zemljama, poput Švedske plaćanje gotovinom je svedeno na minimum – prošle godine plaćanja kešom iznosila su tek 13% ukupnih plaćanja u toj zemlji. Prema podacima znanstvenika na institutu Royal Institute of Technology u Stockholmu, tijekom idućih pet godina plaćanje papirnatim novcem moglo bi postati stvar prošlosti. Slično će se dogoditi i u Hrvatskoj, stoga je važno imati pouzdanu, provjerenu i sigurnu karticu s nizom pogodnosti kakva je Premium Visa kartica PBZ Carda.</p>
+            <h3 class="white-space text-container">Najbolja prihvaćenost</h3>
+            <p class="text-container">Naime, Visa je jedan od najsnažnijih kartičnih brendova u svijetu s naboljom prihvaćenošću. To ukratko znači da potpuno možete biti opušteni bez obzira koristite li je u Hrvatskoj ili bilo gdje u svijetu. Jer Visa karticu prihvaća više od 200 država te se može koristiti na 54 milijuna mjesta i 2,7 mlijuna bankomata. Tako da s Premium Visa karticom PBZ Carda vrlo jednostavno možete doći do gotovine bez obzira u kojem kutku svijeta se nalazili.</p>
+            <p class="text-container">Osim fizičkih prodavaonica, Visu prihvaćaju i sve vodeće online trgovine. Tako da vas od predivnih dizajnerskih cipela, haljina i gadgeta s My Therese, net-a portera, Asosa, Yooxa i Amazona dijeli doslovce nekoliko klikova mišem.</p>
+            <p class="text-container"><img src="<?php echo $directory; ?>img/platinum.jpg" alt="Visa kartice" class="responsive no-horizontal-padding padding50"/></p>
+            <h3 class="white-space text-container">Dobre pogodnosti</h3>
+            <p class="text-container">Naime, istraživanja pokazuju kako korisnici kartice najviše koriste za kupnju odjeće i obuće (72%), hranu i piće (62 %). Kreditna kartica se iz novčanika najčešće vadi još i za kupovinu goriva na benzinskim postajama (53%) te kupnju elektronike (20%). Premium Visa kartica PBZ Carda i tu je sjajna stvar jer na prodajnim mjestima Visa odabranih partnera možete dobiti dobar popust i druge pogodnosti. I još sudjelujete u programu nagrađivanja u kojem se nagrađuje potrošnja i uredno plaćanje računa.</p>
+            <h3 class="white-space text-container">Kupovina na rate i krediti</h3>
+            <p class="text-container">Mnoge gore navedene stvari ponekad znaju poprilično i koštati pa ne čudi da Hrvati vole kupovati na rate, prvenstveno za iznose veće od 500 kuna. Na rate se najčešće kupuju skuplje stvari, a Premium Visa kartica im to olakšava i to vrlo konkretno – s mogućnošću otplate na 24 rate bez kamata i naknada. A kada, recimo, trebate nešto veću sumu novca i to hitno, Premium Visa kartica PBZ Carda je sve što vam treba. Ona je najkraći put do PBZ potrošačkog kredita. </p>
+            <h3 class="white-space text-container">Gotovina za sitnu kupnju </h3>
+            <p class="text-container">Život bez gotovine je zapravo svjetski trend koji su Hrvati objeručke prihvatili. Sada se gotovina koristi samo za tzv. sitnu kupnju do 100 kuna, a ostalo se plaća karticama. To je sve zahvaljujući mnogobrojnim pogodnostima koje kartice imaju. Tako, primjerice, uz korištenje Premium Visa kartica PBZ Carda dobivate mnoge pogodnosti kao što su otplata na rate, potrošački kredit, program nagrađivanja, podizanje gotovine u Hrvatskoj i svijetu, osiguranje od posljedica nesretnog slučaja i pakete putnog osiguranja.</p>
+        </div>
+    </article>
+    <script src="<?php echo $directory; ?>js/tilt.jquery.js"></script>
+    <script>
+        $(document).ready(function(){
+        $('.slick').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: false
+            });
+            $('.js-tilt').tilt({
+                glare: true,
+                maxGlare: .3,
+                maxTilt: 20,
+                perspective: 1000
+            })
+        });
+    </script>
+<footer>
+        <div class="container flex">
+            <div class="third center">
+                <a href="https://www.pbzcard.hr/hr/naslovnica/" target="_blank">
+                    <img src="<?php echo $directory; ?>img/pbzcard.jpg" alt="partner" class="partner-logo">
+                </a>
+            </div>
+            <p class="third center-text center">
+                Sadržaj omogućio PBZ Card u suradnji s TG Studijem za native marketing.
+            </p>
+            <div class="third center">
+            <a href="https://www.telegramgrupa.hr" target="_blank">
+            <img id="tgstudio" src="<?php echo $directory; ?>img/tg_studio.png" height="98" class="responsive"/>
+            </a>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
+
+</html>
