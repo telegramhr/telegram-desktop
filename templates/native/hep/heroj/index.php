bfd6514f50ca66bb7da87e1b7e6da36adc85eda3
Marijan Mikolić <marijan@telegramgrupa.hr>
2019-12-02 12:32:29 +0100

    HEP heroj native

---
+++ templates/native/hep/heroj/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$template_url = get_theme_root_uri() . '/telegram-desktop';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap&subset=latin-ext" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo esc_url( $template_url ) ?>/templates/native/hep/heroj/tmg_framework.css?ver=1.1" type="text/css" />
+    <link rel="stylesheet" href="<?php echo esc_url( $template_url ) ?>/templates/native/hep/heroj/style.css?ver=1.1" type="text/css" />
+</head>
+<div class="main-container barracuda">
+    <img class="animate" src="https://www.telegram.hr/wp-content/uploads/2019/12/hep_heroji_header.png" alt="Header vizual heroji">
+    <div class="full center hero">
+        <div>
+            <h1 class="full center-text">Biti heroj na radnom mjestu </h1>
+            <h2 class="full center-text" data-wow-delay="0.5s">Nesebični podvizi radnika HEP-a</h2>
+        </div>
+    </div>
+    <a href="https://www.telegram.hr/partneri/za-hep-ovu-radnicu-sanju-beslic-briga-o-napustenim-zivotinjama-obiteljska-je-misija/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img class="animate" src="https://www.telegram.hr/wp-content/uploads/2019/09/Slika-2-Sanja-Be%C5%A1li%C4%87-840x530.jpg" alt="Za HEP-ovu radnicu Sanju Bešlić briga o napuštenim životinjama obiteljska je misija ">
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Za HEP-ovu radnicu Sanju Bešlić briga o napuštenim životinjama obiteljska je misija </h1>
+                <p>Zajedno sa Sanjom, u osječkom azilu za napuštene životinje "Pobjeda", volontiraju i njezin sin i suprug. To ih čini sretnijim ljudima</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+
+    <a href="https://www.telegram.hr/partneri/kakav-heroj-kad-mu-zavrsi-radno-vrijeme-ovaj-hep-ov-elektromonter-u-hgss-u-spasava-ljude-i-daruje-krv/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Kakav heroj. Kad mu završi radno vrijeme, ovaj HEP-ov elektromonter u HGSS-u spašava ljude i daruje krv </h1>
+                <p>Anton Vukičević voli svoj posao elektromontera u HEP-u, no angažman u HGSS-u i darivanje krvi ga ispunjavaju i oplemenjuju </p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img class="animate" src="https://www.telegram.hr/wp-content/uploads/2019/09/1Anton-Vuki%C4%8Devi%C4%87-darivatelj-krvi-Crveni-kri%C5%BE-840x530.jpg" alt="Kakav heroj. Kad mu završi radno vrijeme, ovaj HEP-ov elektromonter u HGSS-u spašava ljude i daruje krv ">
+        </div>
+    </a>
+    <a href="https://www.telegram.hr/partneri/gordana-nikolic-jedina-je-zena-koja-u-hep-ovoj-toplani-radi-kao-voda-bloka-taj-zahtjevan-posao-radi-vec-nekoliko-godina-i-uziva-u-njemu/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img class="animate" src="https://www.telegram.hr/wp-content/uploads/2019/09/Slika-zaslona-2019-09-24-u-19.48.01-840x530.png" alt="Gordana Nikolić jedina je žena koja u HEP-ovoj Toplani radi kao vođa bloka. Taj zahtjevan posao radi već nekoliko godina i uživa u njemu ">
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Gordana Nikolić jedina je žena koja u HEP-ovoj Toplani radi kao vođa bloka. Taj zahtjevan posao radi već nekoliko godina i uživa u njemu </h1>
+                <p>Popričali smo s Gordanom Nikolić, strojarskom tehničarkom koja radi izazovan posao u Elektrani toplani Zagreb </p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <div class="native-signature wow fadeInUp">
+        <div class="container flex center">
+            <img class="tg-studio" src="<?php echo esc_url( $template_url ) ?>/templates/native/hep/heroj/img/logos.png" alt="TG Studio i HEP" />
+            <p class="center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom HEP i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
