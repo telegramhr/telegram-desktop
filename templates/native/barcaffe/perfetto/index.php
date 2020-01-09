69c5d731e2d89091892a7120e54e6f074e52b09e
Marko Banušić <mbanusic@gmail.com>
2020-01-09 16:11:17 +0100

    Barcaffe native

---
+++ templates/native/barcaffe/perfetto/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$template_url = get_theme_root_uri() . '/telegram-desktop';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap&subset=latin-ext" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo esc_url( $template_url ) ?>/templates/native/barcaffe/perfetto/tmg_framework.css?ver=1.1" type="text/css" />
+    <link rel="stylesheet" href="<?php echo esc_url( $template_url ) ?>/templates/native/barcaffe/perfetto/style.css?ver=1.1" type="text/css" />
+</head>
+<div class="main-container barracuda">
+    <video preload="auto" autoplay="" muted="" loop="" class="native-hero-background desktop-only" src="<?php echo get_stylesheet_directory_uri() ?>/templates/native/barcaffe/perfetto/img/barcaffe.mp4"  type="video/mp4" alt="" ></video>
+    <video preload="auto" autoplay="" muted="" loop="" class="native-hero-background mobile-only" src="<?php echo get_stylesheet_directory_uri() ?>/templates/native/barcaffe/perfetto/img/barcaffe.mp4" alt="" ></video>
+    <div class="full center hero">
+        <div>
+            <h1 class="full center-text">Savršen dan uz savršenu kavu</h1>
+            <h2 class="full center-text" data-wow-delay="0.5s">Otkrij svoj  Barcaffè Perfetto omiljeni okus  </h2>
+        </div>
+    </div>
+    <a href="https://www.telegram.hr/partneri/radni-dan-mnogima-pa-tako-i-nama-pretrpan-je-raznim-obavezama-i-dogovorima-ali-uvijek-imamo-vremena-za-kavu/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img class="animate" src="https://www.telegram.hr/wp-content/uploads/2019/12/1-2-840x530.jpg" alt="#">
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Radni dan mnogima, pa tako i nama, pretrpan je raznim obavezama i dogovorima. Ali uvijek imamo vremena za kavu</h1>
+                <p>Novinari,  web dizajneri, social manageri, urednici  Telegram media grupe početak radnog dana ne mogu zamisliti bez kave.</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <a href="https://www.telegram.hr/partneri/bez-nekih-stvari-zivot-je-gotovo-nezamisliv-kava-je-cini-se-jedna-od-njih/" target="_blank" class="container flex article-container">
+        <div class="half wow fadeInUp flex-responsive article-copy center">
+            <div class="single-head">
+                <h1 class="title">Bez nekih stvari život je gotovo nezamisliv. Kava je, čini se, jedna od njih</h1>
+                <p>Pitali smo ljude u gradu mogu li zamisliti dan bez kave, kakvu kavu najradije piju i koliko dugo traje omiljeni ritual ispijanja</p>
+                <div class="full">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+        <div class="half wow fadeInUp flex-responsive article-image center">
+            <img class="animate" src="https://www.telegram.hr/wp-content/uploads/2019/12/Barcaffe_VOX-840x530.jpg" alt="#">
+        </div>
+    </a>
+
+    <div class="gray-bg padding25 no-vertical-padding">
+        <div class="container">
+            <div class="white-space full center-text">
+                <img src="<?php echo esc_url( $template_url ) ?>/templates/native/barcaffe/perfetto/img/barcaffe.jpg" alt="Infografika"> 
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <div class="native-signature wow fadeInUp">
+        <div class="container flex center">
+            <img class="tg-studio" src="<?php echo esc_url( $template_url ) ?>/templates/native/barcaffe/perfetto/img/logos.png" alt="TG Studio i Barcaffe" />
+            <p class="center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom Barcaffe i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    (_gdeaq = window._gdeaq || []).push(['nc', 'false']);
+</script>
+<script type="text/javascript">
+    (_gdeaq = window._gdeaq || []).push(['cmp', false]);
+</script>
+<i id="_gde_isndwl_zirrmvnlnb" style="display: none !important; visibility: hidden !important"></i>
+<script type="text/javascript">
+    (_gdeaq = window._gdeaq || []).push(['hit', 'gdehr', 'F0nwZ6tW18cEwiKZhTV5xpUlsv7tFTIlL9WphzBkE5D.j7', 'mikljiehkg', 'mwyciiwjmpnyljawbknysmjfwond'],
+        ['viewable', 'gdehr', 'F0nwZ6tW18cEwiKZhTV5xpUlsv7tFTIlL9WphzBkE5D.j7', 'zirrmvnlnb', '_gde_isndwl_zirrmvnlnb']);
+</script>
+<script type="text/javascript" src="https://gdehr.hit.gemius.pl/gdejs/xgde.js"> </script>
