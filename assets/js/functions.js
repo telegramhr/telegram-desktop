fa748ce538136707814f33f12a81c47baa4e1c4f
Marko Banušić <mbanusic@gmail.com>
2020-01-22 10:41:44 +0100

    new tag

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -155,10 +155,10 @@ function outstream_load() {
             outstream: true,
             adscheduleid: '00000000',
             autoplayadsmuted: true,
-            client: 'googima',
+            client: 'vast',
             tag: [
-                'https://pubads.g.doubleclick.net/gampad/ads?iu=/1092744/telegram/telegram_video_v1&description_url=https%3A%2F%2Fwww.telegram.hr%2Fvideo&tfcd=0&npa=0&sz=300x250%7C660x350&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator='
-            ]
+                'https://tpc.googlesyndication.com/ima3vpaid?vad_format=linear&correlator=&adtagurl=https%3A%2F%2Fpubads.g.doubleclick.net%2Fgampad%2Fads%3Fiu%3D%2F1092744%2Ftelegram%2Ftelegram_video_v1%26description_url%3Dhttps%253A%252F%252Fwww.telegram.hr%252Fvideo%26tfcd%3D0%26npa%3D0%26sz%3D300x250%257C400x300%257C660x350%26gdfp_req%3D1%26output%3Dvast%26unviewed_position_start%3D1%26env%3Dvp%26vpos%3Dpreroll%26vpmute%3D1%26vpa%3D1%26type%3Djs%26nofb%3D1'
+                ]
         },
         displayHeading: true,
         intl: {
