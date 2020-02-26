59cfdddaa958638f0b8f235656d86973ece12c21
Marko Banušić <mbanusic@gmail.com>
2020-02-26 12:08:49 +0100

    Ibudolor native

---
+++ templates/native/ibudolor/udarite_bol/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$template_url = get_theme_root_uri() . '/telegram2-desktop';
+$template_url = get_theme_root_uri() . '/telegram-desktop';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700|Raleway:400,700,900&display=swap&subset=latin-ext" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/tmg_framework.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/style.css?ver=1.1" type="text/css" />
+    <link rel="stylesheet" href="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/aos.css" type="text/css" />
+</head>
+<div class="main-container barracuda">
+    <div class="native-header full">
+        <div class="native-title full">
+            <div data-aos="fade-left" data-aos-duration="750" data-aos-delay="750">
+                <h1>Udarite bol direktno</h1>
+            </div>
+        </div>
+        <video preload="auto" autoplay="" muted="" loop="">
+            <source src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibulodor_header.webm" type="video/webm">
+            <source src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibulodor_header.mp4" type="video/mp4">
+        </video>
+    </div>
+    <article class="full flex" data-aos="fade-right" data-aos-duration="500" data-aos-delay="1000">
+        <h2>Sve što morate znati o boli</h2>
+        <p>Iako je doživljavamo kao nešto neugodno i nepoželjno, bol je vrlo važna jer nas upozorava, a na taj način i štiti od mogućih ozljeda i oštećenja. Ona ukazuje i na neke moguće bolesti ili bolesna stanja te nas ”tjera” da odemo liječniku i da se liječimo.</p>
+        <p>Bol je, premda svima poznat, neugodan individualni osjećaj koji je vrlo teško precizno definirati. Pojavljuje se u najrazličitijim oblicima i intenzitetima i ovisi o osobnom doživljaju onoga kod koga se javi. Kaže se da se s boli rađamo, živimo i umiremo. Bol nastaje zbog fizičkog oštećenja tkiva ili kao upozorenje na moguće oštećenje, ali i psihičke reakcije na to oštećenje.</p>
+        <h3>Ne zanemarujte bol</h3>
+        <p>Osim na moguća mehanička ozljeđivanja, koja nam prijete iz okoline, bol ukazuje na neke moguće bolesti ili bolesna stanja i potiče nas na njihovo otkrivanje i liječenje. „Mana“ mnogih teških bolesti, često je izostanak boli pa takve budu znatno teže prepoznate, a kada se i otkriju, slabija je motivacija za njihovo liječenje. Bolesti kao što su, primjerice, povišeni krvni tlak, ne izazivaju jaču bol te je znatno teže liječiti hipertenziju, negoli, primjerice, križobolju, zbog koje će se redovito uzimati lijekove, dolaziti na injekcije, ići na fizikalnu terapiju, vježbati i prilagođavati način života. Sve se to znatno teže postiže kod često puno opasnijih bolesti koje nisu praćene osjećajem boli.</p>
+        <div class="infographic flex">
+            <h3 class="full">4 prednosti Ibudolora</h3>
+            <div class="flex half flex-responsive">
+                <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ikonica_2.png" alt="Ikonica prekrižene vode by Font Awesome">
+                <p class="full center-text">Jedini ibuprofenski prašak koji se uzima bez tekućine</p>
+            </div>
+            <div class="flex half flex-responsive">
+                <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ikonica_5.png" alt="Ikonica limuna by Font Awesome">
+                <p class="full center-text">Ugodnog okusa limuna</p>
+            </div>
+            <div class="flex half flex-responsive">
+                <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ikonica_3.png" alt="Ikonica termometra by Font Awesome">
+                <p class="full center-text">Djeluje protiv blagih do umjerenih bolova i vrućice</p>
+            </div>
+            <div class="flex half flex-responsive">
+                <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ikonica_4.png" alt="Ikonica sata by Font Awesome">
+                <p class="full center-text">Uzima se u razmaku od najmanje 6 sati između dvije primjene</p>
+            </div>
+            <p class="disclaimer">Prije upotrebe pažljivo pročitajte uputu o lijeku, a o rizicima i nuspojavama upitajte svog liječnika ili ljekarnika.</p>
+        </div>
+        <h3>Ne doživljavamo bol svi jednako</h3>
+        <p>Kod sposobnosti podnošenja boli postoje znatne individualne razlike pa će pojedinci različito reagirati na isti stupanj boli, a dodatno će na to utjecati i situacija i trenutno raspoloženje. Prema trajanju, bol se dijeli na akutnu (kratkotrajnu) i kroničnu (dugotrajnu). </p>
+        <h3>Putevi boli</h3>
+        <p>Bol počinje na posebnim receptorima za bol koji se nalaze svuda po tijelu. Kada su izloženi nekom bolnom podražaju, oni putem živaca prenose električne impulse u leđnu moždinu i dalje prema mozgu. Tijekom tog puta ti se impulsi obrađuju, stvarajući osjećaj boli, ali i pokreću određene reakcije kojima se nastoji izbjeći ili umanjiti bol, primjerice odmicanje ruke od vrućeg predmeta. Receptori za bol i njihovi živčani putovi razlikuju se u različitim dijelovima tijela pa je zbog toga osjet boli različit u odnosu na vrstu i mjesto pojave.</p>
+        <h3>Bol nije bolest, ali može dovesti do bolesti</h3>
+        <p>Neliječena akutna bol može prijeći u kroničnu bol što smanjuje kvalitetu života i može dovesti do psiholoških, imunoloških, te patofizioloških promjena u funkcijama tjelesnih sustava. Za bolesnika kronifikacija znači da bol prodire u sve sfere života i loše utječe na mentalnu, kognitivnu i psihosocijalnu funkcionalnost.</p>
+        <h3>Važan je oprez</h3>
+        <p>Iako se propagira apsolutno pravo na otklanjanje boli, treba kod toga biti vrlo oprezan i svako liječenje provoditi u suradnji s liječnikom. Neracionalna i pretjerana upotreba analgetika može dovesti do niza nuspojava, kao što su navikavanje, oštećenje jetre ili krvarenje u probavnom sustavu. Osim toga, uzimanje analgetika prije jasno utvrđene dijagnoze može dovesti do prikrivanja kliničke slike i životne ugroženosti (primjerice, upala slijepog crijeva).</p>
+        <h3>Kako se liječi bol?</h3>
+        <p>Najčešće se primjenjuju lijekovi protiv bolova: ibruprofen, paracetamol, acetil salicilna kiselina i ostali nesteroidni antireumatici, a u težim slučajevi opioidni analgetici. Uz to se koriste fizikalna terapija, kirurški zahvati, ali i neke tradicionalne metode kao što su akupunktura, akupresura, kiropraktika… </p>
+        <!--<img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/vrste_boli.png" alt="Ilustracije boli" />-->
+    </article>
+    <article class="full flex" data-aos="fade-right" data-aos-duration="500" data-aos-delay="1000">
+        <h2>Kada odabrati Ibudolor Direkt</h2>
+        <p>Ibudolor Direkt je lijek protiv blagih do umjerenih bolova i povišene tjelesne temperature. Sadrži ibuprofen u obliku praška, ugodnog okusa limuna koji se uzima bez tekućine direktno u usta. Dostupan je u dozama od 200 mg i 400 mg. Pakiranje sadrži 10 pojedinačnih vrećica.</p>
+        <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/packshot.jpg" alt="Pakiranja Ibudolor Direkta" />
+        <h3>Kako se primjenjuje Ibudolor Direkt?</h3>
+        <p>Sadržaj vrećice isprazni se u usta i proguta sa slinom. Nije potreban dodatak bilo kakve tekućine.</p>
+        <h3>Tko može koristiti Ibudolor Direkt?</h3>
+        <p>Ibudolor Direkt 200 mg mogu koristiti odrasli, adolescenti i djeca od 20 kg tjelesne težine (od 6 godina i stariji).</p>
+        <p>Ibudolor Direkt 400 mg mogu koristiti odrasli i adolescenti od 40 kg tjelesne težine (od 12 godina i stariji).</p>
+    </article>
+    <article class="full flex" data-aos="fade-right" data-aos-duration="500" data-aos-delay="1000">
+        <h2>Kada posegnuti za lijekovima protiv bolova</h2>
+        <p>Najčešća bolna stanja zbog kojih posežemo za praškom protiv bolova su glavobolja i migrena, bolovi za vrijeme menstruacije i PMS-a, zubobolja, povišena tjelesna temperatura. Svi navedeni bolovi jednostavno se mogu ublažiti pravilnom primjenom Ibudolor Direkta.</p>
+        <div class="full flex interactive-segment">
+            <p class="full center-text">Interaktivna ilustracija - Klikom otkrijte vrstu boli kod koje Ibudolor može pomoći</p>
+            <details class="fourth flex-responsive animate">
+                <summary>
+                    <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibudolor_migrena.png" alt="Ilustracija boli" />
+                </summary>
+                <p class="center-text">Glavobolje i migrene</p>
+            </details>
+            <details class="fourth flex-responsive animate">
+                <summary>
+                    <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibudolor_pms.png" alt="Ilustracija boli" />
+                </summary>
+                <p class="center-text">Bolovi za vrijeme menstruacije i PMS-a</p>
+            </details>
+            <details class="fourth flex-responsive animate">
+                <summary>
+                    <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibudolor_ledja.png" alt="Ilustracija boli" />
+                </summary>
+                <p class="center-text">Bolovi u leđima</p>
+            </details>
+            <details class="fourth flex-responsive animate">
+                <summary>
+                    <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibudolor_zubobolja.png" alt="Ilustracija boli" />
+                </summary>
+                <p class="center-text">Zubobolja</p>
+            </details>
+        </div>
+        <h3>95 posto žena pati od menstrualnih bolova</h3>
+        <p>Od menstrualnih bolova, primjerice, pati čak 95 posto žena i iako su simptomi najčešće tek osjetni, ponekad mogu biti vrlo jaki te osim bolnog trbuha i leđa mogu izazvati glavobolje, mučnine, proljev i povraćanje. Želite li se boli riješiti brzo i jednostavno, posegnite za  praškom protiv bolova Ibudolor, a važno je ne čekati stvarno jaku bol i grčeve pri kojima se ne možete pomaknuti, već popiti analgetik čim bolovi počnu.</p>
+        <h3>Kako protiv menstrualnih bolova </h3>
+        <p>Lijekove prve linije čine nesteroidni protuupalni antireumatici. Randomizirane studije pokazuju kako 70 do 90% žena navodi značajno smanjenje bolova na terapiju ibuprofenom u dozi od 400-600 mg svakih 4 do 6 sati. Ibudolor Direkt, jedini ibruprofenski prašak, uzima se bez tekućine i djeluje i do šest sati. Terapiju je najbolje započeti na dan početka menstruacije i nastavlja sve do prestanka krvarenja, odnosno bolova. Kod izraženije kliničke slike početak terapije može se pomaknuti i na dan-dva prije očekivanog početka krvarenja, savjetuju liječnici.</p>
+        <h3>Kad naraste temperatura...</h3>
+        <p>Čest razlog zašto se poseže za Ibudolorom je i povišena tjelesna temperatura. Tjelesna temperatura se povisi najčešće kao odgovor organizma na infekcije virusima ili bakterijama, a prati je glavobolja, drhtavica, znojenje, gubitak apetita itd. Lijekove koji snižavaju tjelesnu temperaturu treba primijeniti kod vrijednosti tjelesne temperature iznad 38,5°C, a ukoliko povišena temperatura traje više od tri dana i ne može se sniziti lijekovima i fiziološkim mjerama, potrebno je potražiti pomoć liječnika.</p>
+    </article>
+    <div class="container flex center">
+        <p class="full disclaimer">Plaćeni oglas o lijeku. IBUD-AD-018-14022020</p>
+    </div>
+    <div class="native-signature" data-aos="fade-up" data-aos-duration="500" data-aos-delay="250">
+        <div class="container flex center">
+            <img class="footer-logos" src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/logos.png" alt="TG Studio i HEP" />
+            <p class="center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom Stada i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
+<script src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/aos.js"></script>
+<script>
+    AOS.init({
+        easing: 'ease-out',
+        once: false,
+        mirror: true
+    });
+</script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ templates/native/ibudolor/udarite_bol/index_rustical.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$template_url = get_theme_root_uri() . '/telegram2-desktop';
+$template_url = get_theme_root_uri() . '/telegram-desktop';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Lora&display=swap&subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/tmg_framework.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/style.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/aos.css" type="text/css" />
+</head>
+<div class="main-container barracuda">
+    <img class="desktop-only" src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibulodor_header.png" alt="Udarite bol direktno - Odaberite Ibudolor i popijte ga bez tekućine!" />
+    <img class="mobile-only" src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/ibulodor_header_mobile.png" alt="Udarite bol direktno - Odaberite Ibudolor i popijte ga bez tekućine!"/>
+    <article class="full flex">
+        <h2 class="flex" data-aos="fade-right" data-aos-duration="500" data-aos-delay="250"><img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/rustical_deco.png" alt="Dekorativni dezen" /><span>Sve što morate znati o boli</span><img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/rustical_deco.png" alt="Dekorativni dezen" /></h2>
+        <p>Iako je doživljavamo kao nešto neugodno i nepoželjno, bol je vrlo važna jer nas upozorava, a na taj način i štiti od mogućih ozljeda i oštećenja. Ona ukazuje i na neke moguće bolesti ili bolesna stanja te nas ”tjera” da odemo liječniku i da se liječimo.</p>
+        <p>Bol je, premda svima poznat, neugodan individualni osjećaj, vrlo teško precizno definirati. Pojavljuje se u najrazličitijim oblicima i intenzitetima i ovisi o osobnom doživljaju onoga kod koga se javi. Kaže se da se s boli rađamo, živimo i umiremo. Bol nastaje zbog fizičkog oštećenja tkiva ili kao upozorenje na moguće oštećenje, ali i psihičke reakcije na to oštećenje.</p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Ne zanemarujte bol</h3>
+        <p>Osim na moguća mehanička ozljeđivanja, koja nam prijete iz okoline, bol ukazuje na neke moguće bolesti ili bolesna stanja i potiče nas na njihovo otkrivanje i liječenje. „Mana“ mnogih teških bolesti, često je izostanak boli pa takve budu znatno teže prepoznate, a kada se i otkriju, slabija je motivacija za njihovo liječenje. Bolesti kao što su, primjerice, povišeni krvni tlak, ne izazivaju jaču bol te je znatno teže liječiti hipertenziju, negoli, primjerice, križobolju, zbog koje će se redovito uzimati lijekove, dolaziti na injekcije, ići na fizikalnu terapiju, vježbati i prilagođavati način života. Sve se to znatno teže postiže kod često puno opasnijih bolesti koje nisu praćene osjećajem boli.</p>
+        <img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/pain_drawings.jpg" alt="Ilustracije boli" />
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Ne doživljavamo bol svi jednako</h3>
+        <p>Kod sposobnosti podnošenja boli postoje znatne individualne razlike pa će pojedinci različito reagirati na isti stupanj boli, a dodatno će na to utjecati i situacija i trenutno raspoloženje.  Prema trajanju, bol se dijeli na akutnu (kratkotrajnu) i kroničnu (dugotrajnu). </p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Bol nije bolest</h3>
+        <p>Moderna medicina smatra da je odsutnost boli jedno od osnovnih ljudskih prava, stoga bi liječenje boli trebalo biti u stalnom fokusu. Pri tome se ne smije smetnuti s uma da bol nije bolest nego simptom neke bolesti ili bolesnog stanja. Zato je prvi korak u liječenju otkrivanje osnovnog uzroka koji je doveo do pojave boli, a liječenje osnovne bolesti je najracionalniji i najučinkovitiji način uklanjana boli. </p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Važan je oprez</h3>
+        <p>Iako se propagira apsolutno pravo na otklanjanje boli, treba kod toga biti vrlo oprezan i svako liječenje provoditi u suradnji s liječnikom. Neracionalna i pretjerana upotreba analgetika može dovesti do niza nuspojava, kao što su navikavanje, oštećenje jetre ili krvarenje u probavnom sustavu. Osim toga, uzimanje analgetika prije jasno utvrđene dijagnoze može dovesti do prikrivanja kliničke slike i životne ugroženosti (primjerice, upala slijepog crijeva).</p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Različiti uzroci i vrste boli</h3>
+        <p>Najčešće se primjenjuju lijekovi protiv bolova:  ibruprofen, paracetamol, acetil salicilna kiselina i ostali nesteroidni antireumatici, a u težim slučajevi opioidni analgetici. Uz to se koriste fizikalna  terapija, kirurški zahvati, ali i neke tradicionalne metode kao što su akupunktura, akupresura, kiropraktika… </p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Putevi boli</h3>
+        <p>Bol počinje na posebnim receptorima za bol koji se nalaze svuda po tijelu. Kada su izloženi nekom bolnom podražaju, oni putem živaca prenose električne impulse u leđnu moždinu i dalje prema mozgu. Tijekom tog puta ti se impulsi obrađuju, stvarajući osjećaj boli, ali i pokreću određene reakcije kojima se nastoji izbjeći ili umanjiti bol, primjerice odmicanje ruke od vrućeg predmeta. Receptori za bol i njihovi nervni putovi razlikuju se u različitim dijelovima tijela pa je zbog toga osjet boli različit u odnosu na vrstu i mjesto pojave.</p>
+    </article>
+    <article>
+        <h2 class="flex" data-aos="fade-right" data-aos-duration="500" data-aos-delay="250"><img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/rustical_deco.png" alt="Dekorativni dezen" /><span>Kada odabrati Ibudolor Direkt</span><img src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/rustical_deco.png" alt="Dekorativni dezen" /></h2>
+        <p>Ibudolor Direkt je lijek protiv blagih do umjerenih bolova i povišene tjelesne temperature. Sadrži ibuprofen u obliku praška, ugodnog okusa limuna koji se uzima bez tekućine direktno u usta. Dostupan je u dozama od 200 mg i 400 mg. Pakiranje sadrži 10 pojedinačnih vrećica.</p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Kako se primjenjuje Ibudolor Direkt?</h3>
+        <p>Sadržaj vrećice isprazni se u usta i proguta sa slinom. Nije potreban dodatak bilo kakve tekućine.</p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">Tko može koristiti Ibudolor Direkt?</h3>
+        <p>Ibudolor Direkt 200 mg mogu koristiti odrasli, adolescenti i djeca od 20 kg tjelesne težine (od 6 godina i stariji).</p>
+        <p>Ibudolor Direkt 400 mg mogu koristiti odrasli i adolescenti od 40 kg tjelesne težine (od 12 godina i stariji).</p>
+        <h3 data-aos="fade-right" data-aos-duration="500" data-aos-delay="250">4 prednosti Ibudolora</h3>
+        <p>Jedini ibuprofenski prašak</p>
+        <p>Uzima se bez tekućine</p>
+        <p><i class="fas fa-thermometer-three-quarters"></i>Djeluje protiv blagih do umjerenih bolova i vrućice</p>
+        <p>Uzima se u razmaku od najmanje 6 sati između dvije primjene</p>
+    </article>
+    <div class="native-signature" data-aos="fade-up" data-aos-duration="500" data-aos-delay="250">
+        <div class="container flex center">
+            <img class="footer-logos" src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/img/logos.png" alt="TG Studio i HEP" />
+            <p class="center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom HEP i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
+<script src="<?php echo esc_url($template_url) ?>/templates/native/ibudolor/udarite_bol/aos.js"></script>
+<script>
+    AOS.init({
+        easing: 'ease-out',
+        once: false,
+        mirror: true
+    });
+</script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
