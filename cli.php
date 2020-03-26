6c1262f2e521d9368120367b0db559f51dec1af2
Marko Banušić <mbanusic@gmail.com>
2020-03-26 14:11:19 +0100

    fix

--- cli.php
+++ cli.php
@@ -197,7 +197,8 @@ class Telegram_Command extends WP_CLI_Command {
 		update_option('tmg_covid_total_yesterday', $total);
 		update_option('tmg_covid_croatia_yesterday', $croatia);
 
-		$file = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
+		//$file = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
+		$file = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
 		$fileData=fopen($file,'r');
 		$days = [];
 		$header = fgetcsv($fileData);
