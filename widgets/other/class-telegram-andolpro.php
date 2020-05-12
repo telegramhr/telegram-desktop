562eb2ce04d5e0c139848f5c140f220065fb3d0e
Marko Banušić <mbanusic@gmail.com>
2020-05-12 16:55:14 +0200

    andol fix

--- widgets/other/class-telegram-andolpro.php
+++ widgets/other/class-telegram-andolpro.php
@@ -15,8 +15,8 @@ class Telegram_AndolPro extends WP_Widget
     {
 	        ?>
             <div class="tg-widget rainbow-widget procitajte-danas">
-                <div class="tg-widget-head">
-                    DONOSI AndolPRO
+                <div class="tg-widget-head" style="text-transform: none;">
+                    DONOSI PlivitMagnezij
                 </div>
                 <div class="tg-widget-body">
 			        <?php
