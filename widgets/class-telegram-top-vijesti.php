71a4c5a4b84816ffc2418ffc7e8d80591a9546b7
Marko Banušić <mbanusic@gmail.com>
2020-05-26 11:02:11 +0200

    fix top vijesti

--- widgets/class-telegram-top-vijesti.php
+++ widgets/class-telegram-top-vijesti.php
@@ -13,36 +13,11 @@ class Telegram_Top_Vijesti extends WP_Widget
 
     public function widget($args, $instance)
     {  ?>
-
         <div class="tg-widget rainbow-widget top-vijesti container">
             <div class="tg-widget-body cf">
                 <?php
-                $page = 749255;
 
-                if(get_field('ukljuci', $page)) {
-                    //izbori
-	                $kandidati = get_field('kandidat', $page);
-                        ?>
-                        <article class="article-rainbow-block">
-                            <div class="thumb">
-			                    <img src="<?php echo $kandidati[0]['slika'] ?>" width="280" height="280">
-                            </div>
-                        </article>
-                    <article class="article-rainbow-block" style="width: 60%">
-                        <div class="w3-border" style="background-color: rgb(41,91,165); margin-top: 100px; height: 40px; width: 100%; border: 1px solid white;">
-                            <div style="float:left; width:<?php echo $kandidati[0]['postotak'] ?>%; padding: 3px; background-color: rgb(234,33,45); height: 38px;"><span style="color:white; line-height: 34px;"><?php echo $kandidati[0]['postotak'] ?>%</span></div>
-                            <span style="line-height: 38px; float: right; color:white;"><?php echo $kandidati[1]['postotak'] ?>%</span>
-                        </div>
-                    </article>
-                <article class="article-rainbow-block" style="float:right;">
-                    <div class="thumb">
-                        <img src="<?php echo $kandidati[1]['slika'] ?>" width="280" height="280">
-                    </div>
-                </article>
-                        <?php
-                }
-                else {
-                    $cache = wp_cache_get('top-vijesti', 'widgets');
+                    $cache = wp_cache_get('top-vijesti-1', 'widgets');
                     if (!$cache) {
                         ob_start();
 	                    $q = z_get_zone_query( 'masthead', array( 'posts_per_page' => 5 ) );
@@ -53,10 +28,9 @@ class Telegram_Top_Vijesti extends WP_Widget
 	                    }
 	                    wp_reset_postdata();
 	                    $cache = ob_get_clean();
-	                    wp_cache_set('top-vijesti', $cache, 'widgets');
+	                    wp_cache_set('top-vijesti-1', $cache, 'widgets', 600);
                     }
                     echo $cache;
-                }
                 ?>
             </div>
         </div>
