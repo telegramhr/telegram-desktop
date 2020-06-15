f77097903d1537debdb3737aa58253e5521968f7
Marko Banušić <mbanusic@gmail.com>
2020-06-15 16:27:12 +0200

    fix image data

--- functions-shared.php
+++ functions-shared.php
@@ -585,8 +585,14 @@ add_filter('wp_update_attachment_metadata', 'telegram_attachment', 10, 2);
 
 function telegram_attachment($data, $post_id) {
 	$post = get_post($post_id);
-	if ( isset($data['image_meta']['credit']) && $data['image_meta']['credit'] ) {
-		update_post_meta($post_id, 'fotograf', esc_attr($data['image_meta']['credit']));
+	if ( isset($data['image_meta']['caption']) && $data['image_meta']['caption'] ) {
+	    $fotograf = explode('Photo: ', $data['image_meta']['caption'])[1];
+	    if ($fotograf) {
+		    update_post_meta( $post_id, 'fotograf', esc_attr( $fotograf ) );
+	    }
+	    else if ( isset($data['image_meta']['credit']) && $data['image_meta']['credit'] ) {
+		    update_post_meta($post_id, 'fotograf', esc_attr($data['image_meta']['credit']));
+	    }
 	}
 	return $data;
 }
