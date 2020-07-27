2ccffee1206b7f24dc6e80eed85d79860ffae708
TMG_Marijan <marijan@telegramgrupa.hr>
2020-07-27 10:45:36 +0200

    Native Obnova doma 1.0

---
+++ templates/native/sigurnost/obnova_doma/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$native_path = get_stylesheet_directory_uri() . '/templates/native/sigurnost/obnova_doma/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css" type="text/css" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo $native_path ?>functions.js"></script>
+    <link rel="stylesheet" href="https://use.typekit.net/yjw4lwh.css">
+</head>
+<div class="main-container flex">
+    <div class="native-hero full flex relative center">
+        <a href="https://telegram.hr/" class="header-logos center full">
+            <img src="<?php echo $native_path ?>img/telegram_logo_white.svg" alt="Telegram Logo">
+        </a>
+        <div class="native-title full flex flex-responsive">
+            <h1 class="full wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">Gradimo <span class="red">temelje</span> sigurnosti</h1>
+            <h2 class="full">Vodič za obnovu i osiguranje doma</h2>
+        </div>
+        <video preload="auto" autoplay="" muted="">
+            <source src="<?php echo $native_path; ?>img/header.webm" type="video/webm">
+            <source src="<?php echo $native_path; ?>img/header.mp4" type="video/mp4">
+        </video>
+    </div>
+    <section class="full flex">
+        <div class="container flex">
+            <a class="article full flex stretch" href="https://www.telegram.hr/partneri/imamo-tri-iskustva-ljudi-kojima-su-u-potresu-osteceni-stanovi-u-velikim-su-planovima-za-obnovu/" target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/07/glavna-4-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">Imamo tri iskustva ljudi kojima su u potresu oštećeni stanovi. U velikim su planovima za obnovu</h2>
+                        <p class="full">Iako je od potresa prošlo gotovo četiri mjeseca, malo je toga obnovljeno</p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="article full flex stretch" href="https://www.telegram.hr/partneri/ako-na-godisnjem-planirate-malo-preuredivati-stan-savjeti-arhitektice-kate-marunice-ustedjet-ce-vam-brdo-vremena-i-zivaca/" target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/07/painter-2247395_1280-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">Ako na godišnjem planirate malo preuređivati stan, savjeti arhitektice Kate Marunice uštedjet će vam brdo vremena i živaca</h2>
+                        <p class="full">Jedna od osnivačica arhitektonskog studija NFO napravila nam je popis stvari koje bi nam trebale olakšati makeover doma</p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="article full flex stretch" href="https://www.telegram.hr/price/razgovor-s-cijenjenim-profesorom-kako-obnoviti-zagreb-a-da-mu-ne-ubijemo-duh-i-sprijecimo-spekulante-koji-vrebaju/" target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/03/damjanovic-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">Razgovor s profesorom: Kako obnoviti Zagreb, a da mu ne ubijemo duh i spriječimo špekulante koji vrebaju </h2>
+                        <p class="full">Profesor s Odsjeka za povijest umjetnosti, koji se Zagrebom bavio u mnogim radovima, govori o obnovi nakon potresa</p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+            <a class="article full flex stretch" href="https://www.telegram.hr/partneri/samo-20-posto-hrvata-ima-osiguranu-imovinu-i-na-dnu-smo-europske-ljestvice-potres-cini-se-mijenja-trend/" target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/07/glavna-6-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">Samo 20 posto Hrvata ima osiguranu imovinu i na dnu smo europske ljestvice. Potres, čini se, mijenja trend </h2>
+                        <p class="full">Nedavni je razorni potres mnogima uvelike promijenio život i prioritete</p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="article full flex stretch" href="https://www.telegram.hr/partneri/nakon-razornog-potresa-obnova-nekretnine-mnogima-je-najvaznija-tema-evo-kako-preurediti-tavan-u-savrsenu-oazu/" target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/07/potkrovlje.hr_velux_krovna-kucica_ebw_005-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">Nakon potresa, obnova nekretnine mnogima je najvažnija tema. Evo ideje kako preurediti tavan u savršenu oazu</h2>
+                        <p class="full">Nedavni razorni potres, najsnažniji u zadnjih 140 godina, oštetio je više od 26.000 građevina na kojima su prijavljene štete</p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="article full flex stretch" href="https://www.telegram.hr/partneri/o-zamjeni-bojlera-ovih-se-dana-mnogo-govori-imamo-savjete-koji-ce-vam-dobro-doci/ " target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/07/glavna-7-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">O zamijeni bojlera ovih se dana mnogo govori. Imamo savjete koji će vam dobro doći </h2>
+                        <p class="full">Najvažnije je dobro se pripremiti kako bi investicija bila što isplativija </p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class="article full flex stretch" href="https://www.telegram.hr/price/arhitekti-na-telegramu-koliko-bi-zagreb-manje-stradao-da-prije-potresa-nije-bio-toliko-zapusten/" target="_blank">
+                <div class="two-thirds flex-responsive">
+                    <img src="https://www.telegram.hr/wp-content/uploads/2020/03/fasade-potres-13-2-840x530.jpg" aria-hidden="true">
+                </div>
+                <div class="half flex-responsive center">
+                    <div class="article-content">
+                        <h2 class="full">Arhitekti na Telegramu: Koliko bi Zagreb manje stradao da prije potresa nije bio toliko zapušten </h2>
+                        <p class="full">O stanju u Zagrebu govore Maroje Mrduljaš, Duje Kaliterna, Otto Barić, Davor Mateković, Alan Braun te Ivo Matković, statičar iz Dubrovnika </p>
+                        <div class="full flex">
+                            <div class="cta">Pročitaj više</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </section>
+    <section class="full native-signature">
+        <div class="container flex">
+            <div class="full center">
+                <img class="partner" src="<?php echo $native_path ?>img/hpb.jpg" alt="HPB logo" />
+                <img class="partner" src="<?php echo $native_path ?>img/addiko.jpg" alt="Addiko logo" />
+                <img class="partner" src="<?php echo $native_path ?>img/grawe.jpg" alt="Grawe logo" />
+                <img class="partner" src="<?php echo $native_path ?>img/vaillant.jpg" alt="Vaillant logo" />
+                <img class="partner" src="<?php echo $native_path ?>img/velux.png" alt="Velux logo" />
+                <img class="tg-studio" src="<?php echo $native_path ?>img/tg_studio.png" alt="TG Studio logo" />
+            </div>
+            <p class="full center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerima i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </section>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
