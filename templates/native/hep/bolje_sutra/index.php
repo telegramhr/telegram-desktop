a4e7ba765f6c14e8100e26c72c08effc4b7b9699
TMG_Marijan <marijan@telegramgrupa.hr>
2020-07-28 09:33:22 +0200

    HEP - bolje sutra 1.0

---
+++ templates/native/hep/bolje_sutra/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+$native_path = 'https://www.telegram.hr/wp-content/themes/telegram2-desktop/templates/native/hep/bolje_sutra/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap&subset=latin-ext" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo $native_path; ?>tmg_framework.css?ver=1.1" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path; ?>/style.css?ver=1.2" type="text/css" />
+    <script src="https://kit.fontawesome.com/4878256e09.js" crossorigin="anonymous"></script>
+</head>
+<div class="main-container barracuda">
+    <div class="full center hero-naslov">
+        <div>
+            <h1 class="full center-text">Učinimo promjene za bolje sutra</h1>
+        </div>
+        <video preload="auto" autoplay="" muted="" loop="">
+            <source src="<?php echo $native_path; ?>img/hep_solar_header.mp4" type="video/mp4">
+        </video>
+        <i class="fas fa-angle-down"></i>
+    </div>
+    <a href="https://www.telegram.hr/partneri/projektima-velikih-suncanih-elektrana-u-hrvatskoj-hep-planira-povecati-udio-obnovljivih-izvora-energije/" target="_blank" class="article-container center relative">
+        <div class="article-images full flex">
+            <img class="animate" src="<?php echo $native_path; ?>img/hep_solar_1.jpg" aria-hidden="true">
+            <img class="animate" src="<?php echo $native_path; ?>img/hep_solar_2.jpg" aria-hidden="true">
+        </div>
+        <div class="article-copy">
+            <div class="single-head">
+                <h1 class="title full center-text">HEP će ove godine izgraditi sedam, a u suradnji s gradovima i općinama čak 11 novih sunčanih elektrana</h1>
+                <p class="full center-text">HEP će do 2030. povećati proizvodnju iz obnovljivih izvora energije za 50 posto</p>
+                <div class="full center">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <div class="container">
+        <video preload="auto" autoplay="" muted="" loop="">
+            <source src="<?php echo $native_path; ?>img/hep_solar.mp4?ver=1.3" type="video/mp4">
+        </video>
+    </div>
+
+    <a href="https://www.telegram.hr/partneri/bijele-rode-mnogima-su-omiljena-vrsta-ptica-hep-na-stupovima-svoje-mreze-brine-o-cak-1000-rodinih-gnijezda/" target="_blank" class="article-container center relative blue-boi">
+        <div class="article-images full flex">
+            <img class="animate" src="<?php echo $native_path; ?>img/hep_rode_1.jpg" aria-hidden="true">
+            <img class="animate" src="<?php echo $native_path; ?>img/hep_rode_2.jpg" aria-hidden="true">
+        </div>
+        <div class="article-copy">
+            <div class="single-head">
+                <h1 class="title full center-text">Bijele rode mnogima su omiljena vrsta ptica. HEP na stupovima svoje mreže brine o čak 1000 rodinih gnijezda</h1>
+                <p class="full center-text">HEP aktivno provodi mjere koje osiguravaju zaštitu i sigurno gniježđenje bijele rode</p>
+                <div class="full center">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <a href="https://www.telegram.hr/partneri/u-zagrebu-su-osvanula-tri-nova-murala-nasih-akademskih-umjetnika-ovoga-puta-na-hep-ovim-trafostanicama/" target="_blank" class="article-container purple-boi center relative">
+        <div class="article-images full flex">
+            <img class="animate" src="<?php echo $native_path; ?>img/hep_murali_1.jpg" aria-hidden="true">
+            <img class="animate" src="<?php echo $native_path; ?>img/hep_murali_2.jpg" aria-hidden="true">
+        </div>
+        <div class="article-copy">
+            <div class="single-head">
+                <h1 class="title full center-text">U Zagrebu su osvanula tri nova murala naših akademskih umjetnika. Ovoga puta na HEP-ovim trafostanicama</h1>
+                <p class="full center-text">Likovni umjetnici ukrasili tri zagrebačke transformatorske stanice</p>
+                <div class="full center">
+                    <div class="article-cta">Pročitaj više</div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <div class="native-signature wow fadeInUp">
+        <div class="container flex center">
+            <img class="tg-studio" src="<?php echo $native_path; ?>img/logos.png" alt="TG Studio i HEP" />
+            <p class="center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom HEP i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </div>
+</div>
