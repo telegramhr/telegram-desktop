1bcbfac73b6715f0fbb3db12df915fbb00b6ac56
Marijan Mikolić <marijan.mikolic@gmail.com>
2021-08-16 16:38:12 +0200

    Native Moje mjesto v0.0 - preview launch

---
+++ templates/native/moje_mjesto/tvoja_destinacija/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+//$native_path = get_stylesheet_directory_uri() . '/templates/native/moje_mjesto/tvoja_destinacija/';
+$native_path = 'http://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/moje_mjesto/tvoja_destinacija/';
+//$native_path = 'http://localhost/telegram-desktop/templates/native/moje_mjesto/tvoja_destinacija/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>img/aos.css?ver=1.0" type="text/css" />
+    <link rel="stylesheet" href="https://use.typekit.net/yjw4lwh.css">
+    <script src="<?php echo $native_path ?>img/aos.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css?ver=2.0" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css?ver=2.0" type="text/css" />
+    <script src="<?php echo $native_path ?>functions.js"></script>
+</head>
+<div class="main-container flex relative">
+    <header class="native-hero episode-overlay full flex relative center">
+        <video src="<?php echo $native_path ?>img/tg_videobg_dubrovnik.mp4" autoplay loop muted playsinline class="background-video"></video>
+        <a href="https://telegram.hr/" class="hero-logo mobile-only center full">
+                <img src="<?php echo $native_path ?>img/telegram_logo_white.svg" alt="Telegram Logo">
+        </a>
+        <div class="full native-title center">
+            <h1>
+                <div class="flex" data-aos="fade-left">Moje mjesto, </div>
+                <div class="flex" data-aos="fade-right" data-aos-delay="1000">tvoja destinacija</div>
+            </h1>
+        </div>
+        <a href="#o-projektu" class="mobile-only header-btn insite-btn">O projektu</a>
+        <div class="flex hero-left center">
+            <a href="https://telegram.hr/" class="hero-logo center full">
+                <img src="<?php echo $native_path ?>img/telegram_logo_white.svg" alt="Telegram Logo">
+            </a>
+            <nav class="full flex">
+                <a class="full flex" href="#o-projektu">O projektu</a>
+                <a class="full flex" href="#dubrovnik">Dubrovnik</a>
+                <a class="full flex" href="#sibenik">Šibenik</a>
+                <a class="full flex" href="#korcula">Korčula</a>
+                <a class="full flex hide" href="#zadar">Zadar</a>
+                <a class="full flex" href="#procitajte-vise">Pročitaj više</a>
+                <a class="full flex hide" href="#pozovi-nas">Pozovi nas</a>
+            </nav>
+        </div>
+        <div class="flex hero-right center">
+            <img src="<?php echo $native_path ?>img/vegetamaestro_white_square.png" alt="Vegeta Logo">
+            <img src="<?php echo $native_path ?>img/vegetanatur_white_square.png" alt="Vegeta Logo">
+            <img src="<?php echo $native_path ?>img/benussi_white_square.png" alt="Benussi Logo">
+            <img src="<?php echo $native_path ?>img/addiko_white_square.png" alt="Addiko Bank Logo">
+            <img src="<?php echo $native_path ?>img/enterprise_white_square.png" alt="Enterprise Logo">
+        </div>
+        <div class="flex hero-top">
+            <h2 data-aos="fade-down" data-aos-delay="1500">Odat ću ti zašto živim ovdje</h2>
+        </div>
+    </header>
+    <nav class="full flex mobile-nav stretch">
+        <a class="fourth center" href="#dubrovnik"><img src="<?php echo $native_path ?>img/icon_dubrovnik.png" aria-hidden="true"><span>Dubrovnik</span></a>
+        <a class="fourth center" href="#sibenik"><img src="<?php echo $native_path ?>img/icon_sibenik.png" aria-hidden="true"><span>Šibenik</span></a>
+        <a class="fourth center" href="#korcula"><img src="<?php echo $native_path ?>img/icon_korcula.png" aria-hidden="true"><span>Korčula</span></a>
+        <a class="fourth center hide" href="#zadar"><img src="<?php echo $native_path ?>img/icon_zadar.png" aria-hidden="true"><span>Zadar</span></a>
+    </nav>
+    <section class="full episode relative" id="o-projektu">
+        <div class="full flex">
+            <h2 class="full relative episode-title" data-aos="fade-left">O projektu</h2>
+            <p class="full project-intro" data-aos="fade-left" data-aos-delay="750">U dokumentarnom serijalu "Moje mjesto, tvoja destinacija" vodimo vas na ulice prekrasnih hrvatskih gradova uz lokalne vodiče - pojedince koji su odlučili ostati u svojim mjestima i graditi njihovu kulturu.</p>
+            <div class="full sugovornici flex stretch">
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="1250">
+                    <img src="<?php echo $native_path ?>img/sugovornik_ivan.jpg" aria-hidden="true">
+                    <div class="full sugovornik-episode">01</div>
+                    <h3 class="full">Ivan iz Dubrovnika</h3>
+                    <p>Ivan svojim IT rješenjima osvaja svijet. Nedavno je ušao na tržište Japana. Posjetili smo ga u Dubrovniku.</p>
+                    <a href="#dubrovnik" class="insite-btn">Pogledaj epizodu</a>
+                </div>
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="1500">
+                    <img src="<?php echo $native_path ?>img/sugovornik_danijel.jpg" aria-hidden="true">
+                    <div class="full sugovornik-episode">02</div>
+                    <h3 class="full">Danijel iz Šibenika</h3>
+                    <p>Danijel svojim umjetničkim instalacijama obogaćuje kulturnu ponudu grada i šire. Posjetili smo ga u Šibeniku.</p>
+                    <a href="#sibenik" class="insite-btn">Pogledaj epizodu</a>
+                </div>
+                <div class="third flex-responsive center" data-aos="fade-right" data-aos-delay="1750">
+                    <h2 class="full center-text">Uskoro više...</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="full episode episode-overlay flex relative stretch" id="dubrovnik">
+        <video src="<?php echo $native_path ?>img/tg_videobg_dubrovnik.mp4" autoplay loop muted playsinline class="background-video"></video>
+        <div class="half flex-responsive flex">
+            <h2 class="full episode-title" data-aos="fade-left">Moje mjesto Dubrovnik</h2>
+        </div>
+        <div class="half flex-responsive flex">
+            <p class="full episode-description" data-aos="fade-right">Na šetnju Dubrovnikom poveo nas je mladi IT-ijevac Ivan Ivušić. Nakon osam godina u Zagrebu odlučio se vratiti u svoj kraj, gdje je osnovao IT firmu koja se bavi digitalnim recenzijama. Njegovi klijenti primarno su ugostitelji iz Hrvatske pa sve do Japana. Kroz šetnju svojim gradom pokazao nam je tko su ljudi i mjesta zbog kojih voli Dubrovnik. Pokazao nam je djelić bogate gastronomske ponude i odao tajnu zašto je ostao vjeran Dubrovniku.</p>
+            <div class="insite-btn" data-aos="fade-right" style="cursor: not-allowed;" data-aos-delay="1000">Dolazi uskoro</div>
+        </div>
+    </section>
+    <section class="full episode episode-overlay flex relative stretch" id="sibenik">
+        <video src="<?php echo $native_path ?>img/tg_videobg_sibenik.mp4" autoplay loop muted playsinline class="background-video"></video>
+        <div class="half flex-responsive flex">
+            <h2 class="full episode-title" data-aos="fade-left">Moje mjesto Šibenik</h2>
+        </div>
+        <div class="half flex-responsive flex">
+            <p class="full episode-description" data-aos="fade-right">Ulicama Šibenika proveo nas je dizajner Danijel Hrg, čiji je dućan s unikatnim nakitom smješten u samom centru grada. Osim nakita, Danijel je autor impresivnih umjetničkih instalacija koje krase grad i kultni klub Azimut. S Danijelom smo posjetili Azimut i prekrasnu Tvrđavu sv. Mihovila, upoznali smo gastronomsku i kulturnu ponudu grada te na vlastitoj koži osjetili veliko gostoprimstvo Šibenčana.</p>
+            <div class="insite-btn" data-aos="fade-right" style="cursor: not-allowed;" data-aos-delay="1000">Dolazi uskoro</div>
+        </div>
+    </section>
+    <section class="full episode episode-overlay flex relative stretch" id="korcula">
+        <video src="<?php echo $native_path ?>img/tg_videobg_korcula.mp4" autoplay loop muted playsinline class="background-video"></video>
+        <div class="half flex-responsive flex">
+            <h2 class="full episode-title" data-aos="fade-left">Moje mjesto Korčula</h2>
+        </div>
+        <div class="half flex-responsive flex">
+            <p class="full episode-description" data-aos="fade-right">Dolazi uskoro...</p>
+        </div>
+    </section>
+    <section class="full episode flex relative stretch hide" id="zadar">
+        <div class="half flex-responsive flex">
+            <h2 class="full episode-title" data-aos="fade-left">Moje mjesto Zadar</h2>
+        </div>
+        <div class="half flex-responsive flex">
+            <p class="full episode-description" data-aos="fade-right">Dolazi uskoro...</p>
+        </div>
+    </section>
+    <section class="full episode relative" id="procitajte-vise">
+        <div class="full flex">
+            <h2 class="full relative episode-title" data-aos="fade-left">Pročitaj više</h2>
+            <div class="full sugovornici flex stretch">
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="1000">
+                    <img src="<?php echo $native_path ?>img/grey_placeholder.png" aria-hidden="true">
+                    <h3 class="full">Lorem Ipsum Dolor Sit Amet</h3>
+                    <p>In imperdiet mauris purus, eget volutpat lacus faucibus non. Sed in euismod orci, sit amet sodales risus.</p>
+                    <a href="#" class="insite-btn">Pročitaj više</a>
+                </div>
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="1250">
+                    <img src="<?php echo $native_path ?>img/grey_placeholder.png" aria-hidden="true">
+                    <h3 class="full">Lorem Ipsum Dolor Sit Amet</h3>
+                    <p>In imperdiet mauris purus, eget volutpat lacus faucibus non. Sed in euismod orci, sit amet sodales risus.</p>
+                    <a href="#" class="insite-btn">Pročitaj više</a>
+                </div>
+                <div class="third flex-responsive center" data-aos="fade-right" data-aos-delay="1500">
+                    <h2 class="full center-text">Uskoro više...</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="full episode flex relative stretch hide" id="pozovi-nas">
+        <div class="half flex-responsive flex relative form-parent">
+            <h2 class="full episode-title" data-aos="fade-left">Pozovi nas u svoje mjesto</h2>
+            <form id="applications" class="full flex" data-aos="fade-left" data-aos-delay="500">
+                <p class="full article-description">
+                Želimo čuti i tvoju priču. Pozovi nas u svoje mjesto, ispričaj nam sve što te veže uz tvoje mjesto, odaj nam zašto živiš tu.
+                </p>
+                <label class="full">Tvoje ime:</label>
+                <input type="text" class="full" id="ime">
+                <label class="full">Tvoj e-mail:</label>
+                <input type="email" class="full" id="email">
+                <label class="full">Što nam u svom mjestu želite pokazati i zašto:</label>
+                <textarea type="text" class="full" id="mjesto"></textarea>
+                <div class="insite-btn">Pošalji prijavu</div>
+            </form>
+        </div>
+        <div class="half flex-responsive flex desktop-only" data-aos="fade-right">
+            <img src="<?php echo $native_path ?>img/grey_placeholder.png" aria-hidden="true">
+        </div>
+    </section>
+    <footer class="full native-signature">
+        <div class="container flex">
+            <div class="full center">
+                <img src="<?php echo $native_path ?>img/vegetamaestro_square.png" alt="Vegeta Logo" data-aos="fade-up">
+                <img src="<?php echo $native_path ?>img/vegetanatur_square.png" alt="Vegeta Logo" data-aos="fade-up" data-aos-delay="500">
+                <img src="<?php echo $native_path ?>img/benussi_white_square.png" alt="Benussi Logo" data-aos="fade-up" data-aos-delay="750">
+                <img src="<?php echo $native_path ?>img/addiko_square.png" alt="Addiko Bank Logo" data-aos="fade-up" data-aos-delay="1000">
+                <img src="<?php echo $native_path ?>img/enterprise_square.png" alt="Enterprise Logo" data-aos="fade-up" data-aos-delay="1250">
+            </div>
+            <div class="full center">
+            <img class="tg-studio" src="<?php echo $native_path ?>img/tg_studio_white.svg" alt="TG Studio logo" />
+            </div>
+            <p class="full center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerima i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </div>
+    </footer>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
