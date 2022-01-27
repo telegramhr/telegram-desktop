943da5fc4b2fbe52ec502e864628afdbbcb9f858
Marko Banušić <mbanusic@gmail.com>
2022-01-27 14:18:31 +0100

    hep native

---
+++ templates/native/hep/nova-energija/index.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+<?php
+		$native_path = 'https://www.telegram.hr/wp-content/themes/telegram2-desktop/templates/native/telegram/hep';
+	?>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php wp_head(); ?>
+	<!--font-->
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;800&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+	<!--bootstrap-->
+	<link rel="stylesheet" href="<?php echo $native_path ?>/asset/css/bootstrap.min.css" >
+	<!--custom style-->
+	<link rel="stylesheet" href="<?php echo $native_path ?>/asset/css/style.css" >
+	<link rel="stylesheet" href="<?php echo $native_path ?>/asset/css/responsive.css" >
+
+  </head>
+  <body>
+  <section class="top-banner">
+    <div class="container pt-md-5 pt-0">
+	   <!-- <div class="d-flex justify-content-end ">
+	   <span class="text-white d-none d-lg-block position-relative">Napomena: ovo je animirana <br/>vektorska grafika</span>
+	   </div> -->
+	   <div class="text-center">
+	     <!-- <img src="asset/images/grad_animacija.svg" class="img-fluid"/> -->
+        <embed src="<?php echo $native_path ?>/asset/images/grad_animacija.svg">		 
+	    <h1 class="pt-4 pb-5">NOVA <span>ENERGIJA</span></h1>
+		<div class="d-flex justify-content-center">
+		  <a href="" class="otk-link position-relative pt-5">Otkrij</a>
+		</div>
+       </div>	   
+	</div>
+  </section>
+  
+  <section class="info-section">
+    <div class="container text-center pb-4">
+	  <img src="<?php echo $native_path ?>/asset/images/small-img-1.png" class="img-fluid"/>
+		<h2 class="py-4">Stručnjakinja o prelasku na održivo poslovanje: Tvrtke koje se ne uključe sada izgubit će bitku na tržištu</h2>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-7 ms-auto">	     
+		<p class="pb-4">Ravnateljica Hrvatskog poslovnog savjeta za održivi razvoj Mirjana Matešić objašnjava zašto je važna koordinirana tranzicija na održivost...</p> 
+		<a href="https://www.telegram.hr/partneri/strucnjakinja-o-prelasku-na-odrzivo-poslovanje-tvrtke-koje-se-ne-ukljuce-sada-izgubit-ce-bitku-na-trzistu/" target="_blank" class="btn">Pročitajte</a>
+		</div>
+	  </div>
+	</div>
+	<div class="info-banner info-banner-one mt-5 position-relative">
+	   <div class="d-flex info-head-images position-absolute">
+	     <div class="info-head-images-block">
+		   <img src="<?php echo $native_path ?>/asset/images/person-img-1.png" class="img-fluid rounded-circle"/>
+		 </div>
+	   </div>
+	  <!-- <img src="asset/images/banner-2.png" class="img-info-banner d-none d-sm-block"/>
+	  <img src="asset/images/banner-small-2.png" class="img-info-banner d-block d-sm-none"/> -->
+	</div>
+  </section>
+  
+  
+  <section class="info-section">
+    <div class="container text-center pb-4">
+	  <img src="<?php echo $native_path ?>/asset/images/small-img-2.png" class="img-fluid"/>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-9 ms-auto">
+		<h2 class="py-4">Kako će baterije budućnosti utjecati na tržište električnih automobila? Razgovarali smo s profesorom s FER-a</h2>
+		</div>
+	  </div>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-7 ms-auto">
+		<p class="pb-4">Stanley Whittingham koncipirao je litij-ionske baterije još krajem 70-ih godina prošlog stoljeća, za što je dobio Nobelovu nagradu...</p> 
+		<a href="https://www.telegram.hr/partneri/kako-ce-baterije-buducnosti-utjecati-na-trziste-elektricnih-automobila-razgovarali-smo-s-profesorom-s-fer-a/" target="_blank" class="btn">Pročitajte</a>
+		</div>
+	  </div>
+	</div>
+	<div class="info-banner info-banner-two mt-5 position-relative">
+	   <div class="d-flex info-head-images position-absolute">
+	     <div class="info-head-images-block">
+		   <img src="<?php echo $native_path ?>/asset/images/person-img-2.png" class="img-fluid rounded-circle"/>
+		 </div>
+	   </div>
+	 <!--  <img src="asset/images/banner-3.png" class="img-info-banner d-none d-sm-block"/>
+	  <img src="asset/images/banner-small-3.png" class="img-info-banner d-block d-sm-none"/> -->
+	</div>
+  </section>
+  
+  
+  
+  <section class="info-section">
+    <div class="container text-center pb-4">
+	  <img src="<?php echo $native_path ?>/asset/images/small-img-3.png" class="img-fluid"/>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-9 ms-auto">
+		<h2 class="py-4">Istražili smo kojim projektima hrvatski gradovi i tvrtke love titulu pametnih i održivih. <br/>Ima sjajnih primjera</h2>
+		</div>
+	  </div>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-7 ms-auto">        		
+		<p class="pb-4">Kako bi se osigurala energetska učinkovitost, u Hrvatskoj se sve više koriste IoT tehnologije, a mijenja se i način na koji gradimo</p> 
+		<a href="https://www.telegram.hr/partneri/istrazili-smo-kojim-projektima-hrvatski-gradovi-i-tvrtke-love-titulu-pametnih-i-odrzivih-ima-sjajnih-primjera/" target="_blank" class="btn">Pročitajte</a>
+		</div>
+	  </div>
+	</div>
+	<div class="info-banner d-flex justify-content-between flex-md-nowrap pt-5 position-relative">
+	   <div class="w-100">
+	     <img src="<?php echo $native_path ?>/asset/images/side-img-1.png" class="img-info-banner d-none d-sm-block"/>
+		 <img src="<?php echo $native_path ?>/asset/images/side-small-img-1.png" class="img-info-banner d-block d-sm-none"/>
+	   </div>
+	   <div class="w-100 d-none d-sm-block">
+	     <img src="<?php echo $native_path ?>/asset/images/side-img-2.png" class="img-info-banner"/>
+	   </div>	  
+	</div>
+  </section>
+  
+  <section class="info-section">
+    <div class="container text-center pb-4">
+	  <img src="<?php echo $native_path ?>/asset/images/small-img-4.png" class="img-fluid"/>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-9 ms-auto">
+		<h2 class="py-4">Na korak smo od budućnosti koju je predvidio Jules Verne. O vodiku govore naša dva najveća stručnjaka</h2>	
+		</div>
+	  </div>
+	  <div class="row justify-content-center ">
+	    <div class="col-md-8 ms-auto">        	
+		<p class="pb-4">Krajem desetljeća možemo očekivati tegljače, avione i brodove na čisti vodik ili ne elektrogorivo proizvedeno pomoću zelenog vodika...</p> 
+		<a href="https://www.telegram.hr/partneri/na-korak-smo-od-buducnosti-koju-je-predvidio-jules-verne-o-vodiku-govore-nasa-dva-najveca-strucnjaka/" target="_blank" class="btn">Pročitajte</a>
+		</div>
+	  </div>
+	</div>
+	<div class="info-banner info-banner-three mt-5 position-relative">
+	   <div class="d-flex info-head-images position-absolute">
+	     <div class="info-head-images-block mr-2">
+		   <img src="<?php echo $native_path ?>/asset/images/person-img-3.png" class="img-fluid rounded-circle"/>
+		 </div>
+		 <div class="info-head-images-block">
+		   <img src="<?php echo $native_path ?>/asset/images/person-img-4.png" class="img-fluid rounded-circle"/>
+		 </div>
+	   </div>
+	  <!-- <img src="asset/images/banner-4.png" class="img-info-banner d-none d-sm-block"/>
+	  <img src="asset/images/banner-small-4.png" class="img-info-banner d-block d-sm-none"/> -->
+	</div>
+  </section>
+  
+  
+  <section class="info-section">
+    <div class="container text-center pb-4">
+	  <img src="<?php echo $native_path ?>/asset/images/small-img-5.png" class="img-fluid"/>
+		<!-- <h2 class="py-4">Istraživanja pokazuju da studenti STEM-a očekuju najveće plaće. No jesu li takva očekivanja opravdana?</h2> -->
+	  <div class="row justify-content-center ">
+	    <div class="col-md-8 ms-auto">
+        <h2 class="py-4">Istraživanja pokazuju da studenti STEM-a očekuju najveće plaće. No jesu li takva očekivanja opravdana?</h2>		
+		<p class="pb-4">Kako znanost i tehnologija postaju sve važnije komponente naših života, očekuje se kako će poslovi iz STEM-a (znanost, tehnologija...</p> 
+		<a href="https://www.telegram.hr/partneri/istrazivanja-pokazuju-da-studenti-stem-a-ocekuju-najvece-place-no-jesu-li-takva-ocekivanja-opravdana/"  target="_blank" class="btn">Pročitajte</a>
+		</div>
+	  </div>
+	</div>
+	<div class="info-banner-four info-banner pt-5 position-relative">	   
+	  <!-- <img src="asset/images/banner-5.png" class="img-info-banner d-none d-sm-block"/>
+	  <img src="asset/images/banner-small-5.png" class="img-info-banner d-block d-sm-none"/> -->
+	</div>
+  </section>
+
+
+
+
+	<footer class="pt-5 pb-4">
+		<div class="container">
+		<ul class="d-flex justify-content-center align-center p-0 m-0 flex-md-nowrap footer-logo">
+		  <li class="w-sm-50 w-md-25 text-center"><a href="https://www.hep.hr/" target="_blank"><img src="<?php echo $native_path ?>/asset/images/h-logo.png" class="img-fluid"/></a></li> 
+		  <li class="w-sm-50 w-md-25 text-center"><a href="https://www.telegram.hr/" target="_blank"><img src="<?php echo $native_path ?>/asset/images/t-logo.png" class="img-fluid"/></a></li> 
+		</ul>
+		<div class="footer-text text-center pt-5">
+		  <p>Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom Designer Outlet Croatia i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+		</div>
+		</div>
+	</footer>
+
+  <!--js script-->
+<?php wp_footer() ?>
+	<script src="<?php echo $native_path ?>/asset/js/bootstrap.bundle.min.js"></script>
+	
+  </body>
+  </html>
