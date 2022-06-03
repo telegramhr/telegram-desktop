646a42bb90356d5e58ac21b97590f34f708cb708
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-06-03 10:57:31 +0200

    Native Heineken - revamped and new content

--- templates/native/heineken/za-bolji-svijet/js/app.js
+++ templates/native/heineken/za-bolji-svijet/js/app.js
@@ -17,6 +17,24 @@ jQuery(document).ready(function ($) {
             // instead of a settings object
         ]
     });
+    $('.citati-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        autoplay: true,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
 
 var windowWidth = window.innerWidth ||
