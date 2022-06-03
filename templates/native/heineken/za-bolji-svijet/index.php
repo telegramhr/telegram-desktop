d32f2e24200245257ba7ad969b4e7130200744b1
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-06-03 10:59:07 +0200

    Native Heineken reenable jQuery

--- templates/native/heineken/za-bolji-svijet/index.php
+++ templates/native/heineken/za-bolji-svijet/index.php
@@ -897,7 +897,7 @@ if (!$num) {
         </footer>
 
         <?php wp_footer(); ?>
-        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
