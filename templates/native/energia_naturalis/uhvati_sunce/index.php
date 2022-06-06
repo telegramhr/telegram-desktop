62831139cf69fee61ed7d4dde0843134dfb8b37b
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-06-06 13:07:19 +0200

    Native Energia Naturalis v1.0 - launch

---
+++ templates/native/energia_naturalis/uhvati_sunce/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+//$native_path = get_stylesheet_directory_uri() . '/templates/native/energia_naturalis/uhvati_sunce/';
+$native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/energia_naturalis/uhvati_sunce/';
+//$native_path = 'http://staging.telegram.hr/wp-content/themes/telegram-desktop/templates/native/energia_naturalis/uhvati_sunce/';
+//$native_path = 'http://localhost/telegram-desktop/templates/native/energia_naturalis/uhvati_sunce/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo $native_path ?>img/aos.css?ver=1.00" type="text/css" />
+    <link rel="stylesheet" href="https://use.typekit.net/gaa4apb.css">
+    <script src="<?php echo $native_path ?>img/aos.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css?ver=1.00" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css?ver=1.00" type="text/css" />
+    <script src="<?php echo $native_path ?>functions.js"></script>
+</head>
+<body>
+    <div class="main-container flex relative">
+        <header class="full relative">
+            <h1 class="full center-text" data-aos="fade-down">Uhvati sunce</h1>
+            <img src="<?php echo $native_path ?>/img/tg_visual_solar_city.svg" alt="Ilustracija grada opremljenog solarnim panelima">
+        </header>
+        <section class="full flex relative">
+            <div class="container flex relative infographics">
+                <h2 class="full center-text relative">Energija budućnosti</h2> 
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/sun-thin.svg'); ?>
+                    </div>
+                    <p class="full">s prosječno 2000 sunčanih sati godišnje u kontinentalnom i do 2700 sati u priobalnom dijelu, Hrvatska ima značajan potencijal za korištenje solarnih tehnologija</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/solar-panel-thin.svg'); ?>
+                    </div>
+                    <p class="full">prosječna elektrana ima oko 20 kvadratnih metara solarnih panela</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/plug-thin.svg'); ?>
+                    </div>
+                    <p class="full">prosječnom četveročlanom kućanstvu na kontinentu potrebna je solarna elektrana snage do 6 kW, a na obali do 5 kW</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/hand-holding-dollar-thin.svg'); ?>
+                    </div>
+                    <p class="full">ušteda do 80% posto na računima za električnu energiju</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/piggy-bank-thin.svg'); ?>
+                    </div>
+                    <p class="full">povrat investicije u prosjeku se očekuje za 6 do 8 godina</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/transformer-bolt-thin.svg'); ?>
+                    </div>
+                    <p class="full">višak proizvedene energije vlasnik elektrane predaje u energetski sustav</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/money-bill-thin.svg'); ?>
+                    </div>
+                    <p class="full">tijekom životnog vijeka prosječna solarna elektrana vlasniku uštedi oko 100.000 kuna</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/bolt-thin.svg'); ?>
+                    </div>
+                    <p class="full">sva električna energija koju elektrana proizvede nakon što sama sebe otplati za kupca je besplatna</p>
+                </div>
+                <div class="third flex flex-responsive column-full-pad">
+                    <div class="full flex center infographic-icon" data-aos="flip-up">
+                        <?php echo file_get_contents($native_path.'img/trees-thin.svg'); ?>
+                    </div>
+                    <p class="full">prosječna elektrana u Hrvatskoj smanjuje emisiju 1980 kg CO2 godišnje, što je ekvivalent sadnji 180 stabala</p>
+                </div>
+            </div>
+        </section>
+        <section class="full flex relative white-bg">
+            <div class="container flex relative articles column-vertical-pad">
+                <h2 class="full center-text relative">Više o temi</h2> 
+                <article class="full flex relative stretch">
+                    <a class="full flex relative stretch" target="_blank" href="https://www.telegram.hr/partneri/cijene-energenata-divljaju-a-krizi-se-ne-nazire-kraj-energetski-strucnjak-savjetuje-kako-ustedjeti">
+                        <div class="half center flex-responsive column-full-pad">
+                            <div class="full flex relative">
+                                <h3>Cijene energenata divljaju, a krizi se ne nazire kraj. Energetski stručnjak savjetuje kako uštedjeti</h2>
+                                <p>Naš sugovornik naglašava i kako treba gledati u budućnost te tražiti alternativne izvore energije poput solarnih elektrana</p>
+                                <div class="btn">Pročitaj više</div>
+                            </div>
+                        </div>
+                        <div class="half center flex-responsive column-full-pad article-img">
+                            <img src="https://images.telegram.hr/QH9vhcp-a2xEwJJXOMWYZelt2nAqgHjXlzKrRF_NYt4/preset:single2/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjIvMDYvbWlsaXNhLXpvcmFuMi5qcGc.jpg" aria-hidden="true">
+                        </div>
+                    </a>
+                </article>
+            </div>
+        </section>
+        <section class="full flex relative white-bg">
+            <div class="container flex relative calculator column-vertical-pad">
+                <h2 class="full center-text relative">Izračunaj koliko štediš</h2> 
+                <iframe scrolling="no" src="https://www.energianaturalis.hr/enapi?h=d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35"></iframe>
+            </div>
+        </section>
+        <section class="full flex relative white-bg">
+            <div class="container flex relative more-info column-full-pad">
+                <h2 class="full center-text relative">Želim znati</h2> 
+                <p>Solarnom elektranom kupac proizvodi električnu energiju za svoje potrebe i postaje energetski neovisan.</p>
+                <h4>Što je solarna elektrana?</h4>
+                <p>Pod pojmom solarna elektrana najčešće mislimo na sunčevu fotonaponsku elektranu koja solarnu energiju pretvara u električnu energiju koju koristimo u svakodnevnom životu.</p>
+                <h4>Što su solarni paneli?</h4>
+                <p>Fotonaponske elektrane koriste velika područja fotonaponskih ćelija, poznatih kao PV ili solarne ćelije, za pretvaranje sunčeve svjetlosti u električnu energiju. Više međusobno povezanih grupa solarnih ćelija tvori solarni, fotonaponski ili PV panel.</p>
+                <h4>Koliki je vijek trajanja?</h4>
+                <p>Sastavni dijelovi solarnih elektrana imaju jamstveni rok i do 25 godina. To ne znači da elektrana istekom tog razdoblja prestaje raditi i proizvoditi električnu energiju. Nakon zajamčenih 25 godina, ona će proizvoditi električnu energiju u nešto manjem obujmu dok njezin rok trajanja može biti i više od 30 godina.</p>
+            </div>
+        </section>
+        <footer class="full flex relative gray-bg">
+            <div class="container center relative column-full-pad footer-logos">
+                <img src="<?php echo $native_path ?>/img/enna_logo.png" aria-hidden="true">
+                <img src="<?php echo $native_path ?>/img/tg_studio.png" aria-hidden="true">
+            </div>
+        </footer>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
