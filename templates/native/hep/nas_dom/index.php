d7d5ba373847a2a52817c0560d3f6d2566d30719
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-07-05 11:51:44 +0200

    Native HEP - Naš Dom v1.0 launch

---
+++ templates/native/hep/nas_dom/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+
+    $native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/hep/nas_dom/';
+    //$native_path = 'http://localhost/telegram-desktop/templates/native/hep/nas_dom/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <?php wp_head(); ?>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css?ver=1.00" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css?ver=1.00" type="text/css" />
+    <script src="<?php echo $native_path ?>html2canvas.min.js"></script>
+    <script src="<?php echo $native_path ?>functions.js"></script>
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="<?php echo $native_path ?>assets/css/bootstrap.min.css" />
+    <!--====== Line Icons css ======-->
+    <link rel="stylesheet" href="<?php echo $native_path ?>assets/css/lineicons.css" />
+    <!--====== Tiny Slider css ======-->
+    <link rel="stylesheet" href="<?php echo $native_path ?>assets/css/tiny-slider.css" />
+    <!--====== gLightBox css ======-->
+    <link rel="stylesheet" href="<?php echo $native_path ?>assets/css/glightbox.min.css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css" />
+</head>
+
+<body>
+
+
+  <!-- Start header Area -->
+  <section id="hero-area" class="header-area header-eight">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 col-md-12 col-12">
+          <div class="header-content">
+            <h1><span style="font-weight: 300;">Naš<br>dom,</span> naša odgovornost</h1>
+            <div class="button">
+              <a href="#krenimo" class="btn primary-btn"><span style="font-weight:900">OTKRIJ</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-12">
+          <div class="header-image">
+            <img src="assets/images/hero-svg_01.svg" alt="#" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End header Area -->
+
+  <!--====== ABOUT FIVE PART START ======-->
+
+  <section id="krenimo" class="about-area about-five">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-5 col-12">
+          <div class="about-image-five">
+            <a href="https://www.telegram.hr/partneri/struja-je-odnedavno-skuplja-10-posto-energetski-strucnjak-samo-gasenjem-tv-a-nocu-stedi-se-78-kuna-godisnje/" target="_blank"><img src="<?php echo $native_path ?>assets/images/clanak-1.jpeg" alt="Struja je odnedavno skuplja 10 posto. Energetski stručnjak: 'Samo gašenjem TV-a noću štedi se 78 kuna godišnje'" /></a>
+          </div>
+        </div>
+        <div class="col-lg-7 col-12">
+          <div class="about-five-content">
+            <h2 class="main-title fw-bold"><a href="https://www.telegram.hr/partneri/struja-je-odnedavno-skuplja-10-posto-energetski-strucnjak-samo-gasenjem-tv-a-nocu-stedi-se-78-kuna-godisnje/" target="_blank"><span style="font-weight:700">Struja je odnedavno skuplja 10 posto. Energetski stručnjak:</span> 'Samo gašenjem TV-a noću štedi se 78 kuna godišnje'</a></h2>
+            <div class="about-five-tab">
+              <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-who" role="tabpanel" aria-labelledby="nav-who-tab">
+                  <p>Marko Bišćan s Instituta Hrvoje Požar kaže da uređaje krivo biramo, ali ih i krivo koristimo. No, postoji aplikacija koja će nam pomoći smanjiti troškove</p>
+
+                <div class="button">
+                  <a href="https://www.telegram.hr/partneri/struja-je-odnedavno-skuplja-10-posto-energetski-strucnjak-samo-gasenjem-tv-a-nocu-stedi-se-78-kuna-godisnje/" target="_blank" class="btn primary-btn">Pročitajte više</a>
+                </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- container -->
+  </section>
+
+  <!--====== ABOUT FIVE PART ENDS ======-->
+
+
+
+ <!-- Start Cta Area -->
+  <section id="call-action" class="call-action">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-6 col-xl-7 col-lg-8 col-md-9">
+          <div class="inner-content">
+            <h2><a href="https://www.telegram.hr/partneri/energetski-strucnjak-o-krizi-s-plinom-hrvatska-moze-biti-u-samom-vrhu-eu-po-udjelu-energije-iz-obnovljivih-izvora/" target="_blank"><span style="font-weight:700">Energetski stručnjak o krizi s plinom:</span> Hrvatska može biti u samom vrhu EU po udjelu energije iz obnovljivih izvora</a></h2>
+            <p>
+              Sada je prilika za investiranje u tehnologije obnovljivih izvora pomoću kojih možemo zadovoljiti svoje potrebe, tvrdi prof. Goran Krajačić
+            </p>
+            <div class="light-rounded-buttons">
+              <a href="https://www.telegram.hr/partneri/energetski-strucnjak-o-krizi-s-plinom-hrvatska-moze-biti-u-samom-vrhu-eu-po-udjelu-energije-iz-obnovljivih-izvora/" target="_blank" class="btn primary-btn-outline">Pročitajte više</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Cta Area -->
+
+  <section class="about-area about-five">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-5 col-12">
+          <div class="about-image-five">
+            <a href="https://www.telegram.hr/partneri/iza-nje-je-prva-punionica-i-bicikl-na-vodik-dolina-vodika-sjeverni-jadran-donijet-ce-45-000-radnih-mjesta/" target="_blank"><img src="<?php echo $native_path ?>assets/images/clanak-3.jpeg" alt="Iza nje je prva punionica i bicikl na vodik. 'Dolina vodika Sjeverni Jadran donijet će 45.000 radnih mjesta'" /></a>
+          </div>
+        </div>
+        <div class="col-lg-7 col-12">
+          <div class="about-five-content">
+            <h2 class="main-title fw-bold"><a href="https://www.telegram.hr/partneri/iza-nje-je-prva-punionica-i-bicikl-na-vodik-dolina-vodika-sjeverni-jadran-donijet-ce-45-000-radnih-mjesta/" target="_blank"><span style="font-weight:700">Iza nje je prva punionica i bicikl na vodik.</span> 'Dolina vodika Sjeverni Jadran donijet će 45.000 radnih mjesta'</a></h2>
+            <div class="about-five-tab">
+              <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-who" role="tabpanel" aria-labelledby="nav-who-tab">
+                  <p>Profesorica s Fakulteta strojarstva i brodogradnje kaže da je hrvatska strategija za vodik ambiciozno realna i provediva</p>
+
+                <div class="button">
+                  <a href="https://www.telegram.hr/partneri/iza-nje-je-prva-punionica-i-bicikl-na-vodik-dolina-vodika-sjeverni-jadran-donijet-ce-45-000-radnih-mjesta/" target="_blank" class="btn primary-btn">Pročitajte više</a>
+                </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- container -->
+  </section>
+
+  <!--====== ABOUT FIVE PART ENDS ======-->
+ <!-- Start Cta Area -->
+  <section id="call-action" class="call-action clanak">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-6 col-xl-7 col-lg-8 col-md-9">
+          <div class="inner-content">
+            <h2><a href="https://www.telegram.hr/partneri/energetski-strucnjak-o-krizi-s-plinom-hrvatska-moze-biti-u-samom-vrhu-eu-po-udjelu-energije-iz-obnovljivih-izvora/" target="_blank"><span style="font-weight:700">Stručnjak za bioraznolikost:</span> U boljem smo stanju nego ostale europske zemlje, ali vrijeme i nama istječe</a></h2>
+            <p>
+              Plemenita periska, najveći školjkaš u Jadranu, gotovo je izumro. Žive jedinke ove vrste danas u Hrvatskoj brojimo na prste
+            </p>
+            <div class="light-rounded-buttons">
+              <a href="https://www.telegram.hr/partneri/strucnjak-za-bioraznolikost-u-boljem-smo-stanju-nego-ostale-europske-zemlje-ali-vrijeme-i-nama-istjece/" target="_blank" class="btn primary-btn-outline">Pročitajte više</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Cta Area -->
+  <section class="about-area about-five">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-5 col-12">
+          <div class="about-image-five">
+            <a href="https://www.telegram.hr/partneri/ovi-industrijski-pogoni-sada-su-top-turisticke-destinacije-u-hrvatskoj-je-jedan-uvrsten-u-svjetski-hidro-hall-of-fame/" target="_blank"><img src="<?php echo $native_path ?>assets/images/clanak-3aaa.jpeg" alt="Ovi industrijski pogoni sada su top turističke destinacije. U Hrvatskoj je jedan uvršten u svjetski hidro Hall of Fame" /></a>
+          </div>
+        </div>
+        <div class="col-lg-7 col-12">
+          <div class="about-five-content">
+            <h2 class="main-title fw-bold"><a href="https://www.telegram.hr/partneri/ovi-industrijski-pogoni-sada-su-top-turisticke-destinacije-u-hrvatskoj-je-jedan-uvrsten-u-svjetski-hidro-hall-of-fame/" target="_blank"><span style="font-weight:700">Ovi industrijski pogoni sada su top turističke destinacije.</span> U Hrvatskoj je jedan uvršten u svjetski hidro Hall of Fame</a></h2>
+            <div class="about-five-tab">
+              <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-who" role="tabpanel" aria-labelledby="nav-who-tab">
+                  <p>Hidroelektrana Jaruga u nacionalnom parku Krka samo je tri dana mlađa od hidroelektrane na slavnim slapovima Niagare</p>
+
+                <div class="button">
+                  <a href="https://www.telegram.hr/partneri/ovi-industrijski-pogoni-sada-su-top-turisticke-destinacije-u-hrvatskoj-je-jedan-uvrsten-u-svjetski-hidro-hall-of-fame/" target="_blank" class="btn primary-btn">Pročitajte više</a>
+                </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- container -->
+  </section>
+
+  <!-- Start Footer Area -->
+  <footer class="footer-area footer-eleven">
+    <!-- Start Footer Top -->
+    <div class="footer-top">
+      <div class="row">
+            <div class="col-lg-12 col-md-12 col-12" style="text-align:center;">
+              <img src="assets/images/ikona.png" alt="HEP" style="width:auto; height:150px; margin:20px;">
+            </div>
+      </div>
+      <div class="container">
+        <div class="inner-content">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+              <!-- Single Widget -->
+              <div class="footer-widget f-about">
+                <div class="logo">
+                  <a href="index.html">
+                    <img src="assets/images/hep_logo.svg" alt="#" class="img-fluid" style="width: auto; height: 35px; margin-top:25px; margin-right: 20px; " /> <img src="<?php echo $native_path ?>assets/images/telegram_logo.svg" alt="#" class="img-fluid" style="width: auto; height: 35px; margin-top:25px" />
+                  </a>
+                </div>
+                <p class="copyright-text">
+                  Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing,<br>u suradnji s partnerom HEP i po najvišim uredničkim standardima Telegram Media Grupe.
+                </p>
+              </div>
+              <!-- End Single Widget -->
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ End Footer Top -->
+  </footer>
+  <!--/ End Footer Area -->
+
+  <a href="#" class="scroll-top btn-hover">
+    <i class="lni lni-chevron-up"></i>
+  </a>
+
+  <!--====== js ======-->
+  <script src="<?php echo $native_path ?>assets/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $native_path ?>assets/js/glightbox.min.js"></script>
+  <script src="<?php echo $native_path ?>assets/js/main.js"></script>
+  <script src="<?php echo $native_path ?>assets/js/tiny-slider.js"></script>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
