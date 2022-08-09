bdb3a4fdf659ce6c8d615adaf41cacc89490dc23
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-08-09 10:59:53 +0200

    Native Moje mjesto v2.1 - add another article

--- templates/native/moje_mjesto/tvoja_destinacija/index.php
+++ templates/native/moje_mjesto/tvoja_destinacija/index.php
@@ -170,19 +170,25 @@ $native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/template
         <div class="full flex">
             <h2 class="full relative episode-title" data-aos="fade-left">Pročitaj više</h2>
             <div class="full sugovornici extra-content flex stretch">
-                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="500">
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="200">
+                    <img src="https://images.telegram.hr/HjC2PmBOu6_rtlUENjOZRRh1yPd9QQdi-Vz-Ea1Gisg/preset:single2/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjIvMDgvaWR6MjUwMC5qcGc.jpg" aria-hidden="true">
+                    <h3 class="full">Podno Papuka proizvode gin koji osvaja ozbiljne svjetske nagrade. U planu je i slavonski viski </h3>
+                    <p> S prijateljima su pokrenuli Ideš i piješ, 12 kilometara dugu eno-gastro šetnju koja počinje 27. kolovoza</p>
+                    <a href="https://www.telegram.hr/partneri/podno-papuka-proizvode-gin-koji-osvaja-ozbiljne-svjetske-nagrade-u-planu-je-i-slavonski-viski/" target="_blank" class="insite-btn">Pročitaj više</a>
+                </div>
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="400">
                     <img src="https://super1.telegram.hr/wp-content/uploads/sites/3/2022/07/2022-7-19-naslovna2.jpg" aria-hidden="true">
                     <h3 class="full">Istok Lijepe naše ima toliko toga za ponuditi. Evo što je oduševilo poznatog blogera </h3>
                     <p>Kontinentalni turizam treba dobiti svoje zasluženo mjesto na karti Hrvatske</p>
                     <a href="https://super1.telegram.hr/relax/istok-lijepe-nase-ima-toliko-toga-za-ponuditi-evo-sto-je-odusevilo-poznatog-blogera/" target="_blank" class="insite-btn">Pročitaj više</a>
                 </div>
-                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="500">
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="600">
                     <img src="https://super1.telegram.hr/wp-content/uploads/sites/3/2021/08/d7fa4f22-72c5-4264-8f5c-28644d2eb84b-880x520.jpg" aria-hidden="true">
                     <h3 class="full">Pronašli smo još jedan prekrasan svjetionik u Istri koji možete iznajmiti za boravak </h3>
                     <p>U ovaj svjetionik u Istri koji može primiti osam gostiju zaljubili smo se na prvi pogled</p>
                     <a href="https://super1.telegram.hr/relax/pronasli-smo-jos-jedan-prekrasan-svjetionik-u-istri-koji-mozete-iznajmiti-za-boravak/" target="_blank" class="insite-btn">Pročitaj više</a>
                 </div>
-                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="750">
+                <div class="third flex-responsive flex" data-aos="fade-right" data-aos-delay="800">
                     <img src="https://images.telegram.hr/Ikp0Al1HkYEtSmVpXdU4ETf0YRPglp17jpmwN4m59Rc/preset:single2/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjEvMTAvdHp6ei1va3VzaS1wcmlqZS1zdmVnYS1zdGlsbC0xMTEucG5n.jpg" aria-hidden="true">
                     <h3 class="full">Pitoreskne vinske ceste u okolici Zagreba imaju top ponude za vikend izlete. Saznali smo što sve nude</h3>
                     <p>Ponuda ovog kraja pokriva sve, od vina iz amfora do pjenušaca i famoznih autohtonih sorti</p>
