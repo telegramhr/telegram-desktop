18a33c68cc22827082bfab0382f91ed32a2605cd
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-10-11 11:53:28 +0200

    Chart render tweaks

--- plugins/charts.php
+++ plugins/charts.php
@@ -4,21 +4,6 @@ add_filter('wpdatatables_filter_apexcharts_render_data', 'telegram_charts_option
 
 function telegram_charts_options($apexcharts_render_data, $id, $that) {
     if ($id == 2) {
-        /*$apex_mydata = array(
-            "annotations" => [
-                "xaxis" => [
-                    "x" => 1.1,
-                }
-                "borderColor" => "#efefef",
-                    "label" => [
-                        "style" => [
-                            "color" => "#212112"
-                        ],
-                        "text" => "Trenutna razina zagrijavanja"
-                    ]
-                ]
-            ]
-        );*/
         $apexcharts_render_data['options']['annotations'] =
             [
                 "xaxis" => [
@@ -36,7 +21,7 @@ function telegram_charts_options($apexcharts_render_data, $id, $that) {
                         [
                             "x" => 1.5,
                             "x2" => 2,
-                            "fillColor" => "#3737ae",
+                            "fillColor" => "#ffffff",
                             "textAnchor" => "middle",
                             "label" => [
                                 "style" => [
@@ -48,7 +33,26 @@ function telegram_charts_options($apexcharts_render_data, $id, $that) {
                         ]
                 ]
             ];
+            $apexcharts_render_data['options']['xaxis']['categories'] = [
+                ["Kolaps grenlandskog ", "ledenog pokrivača"],
+                ["Kolaps ledenog pokrivača ", "zapadnog Antarktika"],
+                ["Odumiranje tropskog ", "koraljnog grebena"],
+                ["Naglo otapanje ", "sjevernog permafrosta"],
+                ["Gubitak leda ", "u Barentsovom moru"],
+                ["Kolaps Labradorske ", "morske struje"],
+                ["Gubitak planinskih ", "ledenjaka"],
+                ["Pomak zapadnoafričkih ", "monsuna"],
+                ["Kolaps istočnog ", "antarktičkog ledenjaka"],
+                ["Odumiranje amazonske ", "prašume"],
+                ["Kolaps sjevernog ", "permafrosta"],
+                ["Kolaps atlantske struje"],
+                ["Odumiranje sjevernih ", "šuma na jugu"],
+                ["Odumiranje sjevernih ", "šuma na sjeveru"],
+                ["Kolaps arktičkog ", "zimskog ledenjaka"],
+                ["Kolaps ledenog pokrivača ", "istočne Antarktike"],
+            ];
             $apexcharts_render_data['options']['legend']['onItemClick']['toggleDataSeries'] = false;
+            $apexcharts_render_data['options']['yaxis']['labels']['maxWidth'] = 250;
     }
 
 
