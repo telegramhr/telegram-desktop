9f446ac29fbd8441efdc87290e68ee60f4292940
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-10-24 16:33:26 +0200

    Native EON v1.03 - update charts.php for new chart

--- plugins/charts.php
+++ plugins/charts.php
@@ -3,7 +3,7 @@
 add_filter('wpdatatables_filter_apexcharts_render_data', 'telegram_charts_options', 10, 3);
 
 function telegram_charts_options($apexcharts_render_data, $id, $that) {
-    if ($id == 2) {
+    if ($id == 2 || $id == 4) {
         $apexcharts_render_data['options']['annotations'] =
             [
                 "xaxis" => [
