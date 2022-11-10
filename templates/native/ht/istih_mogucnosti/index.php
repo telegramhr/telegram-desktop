9b7d99bd6a21ebe96323f7f6c3986855a74aeee2
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-11-10 09:46:29 +0100

    Native HT SIM v1.00 - init

---
+++ templates/native/ht/istih_mogucnosti/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+//$native_path = get_stylesheet_directory_uri() . '/templates/native/ht/istih_mogucnosti/';
+$native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/ht/istih_mogucnosti/';
+//$native_path = 'http://staging.telegram.hr/wp-content/themes/telegram-desktop/templates/native/ht/istih_mogucnosti/';
+//$native_path = 'http://localhost/telegram-desktop/templates/native/ht/istih_mogucnosti/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css?ver=3.00" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css?ver=3.12" type="text/css" />
+</head>
+<body>
+    <nav class="side-nav center">
+        <a class="full" href="#uvod"><h3 class="full">Svijet istih mogućnosti</h3></a>
+        <a class="full" href="#talita">Talita</a>
+        <a class="full" href="#molly">Molly</a>
+        <a class="full" href="#hrvoje">Hrvoje</a>
+        <a class="full" href="#podaci1"><div class="side-nav-bar"></div></a>
+        <a class="full" href="#stipo">Stipo</a>
+        <a class="full" href="#hannah">Hannah</a>
+        <a class="full" href="#podaci2"><div class="side-nav-bar"></div></a>
+        <a class="full" href="#anka">Anka</a>
+        <a class="full" href="#chella">Chella</a>
+        <a class="full" href="#podaci3"><div class="side-nav-bar"></div></a>
+        <a class="full" href="#partneri">Partneri</a>
+        <div class="full flex">
+            <img src="<?php echo $native_path ?>img/ht.svg" alt="Hrvatski telekom logo" />
+        </div>
+    </nav>
+    <div class="main-container flex relative">
+        <header class="full center relative slide" id="uvod">
+            <img src="<?php echo $native_path ?>img/tg_naslovna_2022_10_20-3.jpg" aria-hidden="true" class="bg-img">
+            <div class="full flex relative header-text">
+                <h1 class="full">Svijet istih mogućnosti</h1>
+                <p class="full header-p">Oni su se suočili sa svojim preprekama i osvojili naša srca. Iz dana u dan dokazuju kako ništa ne može stati na put njihovoj volji i energiji. Kreiraju sadržaj, razvijaju inovacije, pokreću promjene i nikad ne odustaju. Njihove priče naša su inspiracija za Svijet istih mogućnosti.</p>
+                <p class="full bold">Sadržaj nastao u suradnji s Hrvatskim Telekomom.</p>
+            </div>
+            <div class="arrow-indicator center"><div></div></div>
+        </header>
+        <a class="full flex relative slide slide-bottom-left" id="talita" href="https://www.telegram.hr/partneri/u-skoli-su-je-maltretirali-no-nije-se-dala-uplasiti-talita-je-edukatorica-aktivistica-i-svojim-radom-pomice-granice-umjetnosti/" target="_blank">
+            <img src="https://www.telegram.hr/wp-content/uploads/2022/10/talita1.jpg" alt="Talita pred prozorima u vrijeme sumraka" class="bg-img">
+            <div class="article-text relative">
+                <img src="<?php echo $native_path ?>img/line.png" aria-hidden="true" class="dec-line">
+                <h2 class="full animate">U školi su je maltretirali, no nije se dala uplašiti. Talita je edukatorica, aktivistica i svojim radom pomiče granice umjetnosti</h2>
+                <div class="cta">Pročitaj priču ></div>
+            </div>
+        </a>
+        <a class="full flex relative slide stretch half-slide" id="molly" href="https://www.telegram.hr/partneri/do-14-godine-je-izgubila-vid-i-pala-u-depresiju-danas-je-savjetnica-za-inkluziju-googleu-netflixu-i-microsoftu/" target="_blank">
+            <div class="half flex flex-responsive relative">
+                <img src="<?php echo $native_path ?>img/molly.jpg" aria-hidden="true" class="bg-img">
+            </div>
+            <div class="half center flex flex-responsive">
+                <div class="article-text relative">
+                    <h2 class="full animate">Do 14. godine je izgubila vid i pala u depresiju. Danas je savjetnica za inkluziju Googleu, Netflixu i Microsoftu</h2>
+                    <div class="cta">Pročitaj priču ></div>
+                </div>
+            </div>
+        </a>
+        <a class="full flex relative slide slide-bottom-left" id="hrvoje" href="https://www.telegram.hr/partneri/hrvoje-je-od-rodenja-gotovo-potpuno-slijep-ali-odbio-se-predati-danas-ima-super-posao-i-osmisljava-genijalne-aplikacije/" target="_blank">
+            <img src="https://www.telegram.hr/wp-content/uploads/2022/09/sasa4157xw.jpg" alt="Hrvoje, u hodniku" class="bg-img">
+            <div class="article-text relative">
+                <img src="<?php echo $native_path ?>img/line.png" aria-hidden="true" class="dec-line">
+                <h2 class="full animate">Hrvoje je od rođenja gotovo potpuno slijep, ali odbio se predati. Danas ima super posao i osmišljava genijalne aplikacije</h2>
+                <div class="cta">Pročitaj priču ></div>
+            </div>
+        </a>
+        <div class="full center relative slide darker-bg" id="podaci1">
+            <video src="<?php echo $native_path ?>img/elderly.mp4" autoplay muted playsinline loop class="video-bg"></video>
+            <div class="full flex relative stretch upzindex">
+                <p class="full big-fact">54</p>
+                <p class="full fact-description">doma za starije sudjeluje u programu digitalnog opismenjavanja Hrvatskog Telekoma</p>
+            </div>
+        </div>
+        <a class="full flex relative slide slide-bottom-left" id="stipo" href="https://www.telegram.hr/partneri/ovo-je-stipo-nas-novi-heroj-nije-dozvolio-da-ga-dijagnoza-dotuce-upisao-je-faks-i-razvio-genijalni-servis-za-ljude-u-kolicima/" target="_blank">
+            <img src="https://www.telegram.hr/wp-content/uploads/2022/09/margic-stipo.jpg" alt="Protagonist članka Stipo, sa smješkom na licu" class="bg-img">
+            <div class="article-text relative">
+                <img src="<?php echo $native_path ?>img/line.png" aria-hidden="true" class="dec-line">
+                <h2 class="full animate">Ovo je Stipo, naš novi heroj. Nije dozvolio da ga dijagnoza dotuče, upisao je faks i razvio genijalni servis za ljude u kolicima</h2>
+                <div class="cta">Pročitaj priču ></div>
+            </div>
+        </a>
+        <a class="full flex relative slide stretch half-slide" id="hannah" href="https://www.telegram.hr/partneri/rodila-se-12-tjedana-ranije-rekli-su-da-nikad-nece-ni-hodati-ni-govoriti-danas-mijenja-svjetsku-glazbenu-scenu/" target="_blank">
+            <div class="half flex flex-responsive relative">
+                <img src="<?php echo $native_path ?>img/hannah.jpg" aria-hidden="true" class="bg-img">
+            </div>
+            <div class="half center flex flex-responsive">
+                <div class="article-text relative">
+                    <h2 class="full animate">Rodila se 12 tjedana ranije, rekli su da nikad neće ni hodati ni govoriti. Danas mijenja svjetsku glazbenu scenu</h2>
+                    <div class="cta">Pročitaj priču ></div>
+                </div>
+            </div>
+        </a>
+        <div class="full center relative slide darker-bg" id="podaci2">
+        <video src="<?php echo $native_path ?>img/worker.mp4" autoplay muted playsinline loop class="video-bg"></video>
+            <div class="full flex relative stretch upzindex">
+                <p class="full big-fact small-fact">Poslodavcem</p>
+                <p class="full fact-description">godine za osobe s invaliditetom proglašen je Hrvatski Telekom</p>
+            </div>
+        </div>
+        <a class="full flex relative slide slide-bottom-left" id="anka" href="https://www.telegram.hr/wp-content/uploads/2022/09/sliskovic-anka2.jpg" target="_blank">
+            <img src="https://www.telegram.hr/wp-content/uploads/2022/09/sliskovic-anka2.jpg" alt="Protagonistica članka gospođa Anka, sa smješkom na licu" class="bg-img">
+            <div class="article-text relative">
+                <img src="<?php echo $native_path ?>img/line.png" aria-hidden="true" class="dec-line">
+                <h2 class="full animate">Gospođa Anka postala influencerica u 60-ima: ‘Bojala sam se mirovine, a onda su me kćeri nagovorile na Instagram’</h2>
+                <div class="cta">Pročitaj priču ></div>
+            </div>
+        </a>
+        <a class="full flex relative slide stretch half-slide" id="chella" href="https://www.telegram.hr/partneri/s-13-je-izgubio-sluh-a-danas-je-uspjesan-vloger-model-i-glumac-najponosniji-je-na-ulogu-nijemog-superjunaka/" target="_blank">
+            <div class="half flex flex-responsive relative">
+                <img src="<?php echo $native_path ?>img/chella.jpg" aria-hidden="true" class="bg-img">
+            </div>
+            <div class="half center flex flex-responsive">
+                <div class="article-text relative">
+                    <h2 class="full animate">S 13 je izgubio sluh, a danas je uspješan vloger, model i glumac. Najponosniji je na ulogu nijemog superjunaka</h2>
+                    <div class="cta">Pročitaj priču ></div>
+                </div>
+            </div>
+        </a>
+        <div class="full center relative slide darker-bg" id="podaci3">
+        <video src="<?php echo $native_path ?>img/lady.mp4" autoplay muted playsinline loop class="video-bg"></video>
+            <div class="full flex relative stretch upzindex">
+                <p class="full big-fact">41%</p>
+                <p class="full fact-description">rukovodećih pozicija u HT-u drže žene</p>
+            </div>
+        </div>
+        <footer class="full center relative slide" id="partneri">
+            <div class="full center logo-line">
+                <img src="<?php echo $native_path ?>img/ht.svg" alt="Hrvatski telekom logo" />
+                <img src="<?php echo $native_path ?>img/tg_studio_white.svg" alt="TG Studio logo" class="dark-only" />
+                <img src="<?php echo $native_path ?>img/tg_studio_black.svg" alt="TG Studio logo" class="hide-on-dark" />
+            </div>
+            <p class="full center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom Hrvatski Telekom i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+        </footer>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
