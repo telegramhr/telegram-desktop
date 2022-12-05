891ae007ac62709cd5661055143e6fe162e123a2
Marijan Mikolić <marijan.mikolic@gmail.com>
2022-12-05 16:42:19 +0100

    Native JD Lucky7 v1.0 - launch

---
+++ templates/native/jackdaniels/lucky7/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+//$native_path = get_stylesheet_directory_uri() . '/templates/native/jackdaniels/lucky7/';
+$native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/jackdaniels/lucky7/';
+//$native_path = 'http://staging.telegram.hr/wp-content/themes/telegram-desktop/templates/native/jackdaniels/lucky7/';
+//$native_path = 'http://localhost/telegram-desktop/templates/native/jackdaniels/lucky7/';
+//$native_path = 'http://localhost/lucky_no7/';
+
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;700&family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>img/aos.css?ver=1.00" type="text/css" />
+    <script src="<?php echo $native_path ?>img/aos.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css?ver=1.00" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css?ver=1.10" type="text/css" />
+    <script src="<?php echo $native_path ?>functions.js"></script>
+</head>
+<body>
+    <div class="main-container flex relative">
+        <nav class = "full topnav flex relative">
+            <div class = "container full flex">
+                <div class="center full relative textlogo center-text"><div></div>Lucky no.7<div></div></div>
+                    <!-- <div class = "center relative">
+                        <div class = "full bold uppercase navtext">Make it Count</div>
+                </div> -->
+            </div>
+        </nav>
+        <!-- ARTICLES --------------------------->
+    <section class = "full flex relative margin-top">
+        <a class = "full center relative" href="https://telesport.telegram.hr/partneri/mbappe-vitez/" target = "_blank">
+            <div class = "full center relative flex-responsive">
+                <img class = "full" src="https://telesport.telegram.hr/wp-content/uploads/sites/2/2022/11/jack-daniels-mbappe-sedmica-uvodna-fin.jpg" aria-hidden="true" class="horizontal-pad pad-bot margin-top">
+            </div>
+            <div class = "container full center relative">
+                <div class = "full flex relative center center-text horizontal-pad flex-responsive">
+                    <h2 class = "full">Mbappé: Vitez</h2>
+                    <!-- <p class = "full text-container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac sollicitudin quam, id mattis odio. Cras mattis lacus lectus, vel condimentum velit fringilla nec. Vestibulum tempor eros sit amet neque efficitur, eu congue massa lobortis. Fusce sollicitudin sollicitudin tempor. Nulla at diam ut elit fringilla efficitur sit amet eu massa.</p> -->
+                </div>
+                <div class="full center relative">
+                    <div class="button-48"><span class="text">Pročitaj više...</span></div>
+                </div>
+            </div>
+        </a>
+    </section>
+    <section class = "full flex relative margin-top">
+        <a class = "full center relative" href="https://telesport.telegram.hr/partneri/garrincha-andeo/" target = "_blank">
+            <div class = "full center relative flex-responsive">
+                <img class = "full" src="https://telesport.telegram.hr/wp-content/uploads/sites/2/2022/11/jack-daniels-garicnha-uvodna.jpg" aria-hidden="true" class="horizontal-pad pad-bot margin-top">
+            </div>
+            <div class = "container full center relative">
+                <div class = "full flex relative center center-text horizontal-pad flex-responsive">
+                    <h2 class = "full">Garrincha: Anđeo</h2>
+                    <!-- <p class = "full text-container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac sollicitudin quam, id mattis odio. Cras mattis lacus lectus, vel condimentum velit fringilla nec. Vestibulum tempor eros sit amet neque efficitur, eu congue massa lobortis. Fusce sollicitudin sollicitudin tempor. Nulla at diam ut elit fringilla efficitur sit amet eu massa.</p> -->
+                </div>
+                <div class="full center relative">
+                    <div class="button-48"><span class="text">Pročitaj više...</span></div>
+                </div>
+            </div>
+        </a>
+    </section>
+    <section class = "full flex relative margin-top">
+        <a class = "full center relative" href="https://telesport.telegram.hr/partneri/rakitic-klasa-iz-sjene/" target = "_blank">
+            <div class = "full center relative flex-responsive">
+                <img class = "full" src="https://telesport.telegram.hr/wp-content/uploads/sites/2/2022/11/jack-daniels-rakitic-uvodna.jpg" aria-hidden="true" class="horizontal-pad pad-bot margin-top">
+            </div>
+            <div class = "container full center relative">
+                <div class = "full flex relative center center-text horizontal-pad flex-responsive">
+                    <h2 class = "full margin-bot">Rakitić: Klasa iz sjene</h2>
+                    <!-- <p class = "full text-container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac sollicitudin quam, id mattis odio. Cras mattis lacus lectus, vel condimentum velit fringilla nec. Vestibulum tempor eros sit amet neque efficitur, eu congue massa lobortis. Fusce sollicitudin sollicitudin tempor. Nulla at diam ut elit fringilla efficitur sit amet eu massa.</p> -->
+                </div>
+                <!-- <div class="full center relative">
+                    <a class="cta" href="#">Pročitaj više...</a>
+                </div> -->
+                <div class = "full center relative">
+                    <div class="button-48"><span class="text">Pročitaj više...</span></div>
+                </div>
+            </div>
+        </a>
+    </section>
+    <!--Footer-->
+        <footer class="full flex relative">
+            <div class="container flex relative">
+                <div class="full flex-responsive relative flex">
+                    <div class="full flex relative horizontal-pad center">
+                        <img src="<?php echo $native_path ?>img/logo_MIC.png" aria-hidden="true" class="footer-logo">
+                        <img src="<?php echo $native_path ?>img/tg_studio.png" aria-hidden="true" class="footer-logo">
+                    </div>
+                </div>
+                <div class="full flex-responsive relative flex">
+                    <div class="full center relative horizontal-pad">
+                    <p class="full footer-text center-text">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing, u suradnji s partnerom Jack Daniels i po najvišim uredničkim standardima Telegram Media Grupe.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
