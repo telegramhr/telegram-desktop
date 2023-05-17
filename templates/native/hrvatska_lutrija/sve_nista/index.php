1e110ce2129e61909119bb678e8b525d9b52acfa
Marijan Mikolić <marijan.mikolic@gmail.com>
2023-05-17 16:10:53 +0200

    Native Lutrija v1.0 - launch

---
+++ templates/native/hrvatska_lutrija/sve_nista/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<?php
+//$native_path = get_stylesheet_directory_uri() . '/templates/native/hrvatska_lutrija/sve_nista/';
+$native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/hrvatska_lutrija/sve_nista/';
+//$native_path = 'http://staging.telegram.hr/wp-content/themes/telegram-desktop/templates/native/hrvatska_lutrija/sve_nista/';
+//$native_path = 'http://localhost/telegram-desktop/templates/native/hrvatska_lutrija/sve_nista/';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>img/aos.css?v=1.00" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@300;400;800&display=swap" rel="stylesheet">
+    <script src="<?php echo $native_path ?>img/aos.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>tmg_framework.css?v=1.00" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>style.css?v=1.20" type="text/css" />
+    <script src="<?php echo $native_path ?>slick/slick.js"></script>
+    <link rel="stylesheet" href="<?php echo $native_path ?>slick/slick.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>slick/slick-theme.css" type="text/css" />
+    <script src="<?php echo $native_path ?>functions.js?v=1.10"></script>
+</head>
+
+<body>
+    <div class="main-container flex relative">
+        <header class="full flex relative">
+            <div class="pattern-bg"></div>
+            <h3 class="full center-text">Uvijek idem na</h3>
+            <h1 class="full center-text">Sve ili ništa</h1>
+            <p class="full center-text">Pronašli smo ljude koje su čak i beznadni trenuci nagradili s malo sreće.</p>
+        </header>
+        <!-- (c) 2000-2023 Gemius SA version 2.0 Impressions: campaign: MP_HL_LUTRIJSKE IGRE_Sve ili Ništa_2flight, placement: /telegram/microsite , creative: telegram_microsite  -->
+        <IMG
+            SRC="https://gdehr.hit.gemius.pl/_[TIMESTAMP]/redot.gif?id=bOrldtRaCbFy3c0Jh5qlkpew7zDIICcsf4KAUG7bVC3.W7/fastid=hamtrdxaywprriinlhtqsqbgdhlx/stparam=tdqggrggub/nc=0/gdpr=0/gdpr_consent=" />
+        <section class="full flex relative">
+            <div class="container flex relative">
+                <div class="full relative gallery">
+                    <div class="flex relative column-pad animate">
+                        <div class="full flex relative">
+                            <div class="full relative youtube-embed">
+                                <iframe width="315" height="560"
+                                    src="https://www.youtube-nocookie.com/embed/HkcfP1P9Fzg"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                            <div class="deco-line"></div>
+                            <p class="full">Lana se ozlijedila, Dinu je propao put, a Matei stan. No, ni za čim ne žale.
+                                Evo zašto</p>
+                        </div>
+                    </div>
+                    <div class="flex relative column-pad animate">
+                        <div class="full flex relative">
+                            <div class="full relative youtube-embed">
+                                <iframe width="315" height="560"
+                                    src="https://www.youtube-nocookie.com/embed/krmXt-0NMMo"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                            <div class="deco-line"></div>
+                            <p class="full">Karla je zeznula prijemni, Luka nogu, a Veronika smještaj. No, izvukli su
+                                deblji kraj. Evo kako</p>
+                        </div>
+                    </div>
+                    <div class="flex relative column-pad animate">
+                        <div class="full flex relative">
+                            <div class="full relative youtube-embed">
+                                <iframe width="315" height="560"
+                                    src="https://www.youtube-nocookie.com/embed/1oFXzy6FitM"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                            <div class="deco-line"></div>
+                            <p class="full">Tihana je izgubila smještaj, Filipu je crkao auto, Magdaleni je propao let.
+                                No, pamtit će ta putovanja zauvijek</p>
+                        </div>
+                    </div>
+                    <div class="flex relative column-pad animate">
+                        <div class="full flex relative">
+                            <div class="full relative youtube-embed">
+                                <iframe width="315" height="560"
+                                    src="https://www.youtube-nocookie.com/embed/HkcfP1P9Fzg"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                            <div class="deco-line"></div>
+                            <p class="full">Lana se ozlijedila, Dinu je propao put, a Matei stan. No, ni za čim ne žale.
+                                Evo zašto</p>
+                        </div>
+                    </div>
+                    <div class="flex relative column-pad animate">
+                        <div class="full flex relative">
+                            <div class="full relative youtube-embed">
+                                <iframe width="315" height="560"
+                                    src="https://www.youtube-nocookie.com/embed/krmXt-0NMMo"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                            <div class="deco-line"></div>
+                            <p class="full">Karla je zeznula prijemni, Luka nogu, a Veronika smještaj. No, izvukli su
+                                deblji kraj. Evo kako</p>
+                        </div>
+                    </div>
+                    <div class="flex relative column-pad animate">
+                        <div class="full flex relative">
+                            <div class="full relative youtube-embed">
+                                <iframe width="315" height="560"
+                                    src="https://www.youtube-nocookie.com/embed/1oFXzy6FitM"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                            <div class="deco-line"></div>
+                            <p class="full">Tihana je izgubila smještaj, Filipu je crkao auto, Magdaleni je propao let.
+                                No, pamtit će ta putovanja zauvijek</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="full center relative"><a href="https://www.lutrija.hr/lotoigre/sve-ili-nista" target="_blank"
+                class="full center"><img class="full animate lutrija-btn"
+                    src="<?php echo $native_path ?>img/igraj_tipka.png" alt="Tipka Igraj Sve ili ništa"></a></div>
+        <section class="full flex relative column-margin lutrija-element">
+            <div class="pattern-bg"></div>
+            <div class="container flex relative column-pad">
+                <a href="https://www.lutrija.hr/lotoigre/sve-ili-nista" target="_blank" class="full center"><img
+                        class="full animate lutrija-img" src="<?php echo $native_path ?>img/banner3.jpg"
+                        alt="Jackpot Hrvatske lutrije: pogodio ili ne, dobitno je!"></a>
+                <h2 class="full center-text">Čak i kad ti ništa ne ide od ruke, možeš pobijediti</h2>
+                <p class="limited-width center-text">Često nam se čini da nam ništa ne ide od ruke. Tada obično znamo
+                    reći da je za sve kriv Murphyjev zakon koji glasi "Ako nešto može poći po krivu, poći će po krivu'.
+                    No, mnogi ljudi opiru se takvim mislima. Oni ne mare za pehove nego bez obzira na mogući crni
+                    scenarij uvijek 'igraju' na <a href="https://www.lutrija.hr/lotoigre/sve-ili-nista"
+                        target="_blank">Sve ili Ništa.</a>
+            </div>
+        </section>
+        <section class="full flex relative column-margin">
+            <div class="container flex relative">
+                <a class="full flex relative content-card column-margin" target="_blank"
+                    href="https://www.telegram.hr/promo/dok-birate-prave-brojeve-evo-par-hit-izjava-nasih-loto-dobitnika-djeci-sam-rekla-da-paze-da-pas-ne-pojede-listic/">
+                    <img class="full animate"
+                        src="https://images.telegram.hr/GMb6UJfHyWoSCmCoqNOgsg4-QeMPtBHc--Mc8e_Qj6A/preset:single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjIvMTAvc2h1dHRlcnN0b2NrLTE2MDY1NDY2MDAuanBn.webp"
+                        alt="Fotofrafija mladog para oduševljenog što su na mobitelu dobili informaciju da su loto dobitnici">
+                    <div class="half center flex-responsive">
+                        <div class="full flex white-bg column-pad">
+                            <h2 class="full">Dok birate prave brojeve, evo par hit izjava naših loto dobitnika: 'Djeci
+                                sam rekla da paze da pas ne pojede listić' </h2>
+                            <p> Iako zvuči nevjerojatno, većina ih je na prvu rekla da ne znaju što će učiniti s
+                                dobitkom
+                            </p>
+                        </div>
+                    </div>
+                </a>
+                <a class="full flex relative content-card column-margin" target="_blank"
+                    href="https://www.telegram.hr/partneri/pogledajte-sto-smo-izvukli-iz-arhive-pedesetih-smo-na-igrama-na-srecu-mogli-osvojiti-svinje-i-bacve-vina/">
+                    <img class="full animate"
+                        src="https://images.telegram.hr/DAkUWjw5cdBT-3cWRffTrzcy54TaS9wYJr9Vlr0q9H0/preset:single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjIvMDcvdGctbmFzbG92bmEta29sYXo3LTE4LTEuanBn.jpg"
+                        alt="Srećke Hrvatske lutrije u prošlom stoljeću">
+                    <div class="half center flex-responsive">
+                        <div class="full flex white-bg column-pad">
+                            <h2 class="full">Pogledajte što smo izvukli iz arhive: pedesetih smo na igrama na sreću
+                                mogli osvojiti svinje i bačve vina</h2>
+                            <p>Ovo je prefora, ovako su izgledale srećke u prošlom stoljeću</p>
+                        </div>
+                    </div>
+                </a>
+                <a class="full flex relative content-card column-margin" target="_blank"
+                    href="https://www.telegram.hr/preview/1293409">
+                    <img class="full animate"
+                        src="https://images.telegram.hr/_YxNeZfiZz9yPI8qK1lNqHu1kzyREGXQTqa34kqXjeQ/preset:single2/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjIvMDgvc2h1dHRlcnN0b2NrLTEyNjE1MTM5ODcuanBn.jpg"
+                        aria-hidden="true">
+                    <div class="half center flex-responsive">
+                        <div class="full flex white-bg column-pad">
+                            <h2 class="full">Čuvajte se urnebesnih Murphyjevih zakona. Donosimo deset najzabavnijih koji
+                                vas mogu zadesiti</h2>
+                            <p>'Ako postoji način da nešto učine pogrešno, učinit će', slavna je izjava inženjera
+                                Edwarda Murpheyja isfrustriranog radom njegovog tima </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </section>
+        <footer class="full flex relative">
+            <div class="container flex relative column-pad column-margin">
+                <div class="full center logo-line">
+                    <a href="https://www.lutrija.hr/" target="_blank"><img
+                            src="<?php echo $native_path ?>img/lutrija_logo.png" class="animate"
+                            alt="Hrvatska Lutrija"></a>
+                    <a href="https://www.lutrija.hr/" target="_blank"><img src="<?php echo $native_path ?>img/dop.png"
+                            class="animate" alt="Dobno ograničenje: 18+"></a>
+                    <a href="https://telegramgrupa.hr" target="_blank"><img
+                            src="<?php echo $native_path ?>img/tg_studio.png" class="animate" alt="Telegram Studio"></a>
+                </div>
+                <p class="full center-text limited-width">Producirano u radionici TG Studija, Telegramove in-house
+                    agencije za nativni marketing, u suradnji s partnerom Hrvatska Lutrija i po najvišim uredničkim
+                    standardima Telegram Media Grupe.</p>
+            </div>
+        </footer>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
