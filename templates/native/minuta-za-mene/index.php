5dcd3fa4c800fb13c9ef2e3c04dcb32fcfe4dbff
Marko Banušić <mbanusic@gmail.com>
2023-09-22 15:42:44 +0200

    minuta za mene

---
+++ templates/native/minuta-za-mene/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="hr">
+<?php
+    // use this native path to embed all the files from project that are included with project, images, scripts, ...
+    // we will change this when we upload the project to the server
+    // this way we only need to change in one place
+    $native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/minuta-za-mene/';
+    // use this for versioning, to avoid cache problems
+    $version = 1;
+?>
+<head>
+
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo $native_path ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/animated.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/owl.css">
+
+  </head>
+
+<body>
+
+  <div class="main-banner" id="top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-5 align-self-center">
+              <div class="left-content">
+                <div class="row">
+                  <div class="col-lg-12 text-left">
+                    <img src="<?php echo $native_path ?>/assets/images/logo.png" alt="" class="keyvizual">
+                    <h2 class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">Svaka četvrta žena u Hrvatskoj kojoj je dijagnosticiran rak, ima rak dojke.</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-7 align-self-center">
+              <div class="left-content">
+                <div class="row">
+                  <div class="col-lg-12 text-left padinzh">
+                    <p class="wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">Rak dojke vodeći je po broju i smrtnosti među ženama.<br/>
+                      Pravovremeni početak terapije uspješno liječi u više od 90 posto slučajeva.<br/>
+                      Ključan korak? Samopregled dojki.<br/>
+                      Za njega nam je potrebno svega - 60 sekundi.</p>
+
+                  </div>
+                  <div class="col-lg-12 text-left wow fadeIn" data-wow-duration="1s" data-wow-delay="2s">
+                    <div class="border-first-button">
+                      <button class="border-first-button" id="startButton">Pokreni minutu za mene</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="element1" class="blog">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 show-up prednost wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+          <img src="<?php echo $native_path ?>/assets/images/01.png" alt="">
+        </div>
+        <div class="col-lg-7 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.7s">
+          <h2>Stani ispred ogledala</h2>
+          <p>Stavi ruku na bokove, stisni prsne mišiće i vizualno pregledaj dojke. Obrati pažnju na veličinu, oblik i boju i traži bilo kakvu promjenu, uključujući kvržice, promjene u boji ili izgledu kože te izbočine ili udubine. Podigni desnu ruku kako bi detaljno pregledala desnu dojku. Isto učini i s lijevom. Nježno pritisni bradavicu i promotri javlja li se iscjedak.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="element2" class="blog">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 order-lg-1 order-2 show-up prednost wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+          <h2>Stani uspravno</h2>
+          <p>Tijelo namaži mlijekom ili losionom. Vrijeme je za pipanje dojki ili palpaciju. Za pregled lijeve dojke koristi desnu, a za pregled desne dojke lijevu ruku. Spoji jagodice tri srednja prsta i započni s kružnim pokretima. Kreni od vanjskih rubova dojke prema bradavicama ili obrnuto, bitno je da pregledaš cijelu površinu. Neka ti pokreti najprije budu nježni i površinski, a zatim čvršći i dublji. Obavezno ispipaj i prostor iznad i ispod dojki te pazušnu jamu. Ovaj korak možeš učiniti i u tušu, samo se dobro nasapunaj.</p>
+        </div>
+        <div class="col-lg-5 order-lg-2 order-1 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.7s">
+          <img src="<?php echo $native_path ?>/assets/images/02.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="element3" class="blog">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 show-up prednost wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+          <img src="<?php echo $native_path ?>/assets/images/03.png" alt="">
+        </div>
+        <div class="col-lg-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.7s">
+          <h2>Namjesti se u ležeći položaj</h2>
+          <p>Stavi jastuk ili složeni ručnik ispod ramena, a ruku na kojoj ležiš iznad glave. Ponovno spoji tri srednja prsta suprotne ruke te kreni s nježnim kružnim pokretima, a zatim manjim i čvršćim. Završi pregled opipavanjem područja ispod pazuha tako što ćeš ruku koja miruje i na kojoj ležiš u potpunosti opustiti, a suprotnom rukom duboko opipati cijeli pazuh. Ponovi postupak na drugoj dojci.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="element4" class="blog">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2 text-center show-up prednost wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+          <p style="font-weight: 700; font-size: 22px;">Ukoliko si primjetila neku promjenu ili kvržicu, obrati se svom liječniku.</p>
+          <p style="font-weight: 700; font-size: 22px;">On će te pregledati i, ukoliko je potrebno, naručiti daljnje pretrage.</p>
+          <p style="font-weight: 700; font-size: 22px;">I ne zaboravi, barem jednom mjesečno, izdvojiti minutu za sebe.</p>
+          <p>&nbsp;</p>
+          <img src="<?php echo $native_path ?>/assets/images/logo.png" alt="" style="width:auto; height:120px;">
+        </div>
+
+      </div>
+    </div>
+  </div>
+    <div id="progress-container">
+        <div id="progress-bar"></div>
+    </div>
+
+
+  <!-- Scripts -->
+  <script src="<?php echo $native_path ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo $native_path ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/owl-carousel.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/animation.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/imagesloaded.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/custom.js"></script>
+    <script>
+        var currentId = 1;
+        var maxId = 4;
+        var intervalId; // To hold the interval ID
+
+        // Function to scroll to the next element
+        function scrollToNextElement() {
+            var currentElement = document.getElementById("element" + currentId);
+            if (currentElement) {
+                currentElement.scrollIntoView({ behavior: "smooth" });
+            }
+
+            currentId++;
+
+            if (currentId > maxId) {
+                clearInterval(intervalId); // Stop the interval when the last element is reached
+            }
+        }
+
+        // Function to start automatic navigation
+        function startNavigation() {
+            // Scroll to the first element instantly
+            var firstElement = document.getElementById("element1");
+            if (firstElement) {
+                firstElement.scrollIntoView({ behavior: "auto" });
+            }
+
+            // Show the progress bar container
+            var progressContainer = document.getElementById("progress-container");
+            if (progressContainer) {
+                progressContainer.style.display = "block";
+            }
+
+            // Set up the progress bar
+            var progressBar = document.getElementById("progress-bar");
+            var progress = 0;
+            var intervalDuration = 100; // 100 milliseconds
+            var totalTime = 60000; // 60 seconds
+
+            intervalId = setInterval(function () {
+                progress += (intervalDuration / totalTime) * 100;
+                progressBar.style.width = progress + "%";
+
+                if (progress >= 100) {
+                    clearInterval(intervalId); // Stop the interval when the progress reaches 100%
+                    progressContainer.style.display = "none"; // Hide the progress bar container
+                }
+            }, intervalDuration);
+
+            // Wait for a very short time (instantly) before starting the interval
+            setTimeout(function () {
+                currentId++; // Move to the next element
+                intervalId = setInterval(scrollToNextElement, 20000); // Start the interval
+            }, 10);
+        }
+
+        // Add a click event listener to the start button
+        var startButton = document.getElementById("startButton");
+        if (startButton) {
+            startButton.addEventListener("click", startNavigation);
+        }
+    </script>
+        <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_footer(); ?>
+</body>
+</html>
