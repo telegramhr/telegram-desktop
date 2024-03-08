d8a1569a99f2a5a231ce5eaad7626cdfb3729bfc
Marko Banušić <mbanusic@gmail.com>
2024-03-08 14:58:06 +0100

    update biram sebe

--- templates/native/biram-sebe/assets/js/script.js
+++ templates/native/biram-sebe/assets/js/script.js
@@ -16,6 +16,118 @@ window.addEventListener('scroll', function() {
 });
 
 
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-2');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + scrollPosition / 1 + 'px)'; 
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-kontra-2');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + (-scrollPosition / 1) + 'px)';
+});
+
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-3');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + scrollPosition / 1 + 'px)'; 
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-kontra-3');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + (-scrollPosition / 1) + 'px)';
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-4');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + scrollPosition / 1 + 'px)'; 
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-kontra-4');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + (-scrollPosition / 1) + 'px)';
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-5');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + scrollPosition / 1 + 'px)'; 
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-kontra-5');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + (-scrollPosition / 1) + 'px)';
+});
+
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-6');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + scrollPosition / 1 + 'px)'; 
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-kontra-6');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + (-scrollPosition / 1) + 'px)';
+});
+
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-7');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + scrollPosition / 1 + 'px)'; 
+});
+
+
+window.addEventListener('scroll', function() {
+  var scrollTransform = document.querySelector('.scroll-transform-kontra-7');
+  var scrollPosition = window.scrollY;
+
+  // Primena transformacije na osnovu vrednosti skrolovanja
+  scrollTransform.style.transform = 'translateX(' + (-scrollPosition / 1) + 'px)';
+});
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   const marqueeContent = document.querySelector('.marquee-content');
   const originalText = marqueeContent.textContent;
@@ -27,3 +139,82 @@ document.addEventListener('DOMContentLoaded', function () {
 
   marqueeContent.textContent = newText;
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const marqueeContent = document.querySelector('.marquee-content-2');
+  const originalText = marqueeContent.textContent;
+  let newText = '';
+
+  for (let i = 0; i < 50; i++) { // U ovom slučaju 50 je proizvoljan broj ponavljanja teksta
+    newText += originalText + ' ';
+  }
+
+  marqueeContent.textContent = newText;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const marqueeContent = document.querySelector('.marquee-content-3');
+  const originalText = marqueeContent.textContent;
+  let newText = '';
+
+  for (let i = 0; i < 50; i++) { // U ovom slučaju 50 je proizvoljan broj ponavljanja teksta
+    newText += originalText + ' ';
+  }
+
+  marqueeContent.textContent = newText;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const marqueeContent = document.querySelector('.marquee-content-4');
+  const originalText = marqueeContent.textContent;
+  let newText = '';
+
+  for (let i = 0; i < 50; i++) { // U ovom slučaju 50 je proizvoljan broj ponavljanja teksta
+    newText += originalText + ' ';
+  }
+
+  marqueeContent.textContent = newText;
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const marqueeContent = document.querySelector('.marquee-content-5');
+  const originalText = marqueeContent.textContent;
+  let newText = '';
+
+  for (let i = 0; i < 50; i++) { // U ovom slučaju 50 je proizvoljan broj ponavljanja teksta
+    newText += originalText + ' ';
+  }
+
+  marqueeContent.textContent = newText;
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const marqueeContent = document.querySelector('.marquee-content-6');
+  const originalText = marqueeContent.textContent;
+  let newText = '';
+
+  for (let i = 0; i < 50; i++) { // U ovom slučaju 50 je proizvoljan broj ponavljanja teksta
+    newText += originalText + ' ';
+  }
+
+  marqueeContent.textContent = newText;
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const marqueeContent = document.querySelector('.marquee-content-7');
+  const originalText = marqueeContent.textContent;
+  let newText = '';
+
+  for (let i = 0; i < 50; i++) { // U ovom slučaju 50 je proizvoljan broj ponavljanja teksta
+    newText += originalText + ' ';
+  }
+
+  marqueeContent.textContent = newText;
+});
+
+
--- templates/native/biram-sebe/index.php
+++ templates/native/biram-sebe/index.php
@@ -21,6 +21,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="<?php echo $native_path ?>/assets/images/favicon.png">
+
     <!-- Bootstrap core CSS -->
     <link href="<?php echo $native_path ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
@@ -65,29 +66,235 @@
       </div>
   </div>
 </div>
+
 <div id="vise" class="blog" style="padding-bottom: 0px !important;">
+  <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 30px; overflow: hidden;">
+
+    <div class="row">
+      <div class="col-lg-12 text-center" style="padding-bottom:120px">
+
+
+        <div class="marquee">
+          <div class="marquee-content">OPĆE STANJE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        </div>
+
+
+
+        </div>
+      <div class="col-lg-12 text-center scroll-transform">
+          <h1>Tek svaka 10. žena je sasvim zadovoljna sobom</h1>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-kontra">
+          <h1 style="font-size: 34px !important; font-weight: 200;"><span style="font-weight: 900;">44%</span> danas ima manje samopouzdanja nego prije 5 godina</span></h1>
+        </div>
+
+      <div class="col-lg-4 offset-lg-4 text-center" >
+
+
+        <p style="margin-top: 50px;">Koliko god se trudim, ne pomičem se i to jako utječe na moje samopouzdanje jer imam osjećaj kao da sam sama sebe iznevjerila, ostvarila puno manje od ljudi za koje smatram da ne vrijede više.
+          <br/>
+        <svg style="width:70px;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FormatQuoteIcon"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"></path></svg>
+      </p>
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog" style="padding-bottom: 0px !important;">
+  <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 30px; overflow: hidden;">
+
+    <div class="row">
+      <div class="col-lg-12 text-center" style="padding-bottom:120px">
+
+
+        <div class="marquee-2">
+          <div class="marquee-content-2">FIZIČKI IZGLED&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        </div>
+
+
+
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-2">
+          <h1>Više od trećine izbjegava razne aktivnosti zbog izgleda</h1>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-kontra-2">
+          <h1  style="font-size: 34px !important; font-weight: 200;">poput izlazaka, seksa, teretane, druženja, kupanja...</h1>
+        </div>
+      <div class="col-lg-4 offset-lg-4 text-center" >
+
+
+        <p style="margin-top: 50px;">Ne idem na plažu jer se sramim kako izgledam u kupaćem, ne idem u prirodu jer čekam dan kad će mi tajice stajati bolje, ne idem na druženja jer se brinem hoće li ljudi misliti kako sam ostarila. Na poslovnim sastancima gledam u pod jer me sram kako mi izgleda kosa, kako mi stoji odjeća.
+          <br/>
+        <svg style="width:70px;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FormatQuoteIcon"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"></path></svg>
+      </p>
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog" style="padding-bottom: 0px !important;">
+  <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 30px; overflow: hidden;">
+
+    <div class="row">
+      <div class="col-lg-12 text-center" style="padding-bottom:120px">
+
+
+        <div class="marquee-3">
+          <div class="marquee-content-3">POSAO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        </div>
+
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-3">
+          <h1>Gotovo svaka druga je pod visokim stresom</h1>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-kontra-3">
+          <h1  style="font-size: 34px !important; font-weight: 200;">Posao je vodeći <span style="font-weight: 900;">uzrok stresa</span> i nezadovoljstva</h1>
+        </div>
+      <div class="col-lg-4 offset-lg-4 text-center" >
+
+
+        <p style="margin-top: 50px;">Ako nisam u svakom zadatku izvrsna i najbolja onda mislim da ne vrijedim dovoljno za taj posao. Također, moram podmetnuti leđa za cijeli tim i biti na usluzi kolegama i kolegicama, jer više vrijedim ako sam od koristi drugima nego da se borim za svoje ciljeve.
+          <br/>
+        <svg style="width:70px;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FormatQuoteIcon"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"></path></svg>
+      </p>
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog" style="padding-bottom: 0px !important;">
+  <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 30px; overflow: hidden;">
+
+    <div class="row">
+      <div class="col-lg-12 text-center" style="padding-bottom:120px">
+
+
+        <div class="marquee-4">
+          <div class="marquee-content-4">DRUŠTVO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        </div>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-4">
+          <h1>Svaka treća osjeća se manje vrijedno</h1>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-kontra-4">
+          <h1  style="font-size: 34px !important; font-weight: 200;">kada sluša <span style="font-weight: 900;">priče o uspjehu</span> svojih prijatelja</h1>
+        </div>
+      <div class="col-lg-4 offset-lg-4 text-center" >
+
+
+        <p style="margin-top: 50px;">Radim kao spremačica. Mi smo često zamišljene kao žene nižeg obrazovanja ili niže inteligencije. U tom smislu znam čuti komentare tipa 'A kaj ti znaš o tome, ti čistiš tu.'
+          <br/>
+        <svg style="width:70px;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FormatQuoteIcon"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"></path></svg>
+      </p>
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
+<div class="blog" style="padding-bottom: 0px !important;">
+  <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 30px; overflow: hidden;">
+
+    <div class="row">
+      <div class="col-lg-12 text-center" style="padding-bottom:120px">
+
+
+        <div class="marquee-5">
+          <div class="marquee-content-5">INTIMNI ODNOSI&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SEKS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        </div>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-5">
+          <h1>Na svaku drugu utječu komentari partnera</h1>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-kontra-5">
+          <h1  style="font-size: 34px !important; font-weight: 200;"><span style="font-weight: 900;">36%</span> je neugodno skinuti se pred partnerom</h1>
+        </div>
+      <div class="col-lg-4 offset-lg-4 text-center" >
+
+
+        <p style="margin-top: 50px;">Seksualni život mi stagnira jer se ne osjećam dovoljno zgodnom i osjećam se loše s par kila viška i neutegnutim tijelom, mada partner stalno govori da sam mu taman i da sam mu zgodna. Izbjegavam seks i znam da je to loše.
+          <br/>
+        <svg style="width:70px;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FormatQuoteIcon"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"></path></svg>
+      </p>
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="blog" style="padding-bottom: 0px !important;">
+  <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 30px; overflow: hidden;">
+
+    <div class="row">
+      <div class="col-lg-12 text-center" style="padding-bottom:120px">
+
+
+        <div class="marquee-6">
+          <div class="marquee-content-6">RODITELJSTVO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        </div>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-6">
+          <h1>Svaka druga majka nema vremena za svoje potrebe</h1>
+        </div>
+      <div class="col-lg-12 text-center scroll-transform-kontra-6">
+          <h1  style="font-size: 34px !important; font-weight: 200;"><span style="font-weight: 900;">35%</span> smatra da druge majke sve postižu bolje od nje</h1>
+        </div>
+      <div class="col-lg-4 offset-lg-4 text-center" >
+
+        <p style="margin-top: 50px;">Nedovoljno vježbam i nemam više svoje tijelo. Kad pomislim na to iznerviram se jer znam koliko truda i vremena moram uložit da se vratim na svoje, a to ne mogu s četiri djeteta u dobi od tri mjeseca do 6 godina.
+
+          <br/>
+        <svg style="width:70px;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-dhaba5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FormatQuoteIcon"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"></path></svg>
+      </p>
+
+
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="blog" style="padding-bottom: 0px !important;">
   <div class="container-fluid" style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%); z-index: 100; position: relative; padding-top: 30px; padding-bottom: 120px; overflow: hidden;">
 
     <div class="row">
       <div class="col-lg-12 text-center" style="padding-bottom:150px">
 
 
-        <div class="marquee">
-          <div class="marquee-content">USKORO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        <div class="marquee-7">
+          <div class="marquee-content-7">USKORO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
         </div>
 
 
 
         </div>
-      <div class="col-lg-12 text-right scroll-transform">
+      <div class="col-lg-12 text-right scroll-transform-7">
           <h1>Više rezultata i priča</h1>
         </div>
-      <div class="col-lg-12 text-right scroll-transform-kontra">
+      <div class="col-lg-12 text-right scroll-transform-kontra-7">
           <h1>dolazi uskoro...</h1>
         </div>
     </div>
   </div>
 </div>
+
+
+
 
 
   <div id="clanci" class="blog" style="z-index: 100; position: relative; padding-top: 0px; padding-bottom: 120px; overflow: hidden;">
@@ -111,7 +318,7 @@
   <script src="<?php echo $native_path ?>/assets/js/animation.js"></script>
   <script src="<?php echo $native_path ?>/assets/js/imagesloaded.js"></script>
   <script src="<?php echo $native_path ?>/assets/js/custom.js"></script>
-  <script src="<?php echo $native_path ?>/assets/js/script.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/script.js?v=2"></script>
 
     <!-- this is required to load all the analytics and other scripts -->
     <?php wp_footer(); ?>
