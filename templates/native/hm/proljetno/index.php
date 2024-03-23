a6698866e0881a932b7946407a76f28e748231ec
Marko Banušić <mbanusic@gmail.com>
2024-03-23 14:59:33 +0100

    hm native

---
+++ templates/native/hm/proljetno/index.php
@@ -0,0 +1,222 @@
+
+<!DOCTYPE html>
+<html lang="hr">
+<?php
+    // use this native path to embed all the files from project that are included with project, images, scripts, ...
+    // we will change this when we upload the project to the server
+    // this way we only need to change in one place
+    $native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/hm/proljetno';
+    // use this for versioning, to avoid cache problems
+    $version = 1;
+?>
+
+<head>
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_head(); ?>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo $native_path ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/style.css?v=14032024v14">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/animated.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/owl.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target="#myScrollspy" data-offset="15" class="pozadina">
+<?php do_action('telegram_body_start'); ?>
+  <!-- ***** Preloader Start ***** -->
+
+<div style="top:30px; left:30px; position: absolute; z-index: 10;"><img src="<?php echo $native_path ?>/assets/images/super1.png" ></div>
+
+  <div class="blog hero">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">
+               <img src="<?php echo $native_path ?>/assets/images/claim.svg" style="padding: 30px" /><br/>
+        </div>
+        <div class="col-lg-6" data-wow-duration="1s" data-wow-delay="1s">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog">
+  <div class="container">
+      <div class="row">
+
+          <div class="col-lg-8 offset-lg-2 text-left wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+            <h2>Proljeće je doba novih početaka i ponovnog rođenja, a ove sezone osim u prirodi procvat želimo doživjeti i u - našim ormarima. </h2>
+             <p class="sticky-top" style=" font-weight: 500;">Inspiraciju smo potražili u nostalgiji za nekim prošlim vremenima koja u svojim godinama skrivaju stilsko blago i ponovno podsjećaju na to da je ponekad poželjno pogledati unatrag. <a href="https://www2.hm.com/en_eur/free-form-campaigns/1019-ss-2024-women.html#/women/view" target="_blank" style="font-weight:700 !important; text-decoration:underline !important; color: #000 !important">Proljetna H&amp;M kolekcija</a> donosi komade koji neodoljivo podsjećaju na trendove iz prošlih desetljeća, a istovremeno idealno nadopunjuju svaku suvremenu garderobu.
+              </p>
+          </div>
+        </div>
+      </div>
+</div>
+
+
+<div class="blog" style="padding-top:0px">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-2">
+          </div>
+          <div class="col-lg-3">
+             <p class="sticky-top tekstulja"><img src="<?php echo $native_path ?>/assets/images/sesdesete.svg"  class="wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="0.5s" /><br/><br/><span class="wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="1s">Ovo desetljeće donijelo je velike promjene u društvo, ali i u modu. Iako je još uvijek vladala strogost prošlosti koja se zadržala u vidu tradicionalne siluete, s druge strane je bujala nova filozofija slobode. U ovoj kolekciji pravi primjer spoja klasike i suvremenosti šezdesetih predstavlja matching komplet u obliku pencil suknje i košulje. Klasičan kroj nadograđen je inovativnim korištenjem materijala koji podsjeća na čipku, a rupičasta struktura uz to uvodi i elemente intrige i zavodljivosti.</span>
+              </p>
+          </div>
+          <div class="col-lg-7 wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/sesdesete.jpg" style="z-index:1 !important" />
+          </div>
+        </div>
+      </div>
+</div>
+
+
+<div class="blog" style="background:#000; padding-bottom: 0;">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-12 paddingdolje">
+                <div class="container">
+                  <div class="row">
+                      <div class="col-lg-4 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+
+                        <img src="<?php echo $native_path ?>/assets/images/sedamdesete.svg"/>
+
+                      </div>
+                      <div class="col-lg-1">
+                      </div>
+                      <div class="col-lg-5 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="1s">
+
+                        <p style="color: #fff; vertical-align: center;">Era u kojoj je u svakoj sferi dominirala filozofija slobode u modni je fokus stavila opuštene krojeve i boho estetiku. Ovoga proljeća vraćamo se u sedamdesete i dajemo im suvremeni twist pa nosimo komade kao što su lepršave haljine, bluze s širokim rukavima i romantične, voluminozne suknje. Idealna nadopuna za ovakve kombinacije je chunky nakit koji daje finalni stilski pečat. </p>
+                      </div>
+                      <div class="col-lg-2">
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+          <div class="col-lg-4 p-0 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-01.jpg" />
+          </div>
+          <div class="col-lg-4 p-0 wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-02.jpg" />
+          </div>
+          <div class="col-lg-4 p-0 wow fadeIn" data-wow-duration="1s" data-wow-delay="1.5s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-03.jpg" />
+          </div>
+
+      </div>
+  </div>
+</div>
+
+
+<div class="blog" style="padding-top:0px">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-3 order-lg-2">
+             <p class="sticky-top tekstulja2"><img src="<?php echo $native_path ?>/assets/images/osamdesete.svg"  class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="0.5s" /><br/><br/><span class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1s">Ovo je desetljeće imalo snažan karakter. Vibrantne boje, volumen i blistavi materijali koji podsjećaju na disko kuglu samo su neke modne značajke koje su obilježile osamdesete. Suvremeni stilski pogled na osamdesete u prvi plan stavlja komade poput hlača, suknji ili torbe u metalik izvedbi, voluminozne haljine s pročišćenim i moderniziranim puf elementima te stroge košulje koje podsjećaju na working girl estetiku ovog perioda.</span>
+              </p>
+          </div>
+          <div class="col-lg-7 order-lg-1 p-0 ">
+
+            <div class="image-gallery">
+                <img src="<?php echo $native_path ?>/assets/images/osamdesete-01.jpg"/>
+            </div>
+          </div>
+
+
+          <div class="col-lg-2 order-lg-3">
+          </div>
+        </div>
+      </div>
+</div>
+
+<div class="blog" style="padding-bottom: 0;">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-12 paddingdolje"  style="background: rgb(255,255,255);
+background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(216,205,185,1) 100%);">
+                <div class="container">
+                  <div class="row">
+
+                      <div class="col-lg-5 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+                        <img src="<?php echo $native_path ?>/assets/images/devedesete.svg" />
+                      </div>
+                      <div class="col-lg-1">
+                      </div>
+                      <div class="col-lg-6 align-self-end wow fadeIn" data-wow-duration="1.5s" data-wow-delay="1s">
+                        <p>Posljednje godine prošlog stoljeća danas su jedna od najčešćih referenci u modnom svijetu. Iako postoji puno značajnih stilskih momenata iz ovog desetljeća koji i danas inspiriraju, mi ovoga proljeća biramo komade oprečnih estetika i spajamo ih u moderne outfite. Pod povećalom nam je tako prepoznatljiva baggy silueta traperica koja predstavlja bujanje urbane kulture devedesetih, a kombiniramo je s bluzom u romantic grunge stilu. </p>
+                      </div>
+
+
+                  </div>
+              </div>
+          </div>
+          <div style="background: #d8cdb9" class="col-lg-12 p-0 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <img src="<?php echo $native_path ?>/assets/images/devedesete.jpg" />
+          </div>
+
+      </div>
+  </div>
+</div>
+
+
+  <div id="clanci" class="blog" style="z-index: 100; position: relative; padding-top: 0px; padding-bottom: 120px; overflow: hidden; background: #e2e2e0;">
+    <div class="container-fluid wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+      <div class="row">
+        <div class="col-lg-4 offset-lg-4 text-center">
+              <p>Autor teksta: Petra Okičić<br/>
+              Kreativno vodstvo: TG Studio<br/>
+                Graficko oblikovanje: Reset</p>
+              <p>Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing po najvišim uredničkim standardima Telegram Media Grupe.</p><br/><br/>
+              <img src="<?php echo $native_path ?>/assets/images/telegram.png" style="max-width: 186px;"  />
+          </div>
+        </div>
+      </div>
+  </div>
+  <script type="text/javascript">
+      const images = ["<?php echo $native_path ?>/assets/images/osamdesete-01.jpg", "<?php echo $native_path ?>/assets/images/osamdesete-02.jpg"];
+      let currentImageIndex = 0;
+      const imageElement = document.querySelector(".image-gallery img");
+
+
+      function updateImage() {
+        const imageUrl = images[currentImageIndex];
+        imageElement.style.opacity = 0; // Postavite opacity na 0 prije promjene slike
+        setTimeout(() => {
+          imageElement.src = imageUrl;
+          imageElement.alt = `Slika ${currentImageIndex + 1}`;
+          imageElement.style.opacity = 1; // Postavite opacity na 1 nakon promjene slike
+        }, 500); // Promijenite 1000 ms na željeni vremenski interval za tranziciju
+      }
+
+      function showNextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        updateImage();
+      }
+
+      // Postavite interval za automatsko klizanje svakih 3 sekunde (3000 ms)
+      setInterval(showNextImage, 5000);
+
+      // Inicijalno postavljanje slike
+      updateImage();
+</script>
+  <!-- Scripts -->
+  <script src="<?php echo $native_path ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo $native_path ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/owl-carousel.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/animation.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/imagesloaded.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/custom.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/script.js"></script>
+
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_footer(); ?>
+</body>
+</html>
