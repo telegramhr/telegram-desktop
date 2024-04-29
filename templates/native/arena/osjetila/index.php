6842a9832fa940f265a7de57918db4dc3af18d35
Marko Banušić <mbanusic@gmail.com>
2024-04-29 12:41:05 +0200

    osjetila native

---
+++ templates/native/arena/osjetila/index.php
@@ -0,0 +1,316 @@
+
+<!DOCTYPE html>
+<html lang="hr">
+<?php
+    // use this native path to embed all the files from project that are included with project, images, scripts, ...
+    // we will change this when we upload the project to the server
+    // this way we only need to change in one place
+    $native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/arena/osjetila';
+    // use this for versioning, to avoid cache problems
+    $version = 1;
+?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<head>
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_head(); ?>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo $native_path ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/style.css?v=14032024v14">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/animated.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/owl.css">
+</head>
+
+<body data-spy="scroll" data-target="#myScrollspy" data-offset="15" class="pozadina">
+<?php do_action('telegram_body_start'); ?>
+  <!-- ***** Preloader Start ***** -->
+
+<div style="top:30px; left:30px; position: absolute; z-index: 10;"><img src="<?php echo $native_path ?>/assets/images/super1.png" ></div>
+
+  <div class="blog hero">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">
+               <img src="<?php echo $native_path ?>/assets/images/claim.svg" style="padding: 30px" /><br/>
+        </div>
+        <div class="col-lg-6" data-wow-duration="1s" data-wow-delay="1s">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog">
+  <div class="container">
+      <div class="row">
+
+          <div class="col-lg-8 offset-lg-2 text-left wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+            <h2>Ovoga proljeća želimo probuditi sva svoja osjetila i u potpunosti proživjeti novo doba punim plućima.</h2>
+             <p class="sticky-top" style=" font-weight: 500;">Vrijeme je za buđenje u svakom smislu riječi, pa tako i u - modnom. Kako bismo nadogradili svoju garderobu i spremno dočekali tople, sunčane dane, bacili smo se u
+potragu i u <a href="https://www.arenacentar.hr/" target="_blank">Arena Centru</a> pronašli 5 trendi kombinacija koje će u trenu podići energiju i dati nam #tajnajosjećaj.</p>
+          </div>
+        </div>
+      </div>
+</div>
+
+
+<div class="blog" style="background: #f9cb38;">
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+              <h1><span style="font-weight: 900;"><em>Kušaj</em></span></h1>
+              <p>Možda pomalo neočekivano, ali ove je sezone u svijetu mode zavladala chic polo estetika. Plisirane i utilitarističke suknje sada spajamo s prugastim polo majicama, elegantnim kardiganima i bijelim, sportskim čarapama, a sve nas to, dakako, podsjeća na školske uniforme. Prigodno, jer se ovoga proljeća želimo osjećati kao na praznicima. Vrijeme je da se zabavimo i život ponovno zagrizemo kao najfiniji sladoled!</p>
+          </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center" style="padding:50px;">
+      <div class="row">
+          <div class="col-lg-4 order-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <div class=" sticky-top">
+             <img src="<?php echo $native_path ?>/assets/images/s01.png">
+             </div>
+          </div>
+          <div class="col-lg-4 order-lg-1 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s">
+            <img src="<?php echo $native_path ?>/assets/images/s02.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/s03.jpg" class="sjenanaslici">&nbsp;<br/>
+          </div>
+          <div class="col-lg-4  order-lg-3 wow fadeInRight" data-wow-duration="1s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/s04.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/s05.jpg" class="sjenanaslici"><br/>
+          </div>
+        </div>
+      </div>
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2">
+              <p style="font-size: 13px !important; line-height: 20px !important;">Košulja Ralph Lauren, XYZ Fashion Store; polo majica Ralph Lauren, XYZ Fashion Store; Kratka suknja, Mango; cipele na petu Dune, Kicks &amp; Heelz; narančasta torba Desigual, Fashion &amp; Friends; žuta torba, Fashion &amp; Friends; remen, Reserved</p>
+          </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="blog" style="background: #d10d1d; ">
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+              <h1 style="color: #fff !important"><span style="font-weight: 900;"><em>Dotakni</em></span></h1>
+              <p style="color: #fff;">Crvena je boja i dalje veliki trend, a ovoga proljeća za maksimalni efekt nosimo je u kombinaciji s naglašenom teksturom materijala. Kontrast vatrene nijanse, romantične čipke i glomaznog nakita u zlatnoj izvedbi osigurava sasvim posebno vizualno, ali i taktilno iskustvo.</p>
+              <p style="color: #fff;">Ova nam se kombinacija tako svidjela na prvi pogled, ali i dodir, a samo jednim klikom na novu Arena SPOT mobilnu loyalty aplikaciju za kupce shopping centara Arena Centar i Arena Park prikupili smo bodove vjernosti te tako osigurali buduće pogodnosti i iznenađenja u obliku popusta i nagrada.</p>
+          </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center" style="padding:50px; ">
+      <div class="row">
+          <div class="col-lg-4 order-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <div class=" sticky-top">
+             <img src="<?php echo $native_path ?>/assets/images/a01.png">
+             </div>
+          </div>
+          <div class="col-lg-4 order-lg-1 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s">
+            <img src="<?php echo $native_path ?>/assets/images/a02.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/a03.jpg" class="sjenanaslici">&nbsp;<br/>
+          </div>
+          <div class="col-lg-4  order-lg-3 wow fadeInRight" data-wow-duration="1s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/a04.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/a05.jpg" class="sjenanaslici"><br/>
+          </div>
+        </div>
+      </div>
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2">
+              <p style="color: #fff; font-size: 13px !important; line-height: 20px !important;">Hlače Mango Capsule, Mango; sako, Pinko; srebrne sandale, Mango; zlatne naušnic, Mango; sunčane naočale Dolce&amp;Gabbana, optika Anda; mobitel Redmi Note 13 Pro+, MI Store; nakit, vlasništvo stilista</p>
+          </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog" style="background: #ef0648; ">
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+              <h1 style="color: #fff !important"><span style="font-weight: 900;"><em>Slušaj</em></span></h1>
+              <p style="color: #fff;">Ne želimo samo da nas se vidi, nego da nas se i - čuje. Osim što su melem za oči, modne kombinacije mogu biti i zvučna kulisa za različite proljetne aktivnosti. Ova maksi haljina od najlona čini nam se kao upečatljiv komad za izlazak, a pažnju će privući ne samo izgledom, već i zvukom kod kretanja. Isto vrijedi i za trendi dodatke od metala, a mi biramo nanizane narukvice. Nama zvuči kao najbolji soundtrack ove sezone.</p>
+          </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center" style="padding:50px; ">
+      <div class="row">
+          <div class="col-lg-4 order-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <div class=" sticky-top">
+             <img src="<?php echo $native_path ?>/assets/images/b01.png">
+             </div>
+          </div>
+          <div class="col-lg-4 order-lg-1 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s">
+            <img src="<?php echo $native_path ?>/assets/images/b02.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/b03.jpg" class="sjenanaslici">&nbsp;<br/>
+          </div>
+          <div class="col-lg-4  order-lg-3 wow fadeInRight" data-wow-duration="1s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/b04.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/b05.jpg" class="sjenanaslici"><br/>
+          </div>
+        </div>
+      </div>
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2">
+              <p style="color: #fff; font-size: 13px !important; line-height: 20px !important;">Haljina Diesel, Fashion&amp;Friends; srebrne sandale Nine West, Kicks&amp;Heelz; srebrna torbica, Reserved; narukvice, vlasništvo stilista; slušalice, vlasništvo stilista</p>
+          </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="blog" style="background: #5a406d; ">
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+              <h1 style="color: #fff !important"><span style="font-weight: 900;"><em>Gledaj</em></span></h1>
+              <p style="color: #fff;">Ovog proljeća u budućnost gledamo kroz naočale koje nas asociraju na - prošlost. Naime, devedesete godine prošloga stoljeća vječni su izvor na kojem crpimo modnu inspiraciju, a sada su nas zaintrigirale ove cool sunčane naočale bez okvira koje nas podsjećaju na ponovno trendi modele s kraja prošlog i početka (n)ovog milenija. Vibrantni color block odijela i torbice tu je da nas podsjeti kako je važno biti - vidljiv. </p>
+          </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center" style="padding:50px; ">
+      <div class="row">
+          <div class="col-lg-4 order-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <div class=" sticky-top">
+             <img src="<?php echo $native_path ?>/assets/images/c01.png">
+             </div>
+          </div>
+          <div class="col-lg-4 order-lg-1 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s">
+            <img src="<?php echo $native_path ?>/assets/images/c02.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/c03.jpg" class="sjenanaslici">&nbsp;<br/>
+          </div>
+          <div class="col-lg-4  order-lg-3 wow fadeInRight" data-wow-duration="1s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/c04.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/c05.jpg" class="sjenanaslici"><br/>
+          </div>
+        </div>
+      </div>
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2" data-wow-duration="1s" data-wow-delay="0.5s">
+              <p style="color: #fff; font-size: 13px !important; line-height: 20px !important;">Hlače Paul Smith,  Heraldi for Men & Women; sako Paul Smith,  Heraldi for Men & Women; prsluk, Reserved; sandale, Mango; sunčane naočale Gucci, optika Anda; torba Longchamp, XYZ Fashion Store</p>
+          </div>
+    </div>
+  </div>
+</div>
+
+<div class="blog" style="background: #fcaea2; ">
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+              <h1><span style="font-weight: 900;"><em>Miriši</em></span></h1>
+              <p>Ne možemo zanemariti proljeće kao doba u kojemu priroda ponovno oživljava, buja i - miriši. Svi dobro znamo da je ruža kraljica cvijeća, a ove je sezone procvjetala i na odjeći. Posebno dobro izgleda u raskošnoj 3D izvedbi zbog koje ćete se osjećati kao da ste i sami dio najljepšeg procvjetalog vrta, a ženstvena kombinacija ružičaste i crvene boje još jednom dokazuje da ovaj pomalo zanemareni koloristički spoj funkcionira odlično, posebice u proljeće kada više nosimo jarke boje.</p>
+          </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center" style="padding:50px; ">
+      <div class="row">
+          <div class="col-lg-4 order-lg-2 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+            <div class=" sticky-top">
+             <img src="<?php echo $native_path ?>/assets/images/d01.png">
+             </div>
+          </div>
+          <div class="col-lg-4 order-lg-1 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0s">
+            <img src="<?php echo $native_path ?>/assets/images/d02.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/d03.jpg" class="sjenanaslici">&nbsp;<br/>
+          </div>
+          <div class="col-lg-4  order-lg-3 wow fadeInRight" data-wow-duration="1s" data-wow-delay="1s">
+            <img src="<?php echo $native_path ?>/assets/images/d04.jpg" class="sjenanaslici"><br/>
+            <br/>
+            <img src="<?php echo $native_path ?>/assets/images/d05.jpg" class="sjenanaslici"><br/>
+          </div>
+        </div>
+      </div>
+    <div class="container text-center">
+      <div class="row">
+          <div class="col-lg-8 offset-lg-2">
+              <p style="font-size: 13px !important; line-height: 20px !important;">Kombinezon Mango Capsule, Mango; majica sa cvijetom, Mango; cipele Nine West, Kicks &amp; Heelz; torba, Pinko; parfem Narciso Rodriguez, Douglas</p>
+          </div>
+    </div>
+  </div>
+</div>
+
+
+  <div id="clanci" class="blog" style="z-index: 100; position: relative; padding-top: 120px; padding-bottom: 120px; overflow: hidden; background: #e2e2e0;">
+    <div class="container-fluid wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+      <div class="row">
+        <div class="col-lg-4 offset-lg-4 text-center">
+              <p>
+              Fotografije: Aleksandar Selak<br/>
+              Styling: Studio Onne<br/>
+              Autor teksta: Petra Okičić<br/>
+              Model: Julija Gerenčir<br/>
+              Kosa: Davor Hair & Soul<br/>
+              Makeup: Josipa Drpić</p>
+              <p>Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing po najvišim uredničkim standardima Telegram Media Grupe.</p><br/><br/>
+              <img src="<?php echo $native_path ?>/assets/images/telegram.png" style="max-width: 186px;"  />
+          </div>
+        </div>
+      </div>
+  </div>
+  <script type="text/javascript">
+      const images = ["<?php echo $native_path ?>/assets/images/osamdesete-01.jpg", "<?php echo $native_path ?>/assets/images/osamdesete-02.jpg"];
+      let currentImageIndex = 0;
+      const imageElement = document.querySelector(".image-gallery img");
+
+
+      function updateImage() {
+        const imageUrl = images[currentImageIndex];
+        imageElement.style.opacity = 0; // Postavite opacity na 0 prije promjene slike
+        setTimeout(() => {
+          imageElement.src = imageUrl;
+          imageElement.alt = `Slika ${currentImageIndex + 1}`;
+          imageElement.style.opacity = 1; // Postavite opacity na 1 nakon promjene slike
+        }, 500); // Promijenite 1000 ms na željeni vremenski interval za tranziciju
+      }
+
+      function showNextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        updateImage();
+      }
+
+      // Postavite interval za automatsko klizanje svakih 3 sekunde (3000 ms)
+      setInterval(showNextImage, 5000);
+
+      // Inicijalno postavljanje slike
+      updateImage();
+</script>
+  <!-- Scripts -->
+  <script src="<?php echo $native_path ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo $native_path ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/owl-carousel.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/animation.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/imagesloaded.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/custom.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/script.js"></script>
+
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_footer(); ?>
+</body>
+</html>
