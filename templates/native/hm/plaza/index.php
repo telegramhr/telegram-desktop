59cdafa43897d64b354c487a7fc488d69128155e
Marko Banušić <mbanusic@gmail.com>
2024-05-16 09:21:06 +0200

    hm native

---
+++ templates/native/hm/plaza/index.php
@@ -0,0 +1,258 @@
+
+<!DOCTYPE html>
+<html lang="hr">
+<?php
+    // use this native path to embed all the files from project that are included with project, images, scripts, ...
+    // we will change this when we upload the project to the server
+    // this way we only need to change in one place
+    $native_path = 'https://telegram.hr/wp-content/themes/telegram2-desktop/templates/native/hm/plaza';
+    // use this for versioning, to avoid cache problems
+    $version = 1;
+?>
+
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    <style type="text/css">
+
+        @-webkit-keyframes vrtnja { 0% { -webkit-transform:rotate(0deg) } 100% { -webkit-transform: rotate(360deg);} }
+        @keyframes vrtnja { 0% { transform:rotate(0deg) } 100% { transform: rotate(360deg);} }
+
+
+    </style>
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_head(); ?>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo $native_path ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/style.css?v=09052024">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/animated.css">
+    <link rel="stylesheet" href="<?php echo $native_path ?>/assets/css/owl.css">
+</head>
+
+<body data-spy="scroll" data-target="#myScrollspy" data-offset="15" class="pozadina">
+<?php do_action('telegram_body_start'); ?>
+  <!-- ***** Preloader Start ***** -->
+
+
+  <div class="blog hero">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+               <img src="<?php echo $native_path ?>/assets/images/claim.svg" style="padding: 30px" /><br/>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="blog" style="background: #8ad1cb;">
+  <div style="position: absolute; width:120px; height: 120px; right: 50px; top:250px; -webkit-animation: vrtnja 11s linear 0s infinite; animation: vrtnja 11s linear 0s infinite;"><img src="<?php echo $native_path ?>/assets/images/cvijet01.svg"/></div>
+  <div class="container">
+      <div class="row">
+
+          <div class="col-lg-8 offset-lg-2 text-left wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+            <h2>Ljeto je razdoblje koje iščekujemo cijele godine. Svaki slobodan trenutak koristimo za planiranje godišnjeg odmora i dana provedenih na plaži, uz zvukove valova i cvrčaka.</h2>
+             <p class="sticky-top" style=" font-weight: 500;">Želimo ljeto provesti uživajući u svakom trenutku, potpuno neopterećeni bilo čime. Okruženi najdražim ljudima, na najboljim lokacijama, spremni smo stvarati uspomene koje ćemo pamtiti cijeli život. I sve što nam treba je maleni kofer u kojem nosimo ljetne haljine i šarene kupaće kostime. <a href="https://ad.doubleclick.net/ddm/trackclk/N1025728.3833264SUPER1.TELEGRAM./B31995088.395324084;dc_trk_aid=586987399;dc_trk_cid=216153861;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" target="_blank" style="font-weight:700 !important; text-decoration:underline !important; color: #000 !important">Pronašli smo ih u HM-u.</a> </p>
+          </div>
+        </div>
+      </div>
+</div>
+
+
+<div class="blog" style="background: linear-gradient(180deg, rgba(138,209,203,1) 0%, rgba(247,209,121,1) 100%);">
+  <div style="position: absolute; width:200px; height: 200px; border-radius: 100px; left: -50px; top:600px; -webkit-animation: vrtnja 20s linear 0s infinite; animation: vrtnja 20s linear 0s infinite;"><img src="<?php echo $native_path ?>/assets/images/cvijet02.svg"/></div>
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-5">
+             <p style="color:#fff;" class="sticky-top"><img src="<?php echo $native_path ?>/assets/images/sesdesete.svg"  class="wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0.5s" /><br/><br/><span class="wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0.5s">Nakon dugog doručka i kave koja nas je razbudila, posežemo za omiljenim kupaćim kostimom i dugim kaftanom. Ove godine nosimo crvene i crne kupaće kostime koje možemo i međusobno kombinirati, a za potpuni look biramo dugačke kaftane. Ljeti se ne libimo izaći iz zone komfora u svakom smislu, pa i u onom modnom. Biramo upečatljive komade koji privlače pažnju, široke i prozračne kako bismo što bolje podnijeli visoke temperature.</span>
+              </p>
+          </div>
+          <div class="col-lg-7 wow bounceIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+            <img src="<?php echo $native_path ?>/assets/images/sesdesete.jpg" style="z-index:1 !important" />
+          </div>
+        </div>
+      </div>
+</div>
+
+
+<div class="blog" style="background: linear-gradient(180deg, rgba(247,209,121,1) 0%, rgba(211,121,126,1) 100%); padding-bottom: 0;">
+  <div style="position: absolute; width:180px; height: 180px; right: 100px; top:400px; -webkit-animation: vrtnja 16s linear 0s infinite; animation: vrtnja 16s linear 0s infinite;"><img src="<?php echo $native_path ?>/assets/images/cvijet03.svg"/></div>
+
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-12 paddingdolje">
+                <div class="container">
+                  <div class="row">
+                      <div class="col-lg-6 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+
+                        <img src="<?php echo $native_path ?>/assets/images/sedamdesete.svg"/>
+
+                      </div>
+                      <div class="col-lg-6">
+                      </div>
+                      <div class="col-lg-6 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+
+                        <p style="color: #fff; vertical-align: center;">Morski trening i plivanje slijede nakon pauze za ručak. Za povratak na plažu biramo senzualne dvodijelne kupaće kostime ukrašene zlatnim detaljima. Nosimo <em>statement</em> nakit koji se savršeno uklapa uz brončanu put i posvijetljenu kosu. </p>
+                      </div>
+                      <div class="col-lg-6">
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+          <div class="col-lg-4 p-0 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-01.jpg" />
+          </div>
+          <div class="col-lg-4 p-0 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.2s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-02.jpg" />
+          </div>
+          <div class="col-lg-4 p-0 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.4s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-03.jpg" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 p-0 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.6s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-04.jpg" />
+          </div>
+          <div class="col-lg-6 p-0 wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.8s">
+            <img src="<?php echo $native_path ?>/assets/images/slobodne-05.jpg" />
+          </div>
+      </div>
+  </div>
+</div>
+
+
+<div class="blog" style="background: linear-gradient(180deg, rgba(211,121,126,1) 0%, rgba(123,66,130,1) 100%);">
+  <div style="position: absolute; width:150px; height: 150px; left: -50px; top:100px; -webkit-animation: vrtnja 20s linear 0s infinite; animation: vrtnja 20s linear 0s infinite;"><img src="<?php echo $native_path ?>/assets/images/cvijet01.svg"/></div>
+
+  <div style="position: absolute; width:200px; height: 200px; border-radius: 100px; right: 50px; top:600px; -webkit-animation: vrtnja 15s linear 0s infinite; animation: vrtnja 15s linear 0s infinite;"><img src="<?php echo $native_path ?>/assets/images/cvijet02.svg"/></div>
+
+
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-5 order-lg-2">
+             <p class="sticky-top" style="color:#fff"><img src="<?php echo $native_path ?>/assets/images/osamdesete.svg"  class="wow fadeInRight" data-wow-duration="0.5s" data-wow-delay="0.5s" /><br/><br/><span class="wow fadeInRight" data-wow-duration="0.5s" data-wow-delay="0.5s">Za večernje prilike biramo dugačke lanene haljine koje se savršeno uklapaju s ljetnim ugođajem – opušteni kroj i šareni uzorci garancija su za stylish look, a osobito kada ne želimo gubiti vrijeme na osmišljavanje savršenog outfita. Crno-bijela kombinacija bezvremenski je klasik, a za dašak boje biramo bogate crvene tonove.</span>
+              </p>
+          </div>
+          <div class="col-lg-7 order-lg-1 p-0 wow zoomIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+
+            <div class="image-gallery">
+                <img src="<?php echo $native_path ?>/assets/images/osamdesete-01.jpg"/>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+
+<div class="blog" style="background: linear-gradient(180deg, rgba(123,66,130,1) 0%, rgba(7,24,71,1) 100%);;">
+  <div style="position: absolute; width:100px; height: 100px; left: 50px; top:100px; -webkit-animation: vrtnja 21s linear 0s infinite; animation: vrtnja 21s linear 0s infinite;"><img src="<?php echo $native_path ?>/assets/images/cvijet03.svg"/></div>
+
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-12 paddingdolje">
+                <div class="container">
+                  <div class="row">
+
+                      <div class="col-lg-5 wow zoomIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+                        <img src="<?php echo $native_path ?>/assets/images/devedesete.svg" />
+                      </div>
+                      <div class="col-lg-1">
+                      </div>
+                      <div class="col-lg-6 align-self-end wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+                        <p style="color:#fff">Kratke ljetne noći provodimo budni u najdražem društvu. Umjesto cipela na visoku petu, biramo ravne šlapice, a umjesto uskih haljina nosimo voluminozne bez rukava. Cvjetni uzorci idealni su odabir za večernje kombinacije, a ispod njih nosimo svoje omiljene kupaće kostime kako bismo bili spremni za noćno kupanje.</p>
+                      </div>
+
+
+                  </div>
+              </div>
+          </div>
+
+      </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+          <div class="col-lg-12 p-0 wow zoomIn"  data-wow-duration="0.5s" data-wow-delay="0.5s">
+            <img src="<?php echo $native_path ?>/assets/images/devedesete.jpg" />
+          </div>
+          <div class="col-lg-6 p-0 wow fadeInLeft"  data-wow-duration="0.5s" data-wow-delay="0.5s">
+            <img src="<?php echo $native_path ?>/assets/images/devedesete-01.jpg" />
+          </div>
+          <div class="col-lg-6 p-0 wow fadeInRight"  data-wow-duration="0.5s" data-wow-delay="0.5s">
+            <img src="<?php echo $native_path ?>/assets/images/devedesete-02.jpg" />
+          </div>
+    </div>
+  </div>
+</div>
+
+
+  <div id="clanci" class="blog" style="z-index: 100; position: relative; padding-top: 0px; padding-bottom: 120px; overflow: hidden; background: rgba(7,24,71,1);">
+
+
+    <div class="container-fluid wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+
+      <div class="row">
+        <div class="col-lg-4 offset-lg-4 text-center">
+              <p style="color:#fff">Autor teksta: Ana Šoda<br/>
+              Kreativno vodstvo: TG Studio<br/>
+                Grafičko oblikovanje: Reset</p>
+              <p style="color:#fff">Producirano u radionici TG Studija, Telegramove in-house agencije za nativni marketing po najvišim uredničkim standardima Telegram Media Grupe.</p><br/><br/>
+              <img src="<?php echo $native_path ?>/assets/images/telegram.png" style="max-width: 186px;"  />
+          </div>
+        </div>
+      </div>
+  </div>
+  <script type="text/javascript">
+      const images = ["<?php echo $native_path ?>/assets/images/osamdesete-01.jpg", "<?php echo $native_path ?>/assets/images/osamdesete-02.jpg"];
+      let currentImageIndex = 0;
+      const imageElement = document.querySelector(".image-gallery img");
+
+
+      function updateImage() {
+        const imageUrl = images[currentImageIndex];
+        imageElement.style.opacity = 0; // Postavite opacity na 0 prije promjene slike
+        setTimeout(() => {
+          imageElement.src = imageUrl;
+          imageElement.alt = `Slika ${currentImageIndex + 1}`;
+          imageElement.style.opacity = 1; // Postavite opacity na 1 nakon promjene slike
+        }, 500); // Promijenite 1000 ms na željeni vremenski interval za tranziciju
+      }
+
+      function showNextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        updateImage();
+      }
+
+      // Postavite interval za automatsko klizanje svakih 3 sekunde (3000 ms)
+      setInterval(showNextImage, 5000);
+
+      // Inicijalno postavljanje slike
+      updateImage();
+</script>
+  <!-- Scripts -->
+  <script src="<?php echo $native_path ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo $native_path ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/owl-carousel.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/animation.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/imagesloaded.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/custom.js"></script>
+  <script src="<?php echo $native_path ?>/assets/js/script.js"></script>
+
+    <!-- this is required to load all the analytics and other scripts -->
+    <?php wp_footer(); ?>
+
+<SCRIPT language='JavaScript1.1' SRC="https://ad.doubleclick.net/ddm/trackimpj/N1025728.3833264SUPER1.TELEGRAM./B31995088.395324084;dc_trk_aid=586987399;dc_trk_cid=216153861;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1?" attributionsrc ></SCRIPT>
+</body>
+</html>
