25846e28fd9ed95d56d178b1e8b4f1ef6b67f437
Marijan Mikolić <marijan.mikolic@gmail.com>
2024-07-08 13:26:03 +0200

    Native Lice promjene v1.02 - format data nicer

--- templates/native/lice-promjene/assets/functions.js
+++ templates/native/lice-promjene/assets/functions.js
@@ -24,18 +24,23 @@ jQuery(document).ready(function ($) {
 
             result = highestType; // Use highest scoring type as the result identifier
             var resultMessage = '';
+            var resultType = 'Unknown';
             switch (result) {
                 case 'typeA':
                     resultMessage = "Kategorija: Za dostojanstvo.<br> Borac protiv nepravde. <br> Ti si Robin Hood, Erin Brockovich ili Katness Everdeen - neumorno se boriš protiv nepravde i uvijek si na prvoj liniji kada treba pomoći onima kojima je to najpotrebnije. Poznat si u društvu kao netko tko će uvijek biti moralna podrška i reći koji je put ispravan bez obzira na to hoće li se sugovorniku odgovor svidjeti. Uvijek naglašavaš važnost pravde i jednakosti.";
+                    resultType = 'Dostojanstvo';
                     break;
                 case 'typeB':
                     resultMessage = "Kategorija: Za jednakost.<br> Control freak.<br> Jerry Seinfeld, Monica Geller, Blair Waldorf i ti - sve vas povezuje jedna stvar, a to je činjenica da ste control freakovi. Nema ništa negativno u tome što želiš sve u svakom trenutku imati pod kontrolom, tvoji prijatelji uživaju u tome. Znaju da mogu računati na tebe i da ćeš pred svaku zabavu osigurati svakome njegovu čašu, slamku, tanjur te stolicu.";
+                    resultType = 'Jednakost';
                     break;
                 case 'typeC':
                     resultMessage = "Kategorija: Za prirodu.<br> Ekološki ratnik.<br> Tarzan, Pocahontas ili Doctor Dolittle, velike su vjerojatnosti da nekoga od njih smatraš uzorom. Baš kao i ti, oni su predani zaštiti prirode te borbi za očuvanje planeta. Ukoliko s ekipom roštiljaš pored rijeke, definitivno vodiš svog psa sa sobom te brineš da ne uznemirujete prirodu oko sebe.";
+                    resultType = 'Priroda';
                     break;
                 case 'typeD':
                     resultMessage = "Kategorija: Za budućnost.<br> Sveznalica.<br> Poput Sheldona iz Teorije velikog praska ili Rossa iz Prijatelja, ti si osoba u društvu koja uvijek ima odgovor na svako pitanje. Kod tebe nema dileme - znaš kako se pravilno razvrstavaju i najbizarniji proizvodi, a prijatelje ćeš u trgovini podsjetiti da razmisle dvaput prije uzimanja plastične vrećice. Ipak razgradnja jedne traje 20 godina.";
+                    resultType = 'Budućnost';
                     break;
                 default:
                     resultMessage = "Nešto ne radi kod nas :(";
@@ -46,9 +51,9 @@ jQuery(document).ready(function ($) {
             $('.slide.slide-' + active).addClass('active');
 
             // Sending the result with time to our Google Sheet
-            var rightnow = Date.now();
-            var rightnow_test = rightnow.toString();
-            var response_object = { result: result, time: rightnow_test };
+            var rightnow = new Date();
+            var rightnow_test = rightnow.toISOString();
+            var response_object = { result: resultType, time: rightnow_test };
 
             var jqxhr = $.ajax({
                 url: 'https://script.google.com/macros/s/AKfycbwbLUPyelV9SDDhoS65a_VScIjwQLts49XoR4vVQFOoIzCXRnKqawOuKOFezLrFyvk3/exec',
