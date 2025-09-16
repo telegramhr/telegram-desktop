978faac6e2122dfff53a9a1ce898a28800e176cc
Lovro Tabain <tlovro@gmail.com>
2025-09-16 12:35:09 +0200

    update
    dodao neki fontovi i neke promjene

--- templates/native/kavatip-by-franck/header.php
+++ templates/native/kavatip-by-franck/header.php
@@ -24,14 +24,14 @@ $native_path = 'https://www.telegram.hr/wp-content/themes/telegram2-desktop/temp
     <link rel="stylesheet" href="<?php echo $native_path ?>assets/aos/aos.css?v=1.00" type="text/css" />
     <script src="<?php echo $native_path ?>assets/aos/aos.js"></script>
     <!-- Styles -->
-    <link rel="stylesheet" href="<?php echo $native_path ?>assets/operon.css?v=1.20" type="text/css" />
-    <link rel="stylesheet" href="<?php echo $native_path ?>assets/style.css?v=1.20" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>assets/operon.css?v=1.30" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $native_path ?>assets/style.css?v=1.30" type="text/css" />
     <!-- Slick slider -->
     <script src="<?php echo $native_path ?>assets/slick/slick.js"></script>
     <link rel="stylesheet" href="<?php echo $native_path ?>assets/slick/slick.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $native_path ?>assets/slick/slick-theme.css" type="text/css" />
     <!-- Functions -->
-    <script src="<?php echo $native_path ?>assets/functions.js"></script>
+    <script src="<?php echo $native_path ?>assets/functions.js?v=1.30"></script>
 </head>
 <body>
     <div class="main-container flex relative">
