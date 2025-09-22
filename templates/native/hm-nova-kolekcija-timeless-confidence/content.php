721d56651f4e3d961dfc5c7640bc73fac429447f
marinkosevoTMG <marinko.sevo@telegram.hr>
2025-09-22 15:57:53 +0200

    HM nova kolekcija init

---
+++ templates/native/hm-nova-kolekcija-timeless-confidence/content.php
@@ -0,0 +1,11 @@
+<main class="w-full bg-accent">
+    <?php include 'template-parts/hero.php'; ?>
+    <?php include 'template-parts/hm.php'; ?>
+    <?php include 'template-parts/collection-ticker.php'; ?>
+    <?php include 'template-parts/main-paragraph.php'; ?>
+    <?php include 'template-parts/zig-zag.php'; ?>
+    <?php include 'template-parts/items-slider.php'; ?>
+    <?php include 'template-parts/last-paragraph.php'; ?>
+</main>
+<link rel="stylesheet" href="./dist/main.css">
+<script src="./dist/main.js"></script>
---
+++ templates/native/hm-nova-kolekcija-timeless-confidence/template-parts/last-paragraph.php
@@ -0,0 +1,14 @@
+<div data-aos="fade-up" data-aos-duration="2000" data-aos-delay="250" class="w-full overflow-hidden pb-15 md:pb-20 px-4">
+    <div class="max-w-[900px] mx-auto flex flex-col gap-10 text-center">
+        <h3 class="font-gilda text-[28px] md:text-[40px] text-black leading-[90%]">Paleta i detalji: Priča boja i tekstura</h1>
+            <div class="flex flex-col gap-8 text-[20px] md:text-[28px] leading-[24px] md:leading-[34px] font-normal font-montserrat text-black tracking-tighter">
+                <p>Kolekcija istražuje kontraste kroz bogatu paletu i raskošne materijale. Nijanse smeđe, burgundy i neutralnih tonova protkane su iznenadnim sportskim akcentima crvene i plave, stvarajući dinamičan
+                    vizualni dojam.
+
+                </p>
+                <p>Romantični duh dolazi kroz čipku, šifon i saten, dok koža, bilo prava ili umjetna, i inovativno umjetno krzno donose buntovnu energiju i taktilnu raznolikost.</p>
+
+                <p>Cjelinu upotpunjuju dodaci koji se poigravaju proporcijama i teksturama: od mini torbica od krzna i oversized tote modela do sportskih bowling torbi i eklektičnog nakita s vintage cvjetnim motivima.</p>
+            </div>
+    </div>
+</div>
