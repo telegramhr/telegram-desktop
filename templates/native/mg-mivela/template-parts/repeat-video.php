d0251bee97d420d5fd17086e968a7e3e034f3597
marinkosevoTMG <marinko.sevo@telegram.hr>
2025-10-21 14:12:14 +0200

    Native/mg mivela (#76)

--- templates/native/mg-mivela/template-parts/repeat-video.php
+++ templates/native/mg-mivela/template-parts/repeat-video.php
@@ -2,22 +2,22 @@
 
     <div class="flex flex-row max-h-[683px] overflow-hidden">
         <div class="h-full flex flex-1">
-            <video class="lazy-video w-full h-full object-cover aspect-[0.66] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-1.webm" autoplay muted playsinline loop preload="metadata">
+            <video class="lazy-video w-full h-full object-cover aspect-[0.54] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-1.webm" autoplay muted playsinline loop preload="metadata">
 
             </video>
         </div>
         <div class="h-full flex flex-1">
-            <video class="lazy-video w-full h-full object-cover aspect-[0.66] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-2.webm" autoplay muted playsinline loop preload="metadata">
+            <video class="lazy-video w-full h-full object-cover aspect-[0.54] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-2.webm" autoplay muted playsinline loop preload="metadata">
 
             </video>
         </div>
         <div class="h-full hidden lg:flex flex-1">
-            <video class="lazy-video w-full h-full object-cover aspect-[0.66] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-1.webm" autoplay muted playsinline loop preload="metadata">
+            <video class="lazy-video w-full h-full object-cover aspect-[0.54] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-1.webm" autoplay muted playsinline loop preload="metadata">
 
             </video>
         </div>
         <div class="h-full hidden lg:flex flex-1">
-            <video class="lazy-video w-full h-full object-cover aspect-[0.66] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-2.webm" autoplay muted playsinline loop preload="metadata">
+            <video class="lazy-video w-full h-full object-cover aspect-[0.54] scale-[1.3]" data-src="<?= $native_path ?>/assets/repeat-video/repeat-video-2.webm" autoplay muted playsinline loop preload="metadata">
 
             </video>
         </div>
