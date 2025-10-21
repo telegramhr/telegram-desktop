7d6a987558d71f8634a1344bdbe21b48cae480da
marinkosevoTMG <marinko.sevo@telegram.hr>
2025-10-21 12:48:40 +0200

    Init mg mivela (#73)

---
+++ templates/native/mg-mivela/template-parts/double-video.php
@@ -0,0 +1,17 @@
+<div class="w-full overflow-hidden px-4">
+    <div class="max-w-[1280px] mx-auto flex flex-col md:flex-row gap-8 lg:gap-5 py-7 md:py-20 relative ">
+        <img class="flex lg:hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[355px] z-40 spin-slow" src="<?= $native_path ?>/assets/run-text/run-circle-text.png" alt="Run circle text">
+
+        <div class="flex flex-1 h-full max-h-[512px] md:max-h-[911px] overflow-hidden">
+            <video class="lazy-video w-full h-full object-cover aspect-[0.69] scale-[1.3]" data-src="<?= $native_path ?>/assets/double-video/double-video-1.webm" autoplay muted playsinline loop preload="metadata">
+
+            </video>
+        </div>
+        <div class="flex flex-1 h-full max-h-[512px] md:max-h-[911px] overflow-hidden">
+            <video class="lazy-video w-full h-full object-cover aspect-[0.69] scale-[1.3]" data-src="<?= $native_path ?>/assets/double-video/double-video-2.webm" autoplay muted playsinline loop preload="metadata">
+
+
+            </video>
+        </div>
+    </div>
+</div>
---
+++ templates/native/mg-mivela/template-parts/footer.php
@@ -0,0 +1,88 @@
+<div class="w-full overflow-hidden relative">
+    <video class="lazy-video absolute hidden lg:block w-full h-full opacity-60 object-cover" data-src="<?= $native_path ?>/assets/main-container/bg-main-container.webm" autoplay muted playsinline loop preload="none">
+    </video>
+    <div class="w-full px-4">
+        <div class="max-w-[1060px] mx-auto flex flex-col gap-4 lg:gap-7 pt-7 pb-8 lg:py-20 relative">
+            <h3 class="text-primary text-[20px] lg:text-[40px] font-bold uppercase">
+                Dvostruka uloga vode obogaćene s magnezijem
+            </h3>
+            <p class="text-primary text-[16px] lg:text-[28px] font-bold font-dmsans">
+                Prvi puta se upoznala s magnezijem još u srednjoj školi, kada je pohađala plesnu školu i trenirala svaki dan. Vrlo brzo je primijetila razliku kroz manji umor, manje grčeva i bolju koncentraciju. Danas joj je unos magnezija rutina, dio svakodnevne brige o sebi.
+                Pravo otkriće joj je voda s magnezijem i nju pije svakodnevno, bilo u uredu ili nakon treninga. “Tako istovremeno ostajem hidrirana i dajem tijelu ono što mu treba”, kaže. Aktivne osobe koje se više znoje i troše minerale, trebaju nadoknaditi gubitak vode, kao i magnezija. Pa tako voda s magnezijem ima dvostruku ulogu.
+            </p>
+        </div>
+    </div>
+    <div class="w-full ">
+        <div class="flex flex-col">
+            <video class="lazy-video block lg:block absolute w-full h-full opacity-60 object-cover" data-src="<?= $native_path ?>/assets/main-container/bg-main-container.webm" autoplay muted playsinline loop preload="none">
+
+            </video>
+
+
+            <div class="w-full px-4">
+                <div class="max-w-[1280px] mx-auto flex flex-col lg:flex-row lg:gap-0 lg:max-h-[1115px] relative">
+                    <div class="flex flex-1 w-full h-full max-h-[620px] overflow-hidden lg:max-h-none bg-black relative lg:left-[1px]">
+                        <video class="lazy-video w-full h-full object-cover aspect-[0.69] opacity-40 scale-[1.4]" autoplay muted playsinline loop preload="none"
+                            data-src="<?= $native_path ?>/assets/choice/choice.webm">
+                        </video>
+                    </div>
+
+                    <div class="hidden lg:flex flex-1 w-full h-full max-h-[620px] left-[-1px] overflow-hidden lg:max-h-none bg-black">
+                        <video class="lazy-video w-full h-full object-cover aspect-[0.69] opacity-40 scale-[1.4]" autoplay muted playsinline loop preload="none"
+                            data-src="<?= $native_path ?>/assets/choice/choice.webm">
+                        </video>
+                    </div>
+
+                    <div class="absolute inset-0 flex items-center justify-center text-center px-6">
+                        <p class="text-primary text-[16px] lg:text-[36px] font-bold font-dmsans max-w-[1060px]">
+                            Njezin izbor je Mg Mivela, prirodna mineralna voda bogata magnezijem.
+                            Ona sadrži 343 mg magnezija po litri, što znači da samo jedna litra
+                            dnevno zadovoljava oko 90 posto preporučene dnevne potrebe.
+                            Također, Mg Mivela sadrži 100 posto prirodan magnezij koji dolazi iz
+                            izvora duboko u zemlji, a možete birati između gazirane i blago gazirane
+                            varijante.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-full px-4 relative pt-3 lg:pt-8">
+        <div class="max-w-[1280px] mx-auto">
+            <img src="<?= $native_path ?>/assets/footer/before-footer.webp" class="hidden lg:block w-full object-cover aspect-[1.5]" alt="Before footer image">
+            <img src="<?= $native_path ?>/assets/footer/before-footer-mobile.webp" class="block lg:hidden w-full object-cover aspect-[0.66]" alt="Before footer image">
+
+        </div>
+    </div>
+    <div class="w-full px-4 relative">
+        <div class="max-w-[1060px] mx-auto flex flex-col gap-4 lg:gap-7 py-8 lg:pb-20 lg:pt-[70px]">
+            <h3 class="text-primary text-[20px] lg:text-[40px] font-bold uppercase">Slušajte svoje tijelo</h3>
+            <p class="text-primary text-[16px] lg:text-[28px] font-bold font-dmsans">
+                Tjelesna aktivnost ne mora značiti napor, nego način da se osjećate bolje. Regeneracija je jednako važna kao i sam trening, a briga o mineralima kao što je magnezij, snu i odmoru čini veliku razliku. “Kad naučite osluškivati svoje tijelo, ono vam to vraća s više energije i boljim raspoloženjem. Ima li što bolje od toga?”, savjetuje Filipa za kraj.
+            </p>
+        </div>
+    </div>
+    <div class="w-full px-4 pb-[103px] lg:pb-34 relative">
+        <div class="max-w-[1280px] mx-auto bg-primary relative">
+            <div class="max-w-[1060px] mx-auto flex flex-col gap-8 lg:gap-10 pt-4 px-5 lg:px-0 lg:pt-12 pb-18">
+                <div class="lg:max-h-[800px] flex flex-col lg:flex-row gap-5 lg:gap-10 overflow-hidden">
+                    <div class="flex flex-1">
+                        <img src="<?= $native_path ?>/assets/footer/footer-2.webp" class="w-full object-cover aspect-[0.66]" alt="Footer">
+                    </div>
+                    <div class="flex flex-1">
+                        <img src="<?= $native_path ?>/assets/footer/footer-1.webp" class="w-full object-cover aspect-[0.66]" alt="Footer">
+                    </div>
+                </div>
+
+                <div class="flex flex-col text-[14px] lg:text-[20px] text-black font-bold text-center uppercase">
+                    <span>Fotograf: Matej Dokić</span>
+                    <span>Video: Jigibau</span>
+                    <span>Model: Filipa Ledić</span>
+                    <span>Autor teksta: Lucija Vogrinc</span>
+                    <span>Dizajn: Domia Oršolić</span>
+                </div>
+                <img class="max-w-[62px] lg:max-w-[100px] mx-auto w-full" src="<?= $native_path ?>/assets/footer/logo-footer.png" alt="Mg Mivela logo" />
+            </div>
+        </div>
+    </div>
+</div>
