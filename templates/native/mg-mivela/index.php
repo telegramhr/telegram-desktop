b65095b1b11deafda81040206f54bc4613481bf8
Marinko Sevo <marinko.sevo@telegram.hr>
2025-10-21 14:09:25 +0200

    Fix styles

--- templates/native/mg-mivela/index.php
+++ templates/native/mg-mivela/index.php
@@ -21,7 +21,7 @@ $jsPath = $distPath . '/main.js';
     ?>
     <!-- Styles -->
     <link rel="stylesheet" href="<?php echo $native_path; ?>/dist/style.css?v=<?php echo file_exists($cssPath) ? filemtime($cssPath) : '1'; ?>">
-    <link rel="preload" href="<?php echo $native_path; ?>/assets/hero/hero-video.webm" as="video" type="video/webm" crossorigin="anonymous">
+    <link rel="preload" href="<?= $native_path; ?>/assets/hero/hero-video.webm" as="fetch" type="video/webm" importance="high" crossorigin="anonymous">
 
     <!-- JavaScript -->
     <script src="<?php echo $native_path; ?>/dist/main.js?v=<?php echo file_exists($jsPath) ? filemtime($jsPath) : '1'; ?>" defer></script>
--- templates/native/mg-mivela/template-parts/momentum.php
+++ templates/native/mg-mivela/template-parts/momentum.php
@@ -1,12 +1,12 @@
 <div class="w-full bg-black px-4 overflow-hidden py-8 lg:py-20 z-10 relative">
-    <div class="max-w-[1280px] mx-auto flex flex-col gap-12 lg:gap-[75px]">
+    <div class="max-w-[1280px] mx-auto flex flex-col gap-12 lg:gap-20">
         <h2 class="max-w-[700px] mx-auto text-center font-panchang font-extrabold text-[24px] md:text-[42px] lg:text-[60px] text-primary uppercase">“No map. Just momentum.”</h2>
-        <div class="flex flex-col gap-4 lg:gap-[68px] pt-4 lg:pt-[40px] pb-7 lg:pb-[80px] px-5 lg:px-[106px] bg-primary">
+        <div class="flex flex-col gap-4 lg:gap-20 pt-4 lg:pt-[40px] pb-7 lg:pb-[80px] px-5 lg:px-[106px] bg-primary">
             <div class="flex flex-col">
                 <img class="max-h-[600px] aspect-[1.78] w-full object-cover" src="<?= $native_path ?>/assets//momentum/momentum.webp" alt="Momentum background image">
                 <div class="flex lg:hidden flex-row items-center mx-auto whitespace-pre"> <span class="text-[12px] italic font-crimson "> running</span> <span class="font-panchang font-extrabold text-[8px]">/out of time</span> <span class="text-[12px] italic font-crimson "> running</span> <span class="font-panchang font-extrabold text-[8px]">/out of time</span> <span class="text-[12px] italic font-crimson "> running</span> <span class="font-panchang font-extrabold text-[8px]">/out of time</span> </div>
             </div>
-            <p class="text-center font-panchang text-[14px] lg:text-[36px] px-3 lg:px-0 font-bold text-black">Provodimo dan s Filipom Ledić, odvjetničkom vježbenicom i profesionalnom plesačicom koja se plesom bavila punih dvadeset godina.
+            <p class="text-center font-panchang text-[14px] lg:text-[36px] px-3 lg:px-0 font-bold text-black">Provodimo dan s Filipom Ledić, odvjetničkom vježbenicom iprofesionalnom plesačicom koja se plesom bavila punih dvadeset godina.
                 Obzirom na odabir trenutne karijere, njezin radni dan se sastoji od sjedenja u uredu pa joj je posebno važno da pronađe vrijeme za kretanje, makar to bila kratka šetnja dijelom puta do posla, pilates ili odlazak u teretanu.</p>
         </div>
     </div>
