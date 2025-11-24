600d9e5e6cd7d10440bb363bdaa47d44dc80cbcc
Marinko Sevo <marinko.sevo@telegram.hr>
2025-11-24 10:52:49 +0100

    Platforms update

--- blocks/podcast-platforms/podcast-platforms.php
+++ blocks/podcast-platforms/podcast-platforms.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Plugin Name:       Podcast Platforms
- * Description:       Example block scaffolded with Create Block tool.
- * Version:           0.1.0
- * Requires at least: 6.7
- * Requires PHP:      7.4
- * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       podcast-platforms
- *
- * @package CreateBlock
- */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
