755a68036d252dd453befc57d6d18e183b66e6fe
marinkosevoTMG <marinko.sevo@telegram.hr>
2025-12-02 12:59:47 +0100

    Native/super1 advent (#86)
    * super1-advent desktop start
    
    * super1-advent desktop and mobile
    
    * Style changes, add articles

---
+++ templates/native/super1-advent/template-parts/news-2.php
@@ -0,0 +1,88 @@
+<?php
+$title = "Pročitaj više";
+
+$cards = [
+    [
+        "image" => "https://images.telegram.hr/ElrYNqjgCwTQeThdRuAWoCbx66XPrrxcI0W-1KdXNfY/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvaG90ZWwtYW1hbi1yb3NhLWFscGluYS5qcGc.webp",
+        "text" => "Novo mjesto Gdje bismo najradije proveli zimske praznike? Vjerojatno u novom Aman hotelu smještenom u Dolomitima",
+        "link" => "https://www.telegram.hr/super1/life/aman-rosa-alpina-hotel-dolomiti/"
+    ],
+    [
+        "image" => "https://www.telegram.hr/wp-content/uploads/2025/11/party-kolekcije-cover.jpg",
+        "text" => "Party season Stigle su party kolekcije za blagdansku sezonu! Ovo su svi komadi koje trebate u svojim košaricama",
+        "link" => "https://www.telegram.hr/super1/shopping-vodic/party-kolekcije-najbolji-modeli-zimski-party-look/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/vRwitDQ9p3I6hY4yLMRBbNGOx5O9_aoUVV1r0tTEwrY/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvY2hhbXBhZ25lLXByb2JsZW1zLW5ldGZsaXguanBn.webp",
+        "text" => "Na watchlisti Ovo će postati hit: Na Netflix je stigao ultimativni blagdanski rom-com, Champagne Problems",
+        "link" => "https://www.telegram.hr/super1/life/netflix-champagne-problems-romanticna-komedija/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/ppcmo_8Un5gyRob8D0_7d5K7Lwju-ZgQn0vnmMuDoaA/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvZGlnaXRhbC13aW50ZXItZGlhcnkuanBn.webp",
+        "text" => "Selfcare vodič Kako preživjeti zimu i pritom uživati u njoj? Inspirirajte se s ovih 6 nordijskih wellness navika",
+        "link" => "https://www.telegram.hr/super1/life/nordijske-wellnes-ideje-za-zimu/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/GvHZHC8wJcL4aAimlkOfPiSpBh0i4MwC5mwWzMtZZk0/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvYnVyaW1vdmEtY292ZXIuanBn.webp",
+        "text" => "Holiday 100 Što svi žele za Božić? Google je objavio listu najpoželjnijih poklona na kojoj smo našli super ideje",
+        "link" => "https://www.telegram.hr/super1/life/google-holiday-100-bozicni-pokloni-vodic/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/EEPxk-izrHC2ydhga47j35ZmcXLX3TDxpPgus8VfeMA/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTIvc2h1dHRlcnN0b2NrLTIyMTI4MjE0NzkuanBn.webp",
+        "text" => "Decor inspo Od božićnog drvca od knjiga do papirnatih zvijezda: 7 održivih DIY ideja za blagdansko dekoriranje",
+        "link" => "https://www.telegram.hr/super1/design/odrzivo-dekoriranje-doma-primjeri-i-ideje/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/PEjrkPFJ-eggLpyKVt5jn3lJqtxgEDLNafYhHLR5qJM/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvc2h1dHRlcnN0b2NrLTI1MjU1NjA4MzEuanBn.webp",
+        "text" => "Home decor Znamo kako urediti dom s božićnom girlandom da ne izgleda staromodno, nego svježe i moderno",
+        "link" => "https://www.telegram.hr/super1/design/bozicna-girlanda-uredenje-interijera-inspiracija/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/ZLK63l9x4f2dfewwJfh21X9E3a1eiWFjjva2feTasfI/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvZ2xhc3N5LWxha2UtdGhlLXJpcHBseS1jYW5kbGUtaG9sZGVyLWJ5LW5ld3dvcmtzZGstdmlhLWRhdmVzLWhvbWUtZTE3NjM1NjM1MDQ2MDguanBn.webp",
+        "text" => "Home decor Pronašli smo skulpturalni svijećnjak savršen za blagdane, evo gdje ga možete nabaviti",
+        "link" => "https://www.telegram.hr/super1/design/skulpturalni-metalni-svijecnjak-gdje-ga-kupiti/"
+    ],
+    [
+        "image" => "https://images.telegram.hr/Xeuf1dP5KAnULwnznWnGUk6RuSh6KpJmD6gJK262Dv8/preset:s1single1/aHR0cHM6Ly93d3cudGVsZWdyYW0uaHIvd3AtY29udGVudC91cGxvYWRzLzIwMjUvMTEvcGV4ZWxzLXNldmlsLXlldmEtMTE3NTA2MTU0Mi0yOTY0MDAyMi1lMTc2MzEzMDcyNzYwNi5qcGc.webp",
+        "text" => "Decor inspo Kako ćemo ove godine kititi bor? Okupili smo najljepše blagdanske trendove na jednom mjestu",
+        "link" => "https://www.telegram.hr/super1/design/tri-trenda-u-kicenju-bozicnog-drvca/"
+    ],
+];
+?>
+<div class="w-full bg-primary">
+    <div class="w-full max-w-[1920px] mx-auto  bg-primary px-4 overflow-hidden relative">
+        <div class="max-w-[1200px] mx-auto w-full flex flex-col gap-8 pt-[160px] text-center">
+
+            <h2 data-aos="zoom-in" class="text-white font-ballet text-[110px] lg:text-[250px] leading-[110px] lg:leading-[220px]">
+                <?= $title ?>
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 ">
+                <?php foreach ($cards as $index => $card): 
+                    if($index % 3 == 1){
+                        $delay = 500;
+                        $direction = 'up';
+                    } elseif($index % 3 == 2){
+                        $delay = 700;
+                        $direction = 'left';
+                    } else {
+                        $delay = 300;
+                        $direction = 'right';
+                    }?>
+                    <a data-aos="fade-<?=  $direction; ?>" data-aos-easing="linear" data-aos-delay="<?= $delay ?>" href="<?= $card['link'] ?>" class="cursor-pointer flex flex-col gap-4 relative hover:scale-105 transition-transform duration-300">
+                        <img src="<?= $native_path ?>assets/post-backgroundv2.webp"
+                            alt="background"
+                            class="absolute w-full h-full">
+                        <div class="flex flex-col gap-4 relative z-10 px-[42px] py-[60px]">
+                            <img src="<?= $card['image'] ?>"
+                                class="max-w-[273px] mx-auto w-full aspect-[0.76] object-cover"
+                                alt="card">
+                            <span class="text-secondary font-climatecrisis text-[16px] leading-[18px] lg:text-[18px] uppercase">
+                                <?= $card['text'] ?>
+                            </span>
+                        </div>
+                    </a>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ templates/native/super1-advent/template-parts/tree.php
@@ -0,0 +1,19 @@
+<div class="w-full max-w-[1920px] mx-auto  overflow-hidden relative bg-cream px-4">
+    <div class="w-full lg:max-w-[875px] aspect-[0.95] mx-auto pt-5 lg:pt-[124px] relative">
+        <img src="<?= $native_path ?>/assets/tree.png" alt="Christmas tree" class="w-full absolute object-cover">
+        <div class="flex flex-col w-full pt-[15px] lg:pt-[26px] gap-6 lg:gap-9 font-climatecrisis text-[35px] lg:text-[75px] text-secondary text-center uppercase leading-[30px] lg:leading-[95px] z-50 relative">
+            <span data-aos="fade-left"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine" class="flex self-end pr-10">GIFT </br> GUIDES</span>
+            <span data-aos="fade-right"
+                data-aos-offset="400"
+                data-aos-easing="ease-in-sine" class="flex self-start">SHOPPING </br> RECS</span>
+            <span data-aos="fade-left"
+                data-aos-offset="450"
+                data-aos-easing="ease-in-sine" class="flex self-end">ADVENT </br> REVIEWS</span>
+            <span data-aos="fade-right"
+                data-aos-offset="500"
+                data-aos-easing="ease-in-sine" class="flex self-start">WARM </br> STORIES</span>
+        </div>
+    </div>
+</div>
