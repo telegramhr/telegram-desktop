b68b72cca9be2be6dbf5b04ebbf101ab22112785
Marinko Sevo <marinko.sevo@telegram.hr>
2025-12-12 17:03:41 +0100

    version update advent

--- templates/native/advent-2025/index.php
+++ templates/native/advent-2025/index.php
@@ -19,8 +19,8 @@ $jsPath = $native_path . '/main.js';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head();
     ?>
-    <link rel="stylesheet" href="<?php echo $native_path; ?>/dist/style.css?v=<?php echo file_exists($cssPath) ? filemtime($cssPath) : '1'; ?>">
-    <script src="<?php echo $native_path; ?>/dist/main.js?v=<?php echo file_exists($jsPath) ? filemtime($jsPath) : '1'; ?>" defer></script>
+    <link rel="stylesheet" href="<?php echo $native_path; ?>/dist/style.css?v=23">
+    <script src="<?php echo $native_path; ?>/dist/main.js?v=11" defer></script>
 </head>
 
 <body>
