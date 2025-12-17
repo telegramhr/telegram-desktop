e36c53d911fb25b2f024f77cdc37cabdcd661e8f
Marinko Sevo <marinko.sevo@telegram.hr>
2025-12-17 11:45:33 +0100

    Remove block podcast

--- functions-shared.php
+++ functions-shared.php
@@ -888,9 +888,3 @@ function telegram_block_image($block_content, $block, $that) {
     }
     return $block_content;
 }
-
-function tema_register_podcast_platforms_block()
-{
-	register_block_type(get_template_directory() . '/blocks/podcast-platforms/build/podcast-platforms');
-}
-add_action('init', 'tema_register_podcast_platforms_block');
