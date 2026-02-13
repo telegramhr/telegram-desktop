4c957c70c7ea063952fd6aeefd6bee0f94a82ebf
Marinko Sevo <marinko.sevo@telegram.hr>
2026-02-13 20:28:04 +0100

    fix all

--- templates/native/super-secret-dinner/dist/config.js
+++ templates/native/super-secret-dinner/dist/config.js
@@ -1,8 +1,8 @@
 var RM = window.RM = window.RM || {};
 
 window.RM.config = {
-    root: "/super1/native/wp-content/themes/telegram2-desktop/templates/native/super-secret-dinner/",
+    root: "/wp-content/themes/telegram2-desktop/templates/native/super-secret-dinner/",
     pushState: true
 }
 
-window.chunkURL = "/super1/native/wp-content/themes/telegram2-desktop/templates/native/super-secret-dinner/dist/";
+window.chunkURL = "/wp-content/themes/telegram2-desktop/templates/native/super-secret-dinner/dist/";
