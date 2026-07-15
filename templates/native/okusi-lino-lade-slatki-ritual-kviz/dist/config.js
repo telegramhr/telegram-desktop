2cf210b9b53c768e248f7bef9d7f4a9068df8466
Marinko Sevo <marinko.sevo@telegram.hr>
2026-07-15 16:06:01 +0200

    Fix title linolada

--- templates/native/okusi-lino-lade-slatki-ritual-kviz/dist/config.js
+++ templates/native/okusi-lino-lade-slatki-ritual-kviz/dist/config.js
@@ -1,8 +1,9 @@
+
 var RM = window.RM = window.RM || {};
 
 window.RM.config = {
-    root: "/wp-content/themes/telegram2-desktop/templates/native/okusi-lino-lade-slatki-ritual-kviz/",
-    pushState: true
+  root: "/wp-content/themes/telegram2-desktop/templates/native/okusi-lino-lade-slatki-ritual-kviz/",
+  pushState: true
 }
 
 window.chunkURL = "/wp-content/themes/telegram2-desktop/templates/native/okusi-lino-lade-slatki-ritual-kviz/dist/";
